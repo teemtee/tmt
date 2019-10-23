1df629e3157010f8a0fcad8b186751ac027c57f1
Pavel Valena <pvalena@redhat.com>
2019-10-23 15:57:20 +0200

    WIP: vagrant provisioner

--- tmt/steps/provision/vagrant.py
+++ tmt/steps/provision/vagrant.py
@@ -1,21 +1,29 @@
 # coding: utf-8
 
-""" Provision Step Classes """
+""" Provision Step Vagrnat Class """
+
+from click import echo
+
+from tmt.steps.provision.base import ProvisionBase
 
 import tmt
+import childprocess
 import os
 from urllib.parse import urlparse
 
-class ProvisionVagrant(tmt.steps.Step.Provision):
+class ProvisionVagrant(ProvisionBase):
     """ Use Vagrant to Provision an environment for testing """
     executable = 'vagrant'
     config_prefix = '  config.'
+    sync_type = 'rsync'
 
     def __init__(self, data, plan):
         """ Initialize the Vagrant provision step """
         super(Provision, self).__init__(data, plan)
         if not 'how' in self.data:
             self.data['how'] = ''
+
+        self.data['box'] = 'box_' + self.data['instance_name']
 
         try:
             urlparse(image)
@@ -25,14 +33,14 @@ class ProvisionVagrant(tmt.steps.Step.Provision):
 
         if not 'image' in self.data:
             self.data['box'] = 'fedora/f30-cloud-base'
-            image = ''
+            self.data['image'] = ''
 
-        elsif image_is_uri:
-            if len(re.search("\.box$", image) != 0:
+        elif image_is_uri:
+            if len(re.search("\.box$", image)):
                 # an actual box file
                 self.data['box'] = 'box_' + self.instance_name
 
-            elsif len(re.search("\.qcow2$", image) != 0:
+            elif len(re.search("\.qcow2$", image)):
                 # do some qcow2 magic
                 self.data['box'] = '...'
                 raise('NYI')
@@ -42,12 +50,16 @@ class ProvisionVagrant(tmt.steps.Step.Provision):
 
         else:
             self.data['box'] = image
+            self.data['image'] = ''
 
         #self.instancename = ''.join(random.choices(string.ascii_letters, k=16))
         #self.provisiondir = self.workdir + '/' + self.instance_name
 
         # instance_dir?
-        self.vagrantfile = self.workdir + '/Vagrantfile'
+        self.vagrantfile = os.patch.join(self.workdir, 'Vagrantfile')
+
+    def save(self):
+        echo ('NYI: saving localhost')
 
     def show(self):
         """ Show execute details """
@@ -55,6 +67,7 @@ class ProvisionVagrant(tmt.steps.Step.Provision):
 
     def go(self)
         """ Execute actual provisioning """
+        echo ('provisioning localhost')
         self.create(self)
         # self.plan.run.workdir
         # [ . . . ]
@@ -77,10 +90,20 @@ class ProvisionVagrant(tmt.steps.Step.Provision):
         # 'vm.synced_folder ".", "/vagrant"'
         pass
 
+    def destroy(self)
+        """ sync on demand """
+        self.run_vagrant(self, ['destroy', '-f'])
+        # box remove
+        self.run_vagrant(self, ['box', 'remove', '-f'])
+
+    def run_prepare(self)
+        """ add single 'preparator' and run it """
+        self.add_config(self, 'vm.provision "' + self.plan.run.workdir + '", "' + self.plan.run.workdir + '", type: "rsync", rsync__exclude: ".git/"')
 
     def create(self)
         """ Create default Vagrantfile with our modifications """
         self.run_vagrant(self, ['init', '-fm', self.box])
+        self.add_config(self, 'vm.synced_folder "' + self.plan.run.workdir + '", "' + self.plan.run.workdir + '", type: "rsync", rsync__exclude: ".git/"')
         self.add_config(self, 'vm.synced_folder "' + self.plan.run.workdir + '", "' + self.plan.run.workdir + '", type: "rsync", rsync__exclude: ".git/"')
         # [. . . ]
 
@@ -97,18 +120,18 @@ class ProvisionVagrant(tmt.steps.Step.Provision):
         subprocess.call([executable] + args, timeout = self.timeout, cwd = self.workdir) #instance_dir?
         #exit code?
 
-    def add_config(appenddata)
+    def add_config(self, config)
         """ Add config entry into Vagrantfile """
         vagrantdata = open(vagrantfile).read().splitlines()
 
         # Lookup last 'end' so we know position to insert into
         i = 0
-        for v in vagrantdata:
+        for line in vagrantdata:
             i -= 1
-            if len(re.search("end", v) != 0
+            if len(re.search("end", v)
                 break
 
-        vagrantdata = vagrantdata[:i] + [config_prefix + appenddata] + vagrantdata[i:]
+        vagrantdata = vagrantdata[:i] + [config_prefix + config] + vagrantdata[i:]
 
         with open(vagrantfile, 'w') as f:
             f.write(vagrantdata)
