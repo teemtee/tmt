7e92143ba2679f1090eebac8d9e1d7fa19957ed6
Leos Pol <lpol@redhat.com>
2019-10-23 13:08:59 +0200

    Unittests for execute step, API draft for executors

--- tmt/steps/execute/beakerlib.py
+++ tmt/steps/execute/beakerlib.py
@@ -10,5 +10,18 @@ class ExecutorBeakerlib(ExecutorShell):
         super(ExecutorBeakerlib, self).__init__(data, plan)
 
     def go(self, tests):
+        """ Run tests """
         super(ExecutorBeakerlib, self).go(tests)
 
+    # API
+    def requires(self):
+        """ Returns packages required to run tests"""
+        super(ExecutorBeakerlib, self).requires()
+        packages = (
+            'beakerlib',
+        )
+        return packages
+
+    def results(self):
+        """ Returns results from executed tests """
+        super(ExecutorBeakerlib, self).results()
