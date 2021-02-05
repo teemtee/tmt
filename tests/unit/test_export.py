72551dfc08cb70d3885087b2cb55a4a586db13fb
Lukas Zachar <lzachar@redhat.com>
2021-02-05 18:12:25 +0100

    More playing with mock

--- tests/unit/test_export.py
+++ tests/unit/test_export.py
@@ -1,7 +1,94 @@
 import pytest
 from unittest.mock import MagicMock, PropertyMock, patch, call
+import subprocess
+import textwrap
 
 import tmt
+
+class TestEnd2End:
+    @patch('tmt.export.import_nitrate')
+    def test_not_exist_no_create(self, mock_import_nitrate, tmpdir):
+        # initialize fmf/tmt structure
+        subprocess.check_call("fmf init".split(), cwd=str(tmpdir))
+
+        # prepare main.fmf
+        tmpdir.join('main.fmf').write(textwrap.dedent(
+        """
+        test: echo 'hooray'
+        framework: shell
+        summary: Fancy summary
+        """))
+
+        # Load singe test defined above
+        test = tmt.Tree(str(tmpdir)).tests(names=['/'])[0]
+
+        with pytest.raises(tmt.utils.ConvertError) as exc_info:
+            tmt.export.export_to_nitrate(test, create=False, general=True)
+        assert "Nitrate test case id not found." == str(exc_info.value)
+
+        # TODO: Test invalid extra-nitrate (no prefix, not integer)
+
+    def test_exists(self, tmpdir):
+        # initialize fmf/tmt structure
+        subprocess.check_call("fmf init".split(), cwd=str(tmpdir))
+        subprocess.check_call("git init".split(), cwd=str(tmpdir))
+
+        # prepare main.fmf
+        tmpdir.join('main.fmf').write(textwrap.dedent(
+        """
+        test: echo 'hooray'
+        framework: shell
+        summary: Fancy summary
+        extra-nitrate: TC#1234
+        """))
+        subprocess.check_call("git add .".split(), cwd=str(tmpdir))
+        subprocess.check_call("git commit -m init".split(), cwd=str(tmpdir))
+
+        # Load singe test defined above
+        test = tmt.Tree(str(tmpdir)).tests(names=['/'])[0]
+
+        def fake_import():
+            pass
+
+        class LocalCases:
+            created = []
+            def case_factory(*args, **kwargs):
+                mock = MagicMock()
+                LocalCases.created.append(mock)
+                if args[0] == 1234:
+                    type(mock).notes = "" #PropertyMock(return_value="")
+                    type(mock).tags = Container()
+                return mock
+
+        class Container(object):
+            def __init__(self, *args, **kwargs):
+                self._data = []
+            def add(self, value):
+                self._data.append(value)
+            def clear(self):
+                self._data = []
+
+        tmt.export.import_nitrate = fake_import
+        tmt.export.nitrate = MagicMock
+        tmt.export.nitrate.Tag = str
+        tmt.export.nitrate.CaseStatus = str
+        tmt.export.nitrate.TestCase = MagicMock(side_effect=LocalCases.case_factory)
+        tmt.export.nitrate.NitrateError = tmt.utils.GeneralError
+        tmt.export.gssapi = MagicMock
+        tmt.export.gssapi.raw = MagicMock
+        tmt.export.gssapi.raw.misc = MagicMock
+        tmt.export.gssapi.raw.misc.GSSError = tmt.utils.GeneralError
+
+        tmt.export.export_to_nitrate(test, create=False, general=True)
+        assert len(LocalCases.created) == 1
+        case = LocalCases.created[0]
+        assert case.tags._data == [['fmf-export']]
+        assert case.summary == "Fancy summary"
+        # TODO - assert case.notes
+        test_after = tmt.Tree(str(tmpdir)).tests(names=['/'])[0]
+
+        assert test._metadata == test_after._metadata
+
 
 def test_find_general_plan():
     # correct first attempt
