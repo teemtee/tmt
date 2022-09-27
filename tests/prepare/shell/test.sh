e99ea8819d2a95d13008ce7ef519a40969b9e356
Lukas Zachar <lzachar@redhat.com>
2022-09-27 18:32:13 +0200

    Squash: Add test for multiple scripts from cmdline prepare

--- tests/prepare/shell/test.sh
+++ tests/prepare/shell/test.sh
@@ -14,6 +14,10 @@ rlJournalStart
         rlRun "tmt run -arv plan -n custom prepare -h shell -s './prepare.sh'" 0 "Prepare using a custom script from cmdline"
     rlPhaseEnd
 
+    rlPhaseStartTest "Multiple Commandline Scripts"
+        rlRun "tmt run -arv plans -n multiple prepare -h shell -s 'touch /tmp/first' -s 'touch /tmp/second'"
+    rlPhaseEnd
+
     rlPhaseStartCleanup
         rlRun "popd"
     rlPhaseEnd
