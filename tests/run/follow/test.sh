teemtee/tmt

commit c105887f3684b0cf5f97300630ab6adc11d2c294

Fix ruff SIM: flake8-simplify errors

tmt/base.py
@@ -502,7 +502,7 @@ def normalize_require(
     if raw_require is None:
         return []
 
-    if isinstance(raw_require, str) or isinstance(raw_require, dict):
+    if isinstance(raw_require, (str, dict)):
         return [dependency_factory(raw_require)]
 
     if isinstance(raw_require, list):
@@ -690,10 +690,7 @@ class Core(
 
         # Detect relative path of the last source from the metadata tree root
         relative_path = Path(self.node.sources[-1]).relative_to(self.node.root)
-        if str(relative_path) == '.':
-            relative_path = Path('/')
-        else:
-            relative_path = Path('/') / relative_path
+        relative_path = Path('/') if str(relative_path) == '.' else Path('/') / relative_path
 
         # Add fmf path if the tree is nested deeper in the git repo
         if fmf_id.path:
@@ -774,7 +771,7 @@ class Core(
     def _lint_keys(self, additional_keys: List[str]) -> List[str]:
         """ Return list of invalid keys used, empty when all good """
         known_keys = additional_keys + self._keys()
-        return [key for key in self.node.get().keys() if key not in known_keys]
+        return [key for key in self.node.get() if key not in known_keys]
 
     def lint_validate(self) -> LinterReturn:
         """ C000: fmf node should pass the schema validation """
@@ -1021,10 +1018,7 @@ class Test(
         try:
             directory = Path(node.sources[-1]).parent
             relative_path = directory.relative_to(Path(node.root))
-            if relative_path == Path('.'):
-                default_path = Path('/')
-            else:
-                default_path = Path('/') / relative_path
+            default_path = Path('/') if relative_path == Path('.') else Path('/') / relative_path
         except (AttributeError, IndexError):
             default_path = Path('/')
 
@@ -1057,7 +1051,8 @@ class Test(
                     dependency.to_minimal_spec() for dependency in cast(List[Dependency], value)
                     ]
 
-            elif key == 'test' and isinstance(value, ShellScript):
+            # Combining `if` branches using `or` here would result in long, complex line.
+            elif key == 'test' and isinstance(value, ShellScript):  # noqa: SIM114
                 data[key] = str(value)
 
             elif key == 'path' and isinstance(value, Path):
@@ -2914,20 +2909,20 @@ class Run(tmt.utils.Common):
     def follow(self) -> None:
         """ Periodically check for new lines in the log. """
         assert self.workdir is not None  # narrow type
-        logfile = open(self.workdir / tmt.log.LOG_FILENAME)
-        # Move to the end of the file
-        logfile.seek(0, os.SEEK_END)
-        # Rewind some lines back to show more context
-        location = logfile.tell()
-        read_lines = 0
-        while location >= 0:
-            logfile.seek(location)
-            location -= 1
-            current_char = logfile.read(1)
-            if current_char == '\n':
-                read_lines += 1
-            if read_lines > FOLLOW_LINES:
-                break
+        with open(self.workdir / tmt.log.LOG_FILENAME) as logfile:
+            # Move to the end of the file
+            logfile.seek(0, os.SEEK_END)
+            # Rewind some lines back to show more context
+            location = logfile.tell()
+            read_lines = 0
+            while location >= 0:
+                logfile.seek(location)
+                location -= 1
+                current_char = logfile.read(1)
+                if current_char == '\n':
+                    read_lines += 1
+                if read_lines > FOLLOW_LINES:
+                    break
 
         while True:
             line = logfile.readline()
@@ -3212,30 +3207,30 @@ class Clean(tmt.utils.Common):
         # Clean guests if provision is done but finish is not done
         successful = True
         for plan in run.plans:
-            if plan.provision.status() == 'done':
-                if plan.finish.status() != 'done':
-                    # Wake up provision to load the active guests
-                    plan.provision.wake()
-                    if not self._matches_how(plan):
-                        continue
-                    if self.opt('dry'):
-                        self.verbose(
-                            f"Would stop guests in run '{run.workdir}'"
-                            f" plan '{plan.name}'.", shift=1)
-                    else:
-                        self.verbose(f"Stopping guests in run '{run.workdir}' "
-                                     f"plan '{plan.name}'.", shift=1)
-                        # Set --quiet to avoid finish logging to terminal
-                        quiet = self._cli_options['quiet']
-                        self._cli_options['quiet'] = True
-                        try:
-                            plan.finish.go()
-                        except tmt.utils.GeneralError as error:
-                            self.warn(f"Could not stop guest in run "
-                                      f"'{run.workdir}': {error}.", shift=1)
-                            successful = False
-                        finally:
-                            self._cli_options['quiet'] = quiet
+            if plan.provision.status() == 'done' and plan.finish.status() != 'done':
+                # Wake up provision to load the active guests
+                plan.provision.wake()
+                if not self._matches_how(plan):
+                    continue
+                if self.opt('dry'):
+                    self.verbose(
+                        f"Would stop guests in run '{run.workdir}'"
+                        f" plan '{plan.name}'.", shift=1)
+                else:
+                    self.verbose(f"Stopping guests in run '{run.workdir}' "
+                                 f"plan '{plan.name}'.", shift=1)
+                    # Set --quiet to avoid finish logging to terminal
+
+                    quiet = self._cli_options['quiet']
+                    self._cli_options['quiet'] = True
+                    try:
+                        plan.finish.go()
+                    except tmt.utils.GeneralError as error:
+                        self.warn(f"Could not stop guest in run "
+                                  f"'{run.workdir}': {error}.", shift=1)
+                        successful = False
+                    finally:
+                        self._cli_options['quiet'] = quiet
         return successful
 
     def guests(self) -> bool:

pyproject.toml
@@ -67,7 +67,7 @@ select = [
 #    "ISC",  # flake8-implicit-str-concat
     "PT",   # flake8-pytest-style
     "RET",  # flake8-return
-#    "SIM",  # flake8-simplify
+    "SIM",  # flake8-simplify
 #    "ARG",  # flake8-unused-arguments
 #    "BLE",  # flake8-blind-except
 #    "FBT",  # flake8-boolean-trap
@@ -100,6 +100,7 @@ select = [
 ignore = [
     "B904",  # Within an `except` clause, raise exceptions with `raise ... from err` or `raise ...
     "COM812",  # Trailing comma missing
+    "SIM222",  # expr-or-True charliermarsh/ruff#4058
 ]
 
 [tool.ruff.per-file-ignores]

tests/unit/test_cli.py
@@ -241,8 +241,8 @@ def test_decide_colorization(
     monkeypatch.delenv('TMT_NO_COLOR', raising=False)
     monkeypatch.delenv('TMT_FORCE_COLOR', raising=False)
 
-    no_color = True if testcase.set_no_color_option else False
-    force_color = True if testcase.set_force_color_option else False
+    no_color = bool(testcase.set_no_color_option)
+    force_color = bool(testcase.set_force_color_option)
 
     if testcase.set_no_color_envvar:
         monkeypatch.setenv('NO_COLOR', '')

tests/unit/test_logging.py
@@ -19,11 +19,7 @@ def _exercise_logger(
         reset: bool = True) -> None:
     labels = labels or []
 
-    if labels:
-        prefix = render_labels(labels) + indent_by + ' '
-
-    else:
-        prefix = indent_by
+    prefix = render_labels(labels) + indent_by + ' ' if labels else indent_by
 
     if reset:
         caplog.clear()

tests/unit/test_report_junit.py
@@ -116,10 +116,9 @@ def _compare_xml_node(tree_path: List[str], expected: xml.dom.Node, actual: xml.
 
 
 def assert_xml(actual_filepath, expected):
-    with open(actual_filepath) as f:
-        with xml.dom.minidom.parse(f) as actual_dom:
-            with xml.dom.minidom.parseString(expected) as expected_dom:
-                assert _compare_xml_node([expected_dom.nodeName], expected_dom, actual_dom)
+    with open(actual_filepath) as f, xml.dom.minidom.parse(f) as actual_dom, \
+            xml.dom.minidom.parseString(expected) as expected_dom:
+        assert _compare_xml_node([expected_dom.nodeName], expected_dom, actual_dom)
 
 
 @pytest.mark.skipif(pytest.__version__.startswith('3'),

tests/unit/test_utils.py
@@ -661,10 +661,7 @@ class TestValidateGitStatus:
 
         # In local repo:
         # Init tmt and add test
-        if use_path:
-            fmf_root = mine / 'fmf_root'
-        else:
-            fmf_root = mine
+        fmf_root = mine / 'fmf_root' if use_path else mine
         tmt.Tree.init(logger=root_logger, path=fmf_root, template=None, force=None)
         fmf_root.joinpath('main.fmf').write_text('test: echo')
         run(ShellScript(f'git add {fmf_root} {fmf_root / "main.fmf"}').to_shell_command(),
@@ -733,10 +730,7 @@ class TestValidateGitStatus:
             root_logger):
         origin, mine = origin_and_local_git_repo
 
-        if use_path:
-            fmf_root = origin / 'fmf_root'
-        else:
-            fmf_root = origin
+        fmf_root = origin / 'fmf_root' if use_path else origin
         tmt.Tree.init(logger=root_logger, path=fmf_root, template=None, force=None)
         fmf_root.joinpath('main.fmf').write_text('test: echo')
         run(ShellScript('git add -A').to_shell_command(), cwd=origin)

tmt/cli.py
@@ -1723,7 +1723,8 @@ def setup_completion(shell: str, install: bool) -> None:
     else:
         script = Path(config.path) / f'{COMPLETE_SCRIPT}.{shell}'
 
-    out = open(script, 'w') if install else sys.stdout
+    # SIM115: Use context handler for opening files. Would not reduce complexity here.
+    out = open(script, 'w') if install else sys.stdout  # noqa: SIM115
     subprocess.run(f'{COMPLETE_VARIABLE}={shell}_source tmt',
                    shell=True, stdout=out)
 

tmt/convert.py
@@ -6,6 +6,7 @@ import pprint
 import re
 import shlex
 import subprocess
+from contextlib import suppress
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
 from uuid import UUID, uuid4
 
@@ -628,9 +629,8 @@ def filter_common_data(
     if len(individual_data) > 1:
         for testcase in individual_data[1:]:
             for key, value in testcase.items():
-                if key in common_candidates:
-                    if value != common_candidates[key]:
-                        common_candidates.pop(key)
+                if key in common_candidates and value != common_candidates[key]:
+                    common_candidates.pop(key)
                 if key in histogram:
                     histogram[key] += 1
 
@@ -858,7 +858,7 @@ def read_polarion_case(
         echo(style('description: ', fg='green') + current_data['description'])
 
     # Update status
-    status = True if polarion_case.status == 'approved' else False
+    status = polarion_case.status == 'approved'
     if not current_data.get('enabled') or current_data['enabled'] != status:
         current_data['enabled'] = status
         echo(style('enabled: ', fg='green') + str(current_data['enabled']))
@@ -1116,10 +1116,9 @@ def write(path: Path, data: NitrateDataType, quiet: bool = False) -> None:
         'extra-hardware', 'extra-pepa']
     sorted_data = {}
     for key in tmt.base.Test._keys() + extra_keys:
-        try:
+        with suppress(KeyError):
             sorted_data[key] = data[key]
-        except KeyError:
-            pass
+
     # Store metadata into a fmf file
     try:
         with open(path, 'w', encoding='utf-8') as fmf_file:

commit 0d17d1e29f8895b5931d4cd6fb20def700b6f256

Fix `tmt run --follow`, add test coverage for it
Wrong indent in `Run.follow()` introduced in c105887f caused the
`--follow` option to trigger a traceback. Fix the indent and add
basic test coverage to prevent similar regressions in the future.

tmt/base.py
@@ -3102,12 +3102,12 @@ class Run(tmt.utils.Common):
                 if read_lines > FOLLOW_LINES:
                     break
 
-        while True:
-            line = logfile.readline()
-            if line:
-                print(line, end='')
-            else:
-                time.sleep(0.5)
+            while True:
+                line = logfile.readline()
+                if line:
+                    print(line, end='')
+                else:
+                    time.sleep(0.5)
 
     def go(self) -> None:
         """ Go and do test steps for selected plans """

tests/run/follow/data/.fmf/version
@@ -1,0 +1,1 @@
+1

tests/run/follow/data/main.fmf
@@ -1,0 +1,10 @@
+/plan:
+    discover:
+        how: fmf
+    provision:
+        how: local
+    execute:
+        how: tmt
+
+/test:
+    test: for i in {00..05}; do echo step-$i; sleep 1; done

tests/run/follow/main.fmf
@@ -1,0 +1,1 @@
+summary: Ensure that following debug log works

tests/run/follow/test.sh
@@ -1,0 +1,25 @@
+#!/bin/bash
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "run=\$(mktemp -d)" 0 "Create run directory"
+        rlRun "pushd data"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "tmt run --id $run &>/dev/null &" 0 "Start a tmt run in background"
+        rlRun "sleep 1" 0 "Ignore logging during the first second"
+        rlRun -s "timeout -s INT 3 tmt run --id $run --follow" 124 "Follow logs for 3 seconds"
+        rlAssertGrep "step-01" $rlRun_LOG
+        rlAssertGrep "step-02" $rlRun_LOG
+        rlAssertNotGrep "step-04" $rlRun_LOG
+        rlAssertNotGrep "step-05" $rlRun_LOG
+        rlRun "wait $!" 0 "Wait until the run is finished"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $run" 0 "Remove run directory"
+    rlPhaseEnd
+rlJournalEnd