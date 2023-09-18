c3d77f86bf0cb85590a72b495dbf5d129936449d
Miloš Prchlík <mprchlik@redhat.com>
2023-09-18 18:34:55 +0200

    Hide command event debug logs behind a log topic (#2281)
    These were useful when timeouts did not work as expected, but they are
    not needed in general, therefore hiding them behind a log topic to make
    the log less noisy.

--- tests/execute/duration/test.sh
+++ tests/execute/duration/test.sh
@@ -10,17 +10,17 @@ rlJournalStart
     for execute_method in tmt; do
         for provision_method in ${PROVISION_METHODS:-local container}; do
             rlPhaseStartTest "Test provision $provision_method, execute $execute_method, short tests"
-                rlRun -s "tmt run --scratch -vfi $tmp -a provision -h $provision_method execute -h $execute_method test --name short" 0
+                rlRun -s "tmt --log-topic=command-events run --scratch -vfi $tmp -a provision -h $provision_method execute -h $execute_method test --name short" 0
 
                 rlRun "grep 'duration \"5\" exceeded' $tmp/log.txt" 1
             rlPhaseEnd
 
             rlPhaseStartTest "Test provision $provision_method, execute $execute_method, long tests"
-                rlRun -s "tmt run --scratch -vfi $tmp -a provision -h $provision_method execute -h $execute_method test --name long 2>&1" 2
+                rlRun -s "tmt --log-topic=command-events run --scratch -vfi $tmp -a provision -h $provision_method execute -h $execute_method test --name long 2>&1" 2
                 rlAssertNotGrep "00:02:.. errr /test/long/beakerlib (timeout)" $rlRun_LOG
                 rlAssertNotGrep "00:02:.. errr /test/long/shell (timeout)" $rlRun_LOG
 
-                rlRun -s "tmt run --last report -fvvvv 2>&1" 2
+                rlRun -s "tmt --log-topic=command-events run --last report -fvvvv 2>&1" 2
                 rlAssertGrep "Maximum test time '5s' exceeded." $rlRun_LOG
                 rlAssertGrep "Adjust the test 'duration' attribute" $rlRun_LOG
                 rlAssertGrep "spec/tests.html#duration" $rlRun_LOG
