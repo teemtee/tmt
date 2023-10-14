09e47a16d261e47c6b006e96fffdfb40f3c448bf
Vector Li <huanli@redhat.com>
2023-10-14 19:45:24 +0800

    Reorg class EPEL and its subclasses
    XXX: Please don't review as the tests are not ready, thanks!
    
    Signed-off-by: Vector Li <huanli@redhat.com>

--- tmt/steps/prepare/feature.py
+++ tmt/steps/prepare/feature.py
@@ -17,12 +17,12 @@ from tmt.utils import ShellScript, field
 
 class Distro(enum.Enum):
     FEDORA = enum.auto()
-    RHEL_7 = enum.auto()
-    RHEL_8 = enum.auto()
-    RHEL_9 = enum.auto()
     CENTOS_7 = enum.auto()
     CENTOS_STREAM_8 = enum.auto()
     CENTOS_STREAM_9 = enum.auto()
+    RHEL_7 = enum.auto()
+    RHEL_8 = enum.auto()
+    RHEL_9 = enum.auto()
 
 
 class Feature(tmt.utils.Common):
@@ -89,6 +89,7 @@ class Feature(tmt.utils.Common):
 
 FEDORA_EPEL_PACKAGE = 'epel-release'
 FEDORA_EPEL_NEXT_PACKAGE = 'epel-next-release'
+YUM_UTILS_PACKAGE = 'yum-utils'
 RHEL_7_EPEL_PACKAGE = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
 RHEL_8_EPEL_PACKAGE = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm'
 RHEL_9_EPEL_PACKAGE = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm'
@@ -111,167 +112,271 @@ class ToggleableFeature(Feature):
     def disable(self) -> None:
         raise NotImplementedError
 
-    def get_epel_packages(self) -> Optional[str]:
-        if self.guest_distro in (Distro.FEDORA,
-                                 Distro.CENTOS_STREAM_8,
-                                 Distro.CENTOS_STREAM_9):
-            return ' '.join([FEDORA_EPEL_PACKAGE, FEDORA_EPEL_NEXT_PACKAGE])
-        if self.guest_distro == Distro.CENTOS_7:
-            return FEDORA_EPEL_PACKAGE
-        if self.guest_distro == Distro.RHEL_7:
-            return RHEL_7_EPEL_PACKAGE
-        if self.guest_distro == Distro.RHEL_8:
-            return ' '.join([RHEL_8_EPEL_PACKAGE, RHEL_8_EPEL_NEXT_PACKAGE])
-        if self.guest_distro == Distro.RHEL_9:
-            return ' '.join([RHEL_9_EPEL_PACKAGE, RHEL_9_EPEL_NEXT_PACKAGE])
-        return None
-
-    def get_epel_repos(self) -> Optional[str]:
-        if self.guest_distro in (Distro.CENTOS_7,
-                                 Distro.RHEL_7):
-            return ' '.join(EPEL_REPOS)
-        if self.guest_distro in (Distro.CENTOS_STREAM_8,
-                                 Distro.CENTOS_STREAM_9,
-                                 Distro.RHEL_8,
-                                 Distro.RHEL_9):
-            return ' '.join(EPEL_REPOS + EPEL_NEXT_REPOS)
-        return None
+    def get_guest_repos(self) -> Optional[List[str]]:
+        """ Get all repos on the guest """
+        sudo = self.guest_sudo
+        result = self.guest.execute(
+            ShellScript(f"{sudo} cat /etc/yum.repos.d/*.repo"),
+            silent=True)
+        if result is None or result.stdout is None:
+            return None
+        repo_text = result.stdout
+        config = configparser.ConfigParser()
+        config.read_string(repo_text)
+        return config.sections()
 
 
 class EPEL(ToggleableFeature):
     KEY = 'epel'
 
-    def _get_repos_status(self, packages: List[str], repos: List[str]) -> Optional[Dict[str, str]]:
-        """
-        Get status of repos for CentOS Stream 8/9, RHEL 8/9 , CentOS 7 and RHEL 7
-        """
+    def get_repos_status(self, repos: List[str]) -> Optional[Dict[str, str]]:
+        """ Get status of repos related to 'epel' """
         sudo = self.guest_sudo
-
-        result = self.guest.execute(ShellScript(f"{sudo} rpm -ql {' '.join(packages)}"),
-                                    silent=True)
-        if result is None or result.stdout is None:
-            return None
-
-        repo_files = re.findall(r'/.*epel.*\.repo', result.stdout)
-        if not repo_files:
-            return None
-
-        result = self.guest.execute(ShellScript(f"{sudo} cat {' '.join(repo_files)}"),
-                                    silent=True)
+        result = self.guest.execute(
+            ShellScript(f"{sudo} cat /etc/yum.repos.d/*.repo"),
+            silent=True)
         if result is None or result.stdout is None:
             return None
 
         repo_text = result.stdout
         config = configparser.ConfigParser()
         config.read_string(repo_text)
-        repo_status: Dict[str, str] = {}
+        repos_status: Dict[str, str] = {}
         for repo in repos:
             if config.has_option(repo, 'enabled'):
                 option_value = config.get(repo, 'enabled')
-                repo_status[repo] = 'disabled' if int(option_value) == 0 else 'enabled'
-        return repo_status
+                repos_status[repo] = 'disabled' if int(option_value) == 0 else 'enabled'
+        return repos_status
 
-    def _check_repos_status(self, repos: List[str], status: str) -> None:
-        packages = ['epel-release', 'epel-next-release']
-        if self.guest_distro in (Distro.CENTOS_7, Distro.RHEL_7):
-            packages = ['epel-release']
-
-        repos_status = self._get_repos_status(packages, repos)
+    def check_repos_status(self, repos: List[str], status: str) -> None:
+        repos_status = self.get_repos_status(repos)
         if repos_status is None:
             raise tmt.utils.GeneralError(f"Failed to get status of repos: {' '.join(repos)}.")
         for repo in repos:
             repo_status = repos_status.get(repo, None)
             if repo_status != status:
                 raise tmt.utils.GeneralError(f"Repo {repo} is not {status} but {repo_status}.")
-            self.info(f"Repo {repo} is {status}")
+            self.info('Repo', f"{repo} is {status}.")
+
+    def get_epel_source_packages(self) -> List[str]:
+        """
+        Source packages are more than one type. For instance, on CentOS 9 Stream,
+        the 'epel-release' package is 'epel-release'. But on RHEL 9,
+        the 'epel-release' package is 'https://.../epel-release-latest-9.noarch.rpm'.
+        By default, we don't consider '*.rpm' packages.
+        """
+        return [FEDORA_EPEL_PACKAGE, FEDORA_EPEL_NEXT_PACKAGE]
+
+    def get_epel_target_packages(self) -> List[str]:
+        return [FEDORA_EPEL_PACKAGE, FEDORA_EPEL_NEXT_PACKAGE]
+
+    def get_epel_repos(self) -> List[str]:
+        return EPEL_REPOS + EPEL_NEXT_REPOS
+
+    def get_epel_repos_to_disable(self) -> List[str]:
+        repos_guest = self.get_guest_repos()
+        if repos_guest is None:
+            raise tmt.utils.GeneralError("Failed to get repos on the guest.")
+        repos = self.get_epel_repos()
+        repos2disable = set(repos).intersection(set(repos_guest))
+        return list(repos2disable)
 
     def enable(self) -> None:
-        distro = self.guest_distro
-        sudo = self.guest_sudo
-
-        key_name = self.KEY.upper()
-        distro_name = cast(str, self.guest_distro_name)
-        epel_packages = cast(str, self.get_epel_packages())
-        epel_repos = cast(str, self.get_epel_repos())
-        if distro == Distro.FEDORA:
-            self.info(f"Nothing to do on {distro_name} for {key_name}")
-        elif distro in (Distro.CENTOS_7,
-                        Distro.RHEL_7):
-            self.info(f"Enable {key_name} on {distro_name}")
-            self.guest.execute(
-                ShellScript(f"""
-                            set -x;
-                            rpm -q {epel_packages} || {sudo} yum -y install {epel_packages};
-                            rpm -q yum-utils || {sudo} yum install -y yum-utils;
-                            {sudo} yum-config-manager --enable {epel_repos};
-                            """),
-                silent=True)
-            self._check_repos_status(epel_repos.split(), 'enabled')
-        elif distro == Distro.CENTOS_STREAM_8:
-            self.info(f"Enable {key_name} on {distro_name}")
-            self.guest.execute(
-                ShellScript(f"""
-                            set -x;
-                            rpm -q {epel_packages} || {sudo} dnf -y install {epel_packages};
-                            rpm -q yum-utils || {sudo} yum install -y yum-utils;
-                            {sudo} yum-config-manager --enable {epel_repos};
-                            """),
-                silent=True)
-            self._check_repos_status(epel_repos.split(), 'enabled')
-        elif distro in (Distro.CENTOS_STREAM_9,
-                        Distro.RHEL_8,
-                        Distro.RHEL_9):
-            self.info(f"Enable {key_name} on {distro_name}")
-            self.guest.execute(
-                ShellScript(f"""
-                            set -x;
-                            rpm -q {epel_packages} || {sudo} dnf -y install {epel_packages};
-                            {sudo} dnf config-manager --enable {epel_repos};
-                            """),
-                silent=True)
-            self._check_repos_status(epel_repos.split(), 'enabled')
-        else:
-            self.warn(f"Enable {key_name}: '{distro_name}' of the guest is unsupported.")
+        epel_child = _EPEL_FEATURES.get(cast(Distro, self.guest_distro), None)
+        if epel_child is None:
+            distro_name = cast(str, self.guest_distro_name)
+            self.warn(f"Enable {self.KEY.upper()}: '{distro_name}' of the guest is unsupported.")
+            return
+        feature = epel_child(parent=cast(PrepareFeature, self),
+                             guest=self.guest,
+                             logger=self.logger)
+        feature.enable()
 
     def disable(self) -> None:
         """
-        We just disable the repo because we don't know whether the epel-relase package was
-        installed by user on purpose or not.
+        Just disable the repo because we don't know whether the packages related to epel
+        was installed by user on purpose or not.
         """
+        epel_child = _EPEL_FEATURES.get(cast(Distro, self.guest_distro), None)
+        if epel_child is None:
+            distro_name = cast(str, self.guest_distro_name)
+            self.warn(f"Disable {self.KEY.upper()}: '{distro_name}' of the guest is unsupported.")
+            return
+        feature = epel_child(parent=cast(PrepareFeature, self),
+                             guest=self.guest,
+                             logger=self.logger)
+        feature.disable()
 
-        distro = self.guest_distro
-        sudo = self.guest_sudo
 
-        key_name = self.KEY.upper()
+class FedoraEPEL(EPEL):
+    def enable(self) -> None:
         distro_name = cast(str, self.guest_distro_name)
-        epel_repos = cast(str, self.get_epel_repos())
-        if distro == Distro.FEDORA:
-            self.info(f"Nothing to do on {distro_name} for {key_name}")
-        elif distro in (Distro.RHEL_7,
-                        Distro.CENTOS_7,
-                        Distro.CENTOS_STREAM_8):
-            self.info(f"Disable {key_name} on {distro_name}")
-            self.guest.execute(
-                ShellScript(f"""
-                            set -x;
-                            rpm -q yum-utils || {sudo} yum install -y yum-utils;
-                            {sudo} yum-config-manager --disable {epel_repos}
-                            """),
-                silent=True)
-            self._check_repos_status(epel_repos.split(), 'disabled')
-        elif distro in (Distro.RHEL_8,
-                        Distro.RHEL_9,
-                        Distro.CENTOS_STREAM_9):
-            self.info(f"Disable {key_name} on {distro_name}")
-            self.guest.execute(
-                ShellScript(f"""
-                            set -x;
-                            {sudo} dnf config-manager --disable {epel_repos}
-                            """),
-                silent=True)
-            self._check_repos_status(epel_repos.split(), 'disabled')
-        else:
-            self.warn(f"Disable {key_name}: '{distro_name}' of the guest is unsupported.")
+        self.info(f"Enable {self.KEY.upper()}: nothing to do on {distro_name}.")
+
+    def disable(self) -> None:
+        distro_name = cast(str, self.guest_distro_name)
+        self.info(f"Disable {self.KEY.upper()}: nothing to do on {distro_name}.")
+
+
+class CentOS7EPEL(EPEL):
+    def get_epel_source_packages(self) -> List[str]:
+        # XXX: Utility 'yum-config-manager' is provided by package 'yum-utils'
+        return [FEDORA_EPEL_PACKAGE, YUM_UTILS_PACKAGE]
+
+    def get_epel_target_packages(self) -> List[str]:
+        return [FEDORA_EPEL_PACKAGE, YUM_UTILS_PACKAGE]
+
+    def get_epel_repos(self) -> List[str]:
+        return EPEL_REPOS
+
+    def enable(self) -> None:
+        sudo = self.guest_sudo
+        distro_name = cast(str, self.guest_distro_name)
+        source_packages = self.get_epel_source_packages()
+        target_packages = self.get_epel_target_packages()
+        repos = self.get_epel_repos()
+
+        self.info(f"Enable {self.KEY.upper()} on {distro_name}")
+        self.guest.execute(
+            ShellScript(f"""
+                        set -x;
+                        rpm -q {' '.join(source_packages)} || \
+                            {sudo} yum -y install {' '.join(target_packages)};
+                        {sudo} yum-config-manager --enable {' '.join(repos)}
+                        """),
+            silent=True)
+        self.check_repos_status(repos, 'enabled')
+
+    def disable(self) -> None:
+        sudo = self.guest_sudo
+        distro_name = cast(str, self.guest_distro_name)
+        package = YUM_UTILS_PACKAGE
+
+        repos = self.get_epel_repos_to_disable()
+        if not repos:
+            self.info(f"Disable {self.KEY.upper()} on {distro_name}: nothing to do!")
+            return
+
+        self.info(f"Disable {self.KEY.upper()} on {distro_name}")
+        self.guest.execute(
+            ShellScript(f"""
+                        set -x;
+                        rpm -q {package} || {sudo} yum -y install {package};
+                        {sudo} yum-config-manager --disable {' '.join(repos)}
+                        """),
+            silent=True)
+        self.check_repos_status(repos, 'disabled')
+
+
+class RHEL7EPEL(CentOS7EPEL):
+    def get_epel_source_packages(self) -> List[str]:
+        return [RHEL_7_EPEL_PACKAGE, YUM_UTILS_PACKAGE]
+
+
+class C8SEPEL(EPEL):
+    def get_epel_source_packages(self) -> List[str]:
+        # XXX: Some CentOS 8 Stream or RHEL 8 distros don't support 'dnf config-manager'. Hence,
+        #      we have to use 'yum-config-manager' which is provided by package 'yum-utils'.
+        return [FEDORA_EPEL_PACKAGE, FEDORA_EPEL_NEXT_PACKAGE, YUM_UTILS_PACKAGE]
+
+    def get_epel_target_packages(self) -> List[str]:
+        return [FEDORA_EPEL_PACKAGE, FEDORA_EPEL_NEXT_PACKAGE, YUM_UTILS_PACKAGE]
+
+    def enable(self) -> None:
+        sudo = self.guest_sudo
+        distro_name = cast(str, self.guest_distro_name)
+        target_packages = self.get_epel_target_packages()
+        source_packages = self.get_epel_source_packages()
+        repos = self.get_epel_repos()
+
+        self.info(f"Enable {self.KEY.upper()} on {distro_name}")
+        self.guest.execute(
+            ShellScript(f"""
+                        set -x;
+                        rpm -q {' '.join(target_packages)} || \
+                            {sudo} dnf -y install {' '.join(source_packages)};
+                        {sudo} yum-config-manager --enable {' '.join(repos)};
+                        """),
+            silent=True)
+        self.check_repos_status(repos, 'enabled')
+
+    def disable(self) -> None:
+        sudo = self.guest_sudo
+        distro_name = cast(str, self.guest_distro_name)
+        package = YUM_UTILS_PACKAGE
+
+        repos = self.get_epel_repos_to_disable()
+        if not repos:
+            self.info(f"Disable {self.KEY.upper()} on {distro_name}: nothing to do!")
+            return
+
+        self.info(f"Disable {self.KEY.upper()} on {distro_name}")
+        self.guest.execute(
+            ShellScript(f"""
+                        set -x;
+                        rpm -q {package} || {sudo} dnf -y install {package};
+                        {sudo} yum-config-manager --disable {' '.join(repos)}
+                        """),
+            silent=True)
+        self.check_repos_status(repos, 'disabled')
+
+
+class RHEL8EPEL(C8SEPEL):
+    def get_epel_source_packages(self) -> List[str]:
+        return [RHEL_8_EPEL_PACKAGE, RHEL_8_EPEL_NEXT_PACKAGE, YUM_UTILS_PACKAGE]
+
+
+class C9SEPEL(EPEL):
+    def enable(self) -> None:
+        sudo = self.guest_sudo
+        distro_name = cast(str, self.guest_distro_name)
+        target_packages = self.get_epel_target_packages()
+        source_packages = self.get_epel_source_packages()
+        repos = self.get_epel_repos()
+        self.info(f"Enable {self.KEY.upper()} on {distro_name}")
+        self.guest.execute(
+            ShellScript(f"""
+                        set -x;
+                        rpm -q {' '.join(target_packages)} || \
+                            {sudo} dnf -y install {' '.join(source_packages)};
+                        {sudo} dnf config-manager --enable {' '.join(repos)}
+                        """),
+            silent=True)
+        self.check_repos_status(repos, 'enabled')
+
+    def disable(self) -> None:
+        sudo = self.guest_sudo
+        distro_name = cast(str, self.guest_distro_name)
+
+        repos = self.get_epel_repos_to_disable()
+        if not repos:
+            self.info(f"Disable {self.KEY.upper()} on {distro_name}: nothing to do!")
+            return
+
+        self.info(f"Disable {self.KEY.upper()} on {distro_name}")
+        self.guest.execute(
+            ShellScript(f"""
+                        set -x;
+                        {sudo} dnf config-manager --disable {' '.join(repos)}
+                        """),
+            silent=True)
+        self.check_repos_status(repos, 'disabled')
+
+
+class RHEL9EPEL(C9SEPEL):
+    def get_epel_source_packages(self) -> List[str]:
+        return [RHEL_9_EPEL_PACKAGE, RHEL_9_EPEL_NEXT_PACKAGE]
+
+
+_EPEL_FEATURES = {
+    Distro.FEDORA: FedoraEPEL,
+    Distro.CENTOS_7: CentOS7EPEL,
+    Distro.CENTOS_STREAM_8: C8SEPEL,
+    Distro.CENTOS_STREAM_9: C9SEPEL,
+    Distro.RHEL_7: RHEL7EPEL,
+    Distro.RHEL_8: RHEL8EPEL,
+    Distro.RHEL_9: RHEL9EPEL
+    }
 
 
 class CRB(ToggleableFeature):
@@ -290,7 +395,6 @@ class CRB(ToggleableFeature):
         Ensable CRB on the guest. Note that RHEL8, RHEL9, CentOS Stream 8 and CentOS Stream 9
         are supported.
         """
-
         distro = self.guest_distro
         sudo = self.guest_sudo
 
