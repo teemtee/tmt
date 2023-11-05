84415536171a2dffd3a502a465e351e522b9c24c
Miloš Prchlík <mprchlik@redhat.com>
2023-11-05 21:45:49 +0100

    Fix possible unbound variable after import-under-try
    As reported by Pyright, `tmt` may indeed be unbound should the `import
    tmt.utils` fail with exception that's not `ImportError`. Unlikely, but
    fixing it costs nothing.

--- tmt/__main__.py
+++ tmt/__main__.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def run_cli() -> None:
     """
     Entry point to tmt command.
@@ -16,10 +19,26 @@ def run_cli() -> None:
     except ImportError as error:
         print("Error: tmt package does not seem to be installed")
         raise SystemExit(1) from error
-    # Basic error message for general errors
-    except tmt.utils.GeneralError as error:
-        tmt.utils.show_exception(error)
-        raise SystemExit(2) from error
+    except Exception as error:
+        # ignore[reportUnboundVariable]: linter is right here, `tmt` may be
+        # unbound. In theory, `import tmt.utils` might have raised an exception
+        # that is not `ImportError`, and we might end up touching `tmt.utils`
+        # that's not fully imported. And we cannot `except tmt.utils` either,
+        # as the module does not exist in the global scope yet.
+        #
+        # Silence the linter, but be careful and make sure to report the
+        # possible - although very unlikely - secondary exception. Sounds
+        # pointless, but let's make investigation easier for us.
+        #
+        # ignore[unused-ignore]: mypy does not recognize this issue, and therefore
+        # the waiver seems pointless to it...
+        try:
+            tmt.utils.show_exception(error)  # type: ignore[reportUnboundVariable,unused-ignore]
+            raise SystemExit(2) from error
+
+        except Exception as nested_error:
+            print(f"Error: failed while importing tmt package: {nested_error}", file=sys.stderr)
+            raise SystemExit(2) from nested_error
 
 
 if __name__ == "__main__":
