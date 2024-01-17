31a4346819c81b927161e6193bb57e1e913d1c95
Miloš Prchlík <mprchlik@redhat.com>
2024-01-17 11:50:25 +0100

    Improve imported plan modification test to verify the order as well

--- tests/plan/import/modify.sh
+++ tests/plan/import/modify.sh
@@ -9,22 +9,32 @@ rlJournalStart
         rlRun "pushd modify-data"
     rlPhaseEnd
 
-    rlPhaseStartTest "Show imported plan (invalid unless modified)"
-        rlRun -s "tmt plan show ${imported}"
-        rlAssertGrep 'test "${VARIABLE}" == "foobar"' $rlRun_LOG
+    rlPhaseStartTest "Show imported plan"
+        rlRun -s "tmt plan show /plans/must-be-imported-and-modified"
+
+        rlAssertGrep "environment VARIABLE: foo" $rlRun_LOG
     rlPhaseEnd
 
-    rlPhaseStartTest "Show importing plan (modify imported plan)"
-        rlRun -s "tmt plan show ${importing}"
-        rlAssertGrep 'test "foobar" == "foobar"' $rlRun_LOG
-        rlAssertGrep 'environment VARIABLE: foobar' $rlRun_LOG
+    rlPhaseStartTest "Show importing plan"
+        rlRun -s "tmt plan show /importing/and-dont-modify" 0 "Show importing plan without modifications"
+        rlAssertGrep "environment VARIABLE: foo" $rlRun_LOG
+
+        rlRun -s "tmt plan show /importing/and-modify" 0 "Show importing plan with modification via fmf"
+        rlAssertGrep "environment VARIABLE: bar" $rlRun_LOG
+
+        rlRun -s "tmt plan show --environment VARIABLE=baz /importing/and-modify" 0 "Show importing plan with modification via CLI"
+        rlAssertGrep "environment VARIABLE: baz" $rlRun_LOG
     rlPhaseEnd
 
-    rlPhaseStartTest "Run plan ${importing}"
-        rlRun -s "tmt run -dddvv plan --name ${importing}" 0 "Run plan ${importing}"
-        rlAssertGrep 'cmd: test "foobar" == "foobar"' $rlRun_LOG
-        rlAssertGrep "summary: 1 test passed" $rlRun_LOG
-        rlAssertGrep "VARIABLE: foobar" $rlRun_LOG
+    rlPhaseStartTest "Run plan importing plan"
+        rlRun -s "tmt run -vvv plan --name /importing/and-dont-modify" 0 "Run importing plan without modifications"
+        rlAssertGrep 'out: VARIABLE=foo' $rlRun_LOG
+
+        rlRun -s "tmt run -vvv plan --name /importing/and-modify" 0 "Run importing plan with modifications via fmf"
+        rlAssertGrep 'out: VARIABLE=bar' $rlRun_LOG
+
+        rlRun -s "tmt run -vvv --environment VARIABLE=baz plan --name /importing/and-modify" 0 "Run importing plan with modifications via CLI"
+        rlAssertGrep 'out: VARIABLE=baz' $rlRun_LOG
     rlPhaseEnd
 
     rlPhaseStartCleanup
