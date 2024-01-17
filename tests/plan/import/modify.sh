20b90947a68aa76a8e1b3afa9ff8c57550a7dfc3
Miloš Prchlík <mprchlik@redhat.com>
2024-01-17 15:17:52 +0100

    squash: add more cases

--- tests/plan/import/modify.sh
+++ tests/plan/import/modify.sh
@@ -17,66 +17,102 @@ rlJournalStart
     rlPhaseStartTest "Show importing plans"
         # Bare imported + trivial importing + no CLI => no variable
         rlRun -s "tmt plan show                            /importing/bare/dont-modify"
-        rlNotAssertGrep "environment VARIABLE: foo" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: bar" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: baz" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: foo" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: bar" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: baz" $rlRun_LOG
 
         # Bare imported + trivial importing + CLI => CLI wins
         rlRun -s "tmt plan show --environment VARIABLE=baz /importing/bare/dont-modify"
-        rlNotAssertGrep "environment VARIABLE: foo" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: bar" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: foo" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: bar" $rlRun_LOG
         rlAssertGrep    "environment VARIABLE: baz" $rlRun_LOG
 
         # Bare imported + enhanced importing + no CLI => importing wins
         rlRun -s "tmt plan show                            /importing/bare/modify"
-        rlNotAssertGrep "environment VARIABLE: foo" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: foo" $rlRun_LOG
         rlAssertGrep    "environment VARIABLE: bar" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: baz" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: baz" $rlRun_LOG
 
         # Bare imported + enhanced importing + CLI => CLI wins
         rlRun -s "tmt plan show --environment VARIABLE=baz /importing/bare/modify"
-        rlNotAssertGrep "environment VARIABLE: foo" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: bar" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: foo" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: bar" $rlRun_LOG
         rlAssertGrep    "environment VARIABLE: baz" $rlRun_LOG
 
         # Enhanced imported + trivial importing + no CLI => imported wins
         rlRun -s "tmt plan show                            /importing/with-environment/dont-modify"
         rlAssertGrep    "environment VARIABLE: foo" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: bar" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: baz" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: bar" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: baz" $rlRun_LOG
 
         # Enhanced imported + trivial importing + CLI => CLI wins
         rlRun -s "tmt plan show --environment VARIABLE=baz /importing/with-environment/dont-modify"
-        rlNotAssertGrep "environment VARIABLE: foo" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: bar" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: foo" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: bar" $rlRun_LOG
         rlAssertGrep    "environment VARIABLE: baz" $rlRun_LOG
 
         # Enhanced imported + enhanced importing + no CLI => importing wins
         rlRun -s "tmt plan show                            /importing/with-environment/modify"
-        rlNotAssertGrep "environment VARIABLE: foo" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: foo" $rlRun_LOG
         rlAssertGrep    "environment VARIABLE: bar" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: baz" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: baz" $rlRun_LOG
 
         # Enhanced imported + enhanced importing + CLI => CLI wins
         rlRun -s "tmt plan show --environment VARIABLE=baz /importing/with-environment/modify"
-        rlNotAssertGrep "environment VARIABLE: foo" $rlRun_LOG
-        rlNotAssertGrep "environment VARIABLE: bar" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: foo" $rlRun_LOG
+        rlAssertNotGrep "environment VARIABLE: bar" $rlRun_LOG
         rlAssertGrep    "environment VARIABLE: baz" $rlRun_LOG
     rlPhaseEnd
 
-#    rlPhaseStartTest "Run plan importing plan"
-#        rlRun -s "tmt run -vvv plan --name /importing/and-dont-modify" 0 "Run importing plan without modifications"
-#        rlAssertGrep 'out: VARIABLE=foo' $rlRun_LOG
+    rlPhaseStartTest "Run importing plans"
+        # Bare imported + trivial importing + no CLI => no variable
+        rlRun -s "tmt run -vvv                            plan --name /importing/bare/dont-modify" 1
+        rlAssertNotGrep "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=baz" $rlRun_LOG
 
-#        rlRun -s "tmt run -vvv plan --name /importing/and-modify" 0 "Run importing plan with modifications via fmf"
-#        rlAssertGrep 'out: VARIABLE=bar' $rlRun_LOG
+        # Bare imported + trivial importing + CLI => CLI wins
+        rlRun -s "tmt run -vvv --environment VARIABLE=baz plan --name /importing/bare/dont-modify"
+        rlAssertNotGrep "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertGrep    "out: VARIABLE=baz" $rlRun_LOG
 
-#        rlRun -s "tmt run -vvv --environment VARIABLE=baz plan --name /importing/and-dont-modify" 0 "Run importing plan without modifications but modified via CLI"
-#        rlAssertGrep 'out: VARIABLE=baz' $rlRun_LOG
+        # Bare imported + enhanced importing + no CLI => importing wins
+        rlRun -s "tmt run -vvv                            plan --name /importing/bare/modify"
+        rlAssertNotGrep "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertGrep    "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=baz" $rlRun_LOG
 
-#        rlRun -s "tmt run -vvv --environment VARIABLE=baz plan --name /importing/and-modify" 0 "Run importing plan with modifications via CLI"
-#        rlAssertGrep 'out: VARIABLE=baz' $rlRun_LOG
-#    rlPhaseEnd
+        # Bare imported + enhanced importing + CLI => CLI wins
+        rlRun -s "tmt run -vvv --environment VARIABLE=baz plan --name /importing/bare/modify"
+        rlAssertNotGrep "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertGrep    "out: VARIABLE=baz" $rlRun_LOG
+
+        # Enhanced imported + trivial importing + no CLI => imported wins
+        rlRun -s "tmt run -vvv                            plan --name /importing/with-environment/dont-modify"
+        rlAssertGrep    "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=baz" $rlRun_LOG
+
+        # Enhanced imported + trivial importing + CLI => CLI wins
+        rlRun -s "tmt run -vvv --environment VARIABLE=baz plan --name /importing/with-environment/dont-modify"
+        rlAssertNotGrep "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertGrep    "out: VARIABLE=baz" $rlRun_LOG
+
+        # Enhanced imported + enhanced importing + no CLI => importing wins
+        rlRun -s "tmt run -vvv                            plan --name /importing/with-environment/modify"
+        rlAssertNotGrep "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertGrep    "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=baz" $rlRun_LOG
+
+        # Enhanced imported + enhanced importing + CLI => CLI wins
+        rlRun -s "tmt run -vvv --environment VARIABLE=baz plan --name /importing/with-environment/modify"
+        rlAssertNotGrep "out: VARIABLE=foo" $rlRun_LOG
+        rlAssertNotGrep "out: VARIABLE=bar" $rlRun_LOG
+        rlAssertGrep    "out: VARIABLE=baz" $rlRun_LOG
+    rlPhaseEnd
 
     rlPhaseStartCleanup
         rlRun "popd"
