a007c3c4c9dd67981a2ebed05688a419c286e3b9
Vector Li <huanli@redhat.com>
2024-01-22 22:50:07 +0800

    Reorg epel-enable.yaml and epel-disable.yaml
    Signed-off-by: Vector Li <huanli@redhat.com>

--- tests/prepare/feature/test.sh
+++ tests/prepare/feature/test.sh
@@ -6,39 +6,19 @@ rlJournalStart
         rlRun "pushd data"
     rlPhaseEnd
 
+    # EPEL
     for method in ${PROVISION_METHODS:-"container"}; do
-	# EPEL
-        rlPhaseStartTest "Enable EPEL"
+        rlPhaseStartTest "Enable and disable EPEL"
             rlRun -s "tmt run --all plan --name epel-enable provision --how $method"
-            rlAssertGrep 'Repo epel.*enabled' "$rlRun_LOG"
+            rlRun -s "tmt run --all plan --name epel-disable provision --how $method"
 
+        rlPhaseEnd
+
+        rlPhaseStartTest "Enable and disable EPEL with '--how feature' + '--epel'"
             rlRun -s "tmt run --all plan --name epel-disable provision " \
                      "--how $method prepare --how feature --epel enabled"
-            rlAssertGrep 'Repo epel.*enabled' "$rlRun_LOG"
-        rlPhaseEnd
-
-        rlPhaseStartTest "Disable EPEL"
-            rlRun -s "tmt run --all plan --name epel-disable provision --how $method"
-            rlAssertGrep 'Repo epel.*disabled' "$rlRun_LOG"
-
             rlRun -s "tmt run --all plan --name epel-enable provision " \
                      "--how $method prepare --how feature --epel disabled"
-            rlAssertGrep 'Repo epel.*disabled' "$rlRun_LOG"
-        rlPhaseEnd
-
-	# FIPS
-        rlPhaseStartTest "Enable FIPS"
-            rlRun -s "tmt run --all plan --name fips-enable provision --how $method prepare"
-
-            rlRun -s "tmt run --all plan --name fips-disable provision " \
-                     "--how $method prepare --how feature --fips enabled"
-        rlPhaseEnd
-
-        rlPhaseStartTest "Disable FIPS"
-            rlRun -s "tmt run --all plan --name fips-disable provision --how $method prepare"
-
-            rlRun -s "tmt run --all plan --name epel-enable provision " \
-                     "--how $method prepare --how feature --fips disabled"
         rlPhaseEnd
     done
 
