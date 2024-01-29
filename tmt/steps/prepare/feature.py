579037cbaf7cb54c35af86eed3caa3dfd2800e3e
Miloš Prchlík <mprchlik@redhat.com>
2024-01-29 16:28:38 +0800

    squash: some simplification and improvements

--- tmt/steps/prepare/feature.py
+++ tmt/steps/prepare/feature.py
@@ -11,11 +11,13 @@ import tmt.utils
 from tmt.steps.provision import Guest
 from tmt.utils import Path, field
 
+FEATURE_PLAYEBOOK_DIRECTORY = tmt.utils.resource_files('steps/prepare/feature')
+
 
 class Feature(tmt.utils.Common):
     """ Base class for feature implementations """
 
-    KEY: str
+    NAME: str
 
     def __init__(
             self,
@@ -25,15 +27,18 @@ class Feature(tmt.utils.Common):
             logger: tmt.log.Logger) -> None:
         """ Initialize feature data """
         super().__init__(logger=logger, parent=parent, relative_indent=0)
-        self.guest = guest
-        self.logger = logger
 
-    def get_guest_distro_name(self) -> Optional[str]:
-        """ Get guest distro name by parsing the guest facts """
-        os_release = self.guest.facts.os_release_content
-        if os_release is None:
-            return None
-        return os_release.get('PRETTY_NAME', None)
+        self.guest = guest
+
+    def _find_playbook(self, filename: str) -> Optional[Path]:
+        filepath = FEATURE_PLAYEBOOK_DIRECTORY / filename
+
+        if filepath.exists():
+            return filepath
+
+        self.warn(f"Cannot find any suitable playbook for '{filename}'.")
+
+        return None
 
 
 class ToggleableFeature(Feature):
@@ -47,20 +52,22 @@ class ToggleableFeature(Feature):
         assert parent3.my_run.tree.root is not None  # narrow type
         return parent3.my_run.tree.root
 
-    def _enable_or_disable_by_playbook(self, op: str, key: str, playbook: Path) -> None:
-        distro_name = cast(str, self.get_guest_distro_name())
-        if not playbook.exists():
-            self.warn(f"{op} {key}: '{distro_name}' of the guest is unsupported.")
+    def _run_playbook(self, op: str, playbook_filename: str) -> None:
+        playbook_path = self._find_playbook(playbook_filename)
+
+        if not playbook_path:
+            self.warn(f'{op.capitalize()} {self.NAME.upper()} is not supported on this guest.')
             return
-        self.info(f"{op} {key} on '{distro_name}' ...")
-        self.logger.info('playbook', playbook, 'green')
-        self.guest.ansible(playbook.relative_to(self.get_root_path()))
 
-    def enable_by_playbook(self, key: str, playbook: Path) -> None:
-        self._enable_or_disable_by_playbook('Enable', key, playbook)
+        self.info(f'{op.capitalize()} {self.NAME.upper()}')
 
-    def disable_by_playbook(self, key: str, playbook: Path) -> None:
-        self._enable_or_disable_by_playbook('Disable', key, playbook)
+        self.guest.ansible(playbook_path.relative_to(self.get_root_path()))
+
+    def _enable(self, playbook_filename: str) -> None:
+        self._run_playbook('enable', playbook_filename)
+
+    def _disable(self, playbook_filename: str) -> None:
+        self._run_playbook('disable', playbook_filename)
 
     def enable(self) -> None:
         raise NotImplementedError
@@ -70,45 +77,39 @@ class ToggleableFeature(Feature):
 
 
 class EPEL(ToggleableFeature):
-    KEY = 'epel'
+    NAME = 'epel'
 
     def enable(self) -> None:
-        playbook = Path(__file__).parent / 'feature/epel-enable.yaml'
-        self.enable_by_playbook(self.KEY.upper(), playbook)
+        self._enable('epel-enable.yaml')
 
     def disable(self) -> None:
-        playbook = Path(__file__).parent / 'feature/epel-disable.yaml'
-        self.disable_by_playbook(self.KEY.upper(), playbook)
+        self._disable('epel-disable.yaml')
 
 
 class CRB(ToggleableFeature):
-    KEY = 'crb'
+    NAME = 'crb'
 
     def enable(self) -> None:
-        playbook = Path(__file__).parent / 'feature/crb-enable.yaml'
-        self.enable_by_playbook(self.KEY.upper(), playbook)
+        self._enable('crb-enable.yaml')
 
     def disable(self) -> None:
-        playbook = Path(__file__).parent / 'feature/crb-disable.yaml'
-        self.disable_by_playbook(self.KEY.upper(), playbook)
+        self._disable('crb-disable.yaml')
 
 
 class FIPS(ToggleableFeature):
-    KEY = 'fips'
+    NAME = 'fips'
 
     def enable(self) -> None:
-        playbook = Path(__file__).parent / 'feature/fips-enable.yaml'
-        self.enable_by_playbook(self.KEY.upper(), playbook)
+        self._enable('fips-enable.yaml')
 
     def disable(self) -> None:
-        playbook = Path(__file__).parent / 'feature/fips-disable.yaml'
-        self.disable_by_playbook(self.KEY.upper(), playbook)
+        self._disable('fips-disable.yaml')
 
 
-_FEATURES = {
-    EPEL.KEY: EPEL,
-    CRB.KEY: CRB,
-    FIPS.KEY: FIPS
+_FEATURES: dict[str, type[Feature]] = {
+    EPEL.NAME: EPEL,
+    CRB.NAME: CRB,
+    FIPS.NAME: FIPS
     }
 
 
@@ -116,23 +117,23 @@ _FEATURES = {
 class PrepareFeatureData(tmt.steps.prepare.PrepareStepData):
     epel: Optional[str] = field(
         default=None,
-        option=('--epel'),
-        metavar='<enabled|disabled>',
-        help='epel to be enabled or disabled.'
+        option='--epel',
+        metavar='enabled|disabled',
+        help='Whether EPEL repository should be installed & enabled or disabled.'
         )
 
     crb: Optional[str] = field(
         default=None,
-        option=('--crb'),
-        metavar='<enabled|disabled>',
-        help='crb to be enabled or disabled.'
+        option='--crb',
+        metavar='enabled|disabled',
+        help='Whether CRB repository should be enabled or disabled.'
         )
 
     fips: Optional[str] = field(
         default=None,
-        option=('--fips'),
-        metavar='<enabled|disabled>',
-        help='fips to be enabled or disabled.'
+        option='--fips',
+        metavar='enabled|disabled',
+        help='Whether FIPS should be enabled or disabled.'
         )
 
 
@@ -173,28 +174,26 @@ class PrepareFeature(tmt.steps.prepare.PreparePlugin[PrepareFeatureData]):
         if self.opt('dry'):
             return
 
-        # XXX: Currently four provision methods in the following are supported:
-        #      1) connect
-        #      2) virtual
-        #      3) container
-        #      4) local
-        if not isinstance(guest, (tmt.steps.provision.GuestSsh,
-                                  tmt.steps.provision.testcloud.ProvisionTestcloud,
-                                  tmt.steps.provision.podman.GuestContainer,
-                                  tmt.steps.provision.local.GuestLocal)):
-            raise tmt.utils.GeneralError("The provision method is unsupported by this feature.")
-
         # Enable or disable epel/crb/fips
-        for key in _FEATURES:
-            value = getattr(self.data, key, None)
+        for feature_key in _FEATURES:
+            value = cast(Optional[str], getattr(self.data, feature_key, None))
+
             if value is None:
                 continue
 
-            feature = _FEATURES[key](parent=self, guest=guest, logger=logger)
-            value = value.lower() if isinstance(feature, ToggleableFeature) else None
-            if value == 'enabled':
-                feature.enable()
-            elif value == 'disabled':
-                feature.disable()
+            feature = _FEATURES[feature_key](parent=self, guest=guest, logger=logger)
+
+            if isinstance(feature, ToggleableFeature):
+                value = value.lower()
+
+                if value == 'enabled':
+                    feature.enable()
+
+                elif value == 'disabled':
+                    feature.disable()
+
+                else:
+                    raise tmt.utils.GeneralError(f"Unknown feature setting '{value}'.")
+
             else:
-                raise tmt.utils.GeneralError("Unknown method")
+                raise tmt.utils.GeneralError(f"Unsupported feature '{feature_key}'.")
