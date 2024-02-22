9456044ff529a1ed34735d659b6de54dd5946c2b
Petr Šplíchal <psplicha@redhat.com>
2024-02-22 14:42:48 +0100

    squash: Adjust duration, fix no results reported on reboot timeout

--- tmt/steps/execute/internal.py
+++ tmt/steps/execute/internal.py
@@ -420,8 +420,7 @@ class ExecuteInternal(tmt.steps.execute.ExecutePlugin[ExecuteInternalData]):
             extend_options=test.test_framework.get_pull_options(invocation, logger))
 
         # Produce list of results
-        if not invocation.reboot_requested:
-            invocation.results += self.extract_results(invocation, logger)
+        invocation.results = self.extract_results(invocation, logger)
 
         invocation.check_results += self.run_checks_after_test(
             invocation=invocation,
