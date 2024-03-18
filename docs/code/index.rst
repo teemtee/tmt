cfb09de81f33c4bfd4766d59b491a6e2a230a755
Miloš Prchlík <mprchlik@redhat.com>
2024-03-19 00:46:16 +0100

    Document custom Jinja emplate filters
    Includes a small refactoring of man page building to overcome rst2man
    handling of `:ref:` directives.

--- docs/code/index.rst
+++ docs/code/index.rst
@@ -14,6 +14,7 @@ the documentation generated from sources linked below.
     :maxdepth: 2
 
     Class Overview <classes>
+    Template Filters <template-filters>
     Plugin Introduction <plugin-introduction>
     tmt <autodocs/tmt>
 
---
+++ docs/scripts/generate-template-filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import textwrap
+
+from tmt.utils import TEMPLATE_FILTERS, Path, render_template_file
+
+HELP = textwrap.dedent("""
+Usage: generate-template-filters.py <TEMPLATE-PATH> <OUTPUT-PATH>
+
+Generate docs for all known Jinja2 template filters.
+""").strip()
+
+
+def main() -> None:
+    if len(sys.argv) != 3:
+        print(HELP)
+
+        sys.exit(1)
+
+    template_filepath = Path(sys.argv[1])
+    output_filepath = Path(sys.argv[2])
+
+    output_filepath.write_text(render_template_file(template_filepath, TEMPLATES=TEMPLATE_FILTERS))
+
+
+if __name__ == '__main__':
+    main()
