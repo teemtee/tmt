72b5974347b74095fdcb097694512a00ca12dd52
Miloš Prchlík <mprchlik@redhat.com>
2024-03-27 10:09:42 +0100

    squash: fix simplification

--- tmt/hardware.py
+++ tmt/hardware.py
@@ -1401,16 +1401,21 @@ def parse_hw_requirements(spec: Spec) -> BaseConstraint:
 
 
 def simplify_actual_hardware(hardware: BaseConstraint) -> Spec:
-    assert isinstance(hardware, And)
-
     as_spec: Spec = {}
 
-    for constraint in hardware.constraints:
+    def _simplify_constraint(constraint: BaseConstraint) -> Spec:
         constraint_spec = constraint.to_spec()
 
         assert isinstance(constraint_spec, dict)
 
-        as_spec.update(constraint_spec)
+        return cast(dict[str, str], constraint_spec)
+
+    if isinstance(hardware, And):
+        for constraint in hardware.constraints:
+            as_spec.update(_simplify_constraint(constraint))
+
+    else:
+        as_spec.update(_simplify_constraint(hardware))
 
     return as_spec
 
