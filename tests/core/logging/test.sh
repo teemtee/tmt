f3df5d1268397d7a258ff80d00ca23d4470b9744
Miloš Prchlík <mprchlik@redhat.com>
2024-05-06 09:35:23 +0200

    --quiet shall silence warnings
    Related to https://github.com/teemtee/tmt/issues/2106

--- tests/core/logging/test.sh
+++ tests/core/logging/test.sh
@@ -26,17 +26,17 @@ rlJournalStart
 
     # TODO: enable the test once --quiet starts silencing all kinds of logging
     # See https://github.com/teemtee/tmt/issues/2106
-    # rlPhaseStartTest "Verify --quiet suppresses all logging"
-    #     rlRun "pushd data"
-    #
-    #     rlRun -s "tmt test show"
-    #     rlAssertGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
-    #
-    #     rlRun -s "tmt --quiet test show"
-    #     rlAssertNotGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
-    #
-    #     rlRun "popd"
-    # rlPhaseEnd
+    rlPhaseStartTest "Verify --quiet suppresses all logging"
+        rlRun "pushd data"
+
+        rlRun -s "tmt test show"
+        rlAssertGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
+
+        rlRun -s "tmt --quiet test show"
+        rlAssertNotGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
+
+        rlRun "popd"
+    rlPhaseEnd
 
     rlPhaseStartCleanup
     rlPhaseEnd
