ada5245eebca3c9784dd0f497bfb6fda5b47ef1f
Cristian Le <cristian.le@mpsd.mpg.de>
2024-05-24 17:56:56 +0200

    Expand example to include schemas
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- examples/plugins/example/discover.py
+++ examples/plugins/example/discover.py
@@ -1,9 +1,21 @@
+import dataclasses
+
 import tmt
 import tmt.steps
 import tmt.steps.discover
+import tmt.utils
 
 # See the online documentation for more details about writing plugins
 # https://tmt.readthedocs.io/en/stable/plugins.html
+
+
+@dataclasses.dataclass
+class DiscoverExampleData(tmt.steps.discover.DiscoverStepData):
+    path: str = tmt.utils.field(
+        default=".",
+        option=('-p', '--path'),
+        metavar='ROOT',
+        help='Path to the metadata tree root.')
 
 
 @tmt.steps.provides_method('example')
@@ -15,6 +27,8 @@ class DiscoverExample(tmt.steps.discover.DiscoverPlugin):
     in the --help message. It is recommended to include a couple
     of configuration examples as well.
     """
+
+    _data_class = DiscoverExampleData
 
     def show(self):
         """ Show plugin details for given or all available keys """
@@ -48,7 +62,8 @@ class DiscoverExample(tmt.steps.discover.DiscoverPlugin):
         print("Code should prepare environment for tests.")
 
         # Discover available tests
-        self._tests = tmt.Tree(logger=self._logger, path=".").tests()
+        self._tests = tmt.Tree(logger=self._logger,
+                               path=self.data.path).tests()
 
     def tests(self):
         """
