bd484947c3a18919f0d0b067f8f7c3a8d1ae1968
Miloš Prchlík <mprchlik@redhat.com>
2024-06-18 14:21:54 +0200

    Add HW requirement support matrix to docs
    Generated from HW requriement stories and their links, it should be the
    most up-to-date picture of what's supported by plugins shipped with tmt.

---
+++ docs/scripts/generate-hardware-matrix.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import sys
+import textwrap
+
+import tmt.plugins
+import tmt.steps.provision
+from tmt.utils import Path, render_template_file
+
+HELP = textwrap.dedent("""
+Usage: generate-hardware-matrix.py <TEMPLATE-PATH> <OUTPUT-PATH>
+
+Generate page with HW requirement support matrix from stories.
+""").strip()
+
+
+def main() -> None:
+    if len(sys.argv) != 3:
+        print(HELP)
+
+        sys.exit(1)
+
+    template_filepath = Path(sys.argv[1])
+    output_filepath = Path(sys.argv[2])
+
+    # We will need a logger...
+    logger = tmt.Logger.create()
+    logger.add_console_handler()
+
+    # Explore available *export* plugins - do not import other plugins, we don't need them.
+    tmt.plugins._explore_packages(logger)
+
+    known_methods = sorted(
+        tmt.steps.provision.ProvisionPlugin._supported_methods.iter_plugin_ids())
+
+    tree = tmt.Tree(logger=logger, path=Path.cwd())
+
+    logger.info('Generating reST file for HW requirement support matrix')
+
+    matrix: dict[str, dict[str, tuple[bool, str]]] = {}
+    notes: list[str] = []
+
+    for story in tree.stories(logger=logger, names=['^/spec/hardware/.*'], whole=True):
+        hw_requirement = story.name.replace('/spec/hardware/', '')
+
+        if hw_requirement == 'arch':
+            continue
+
+        matrix[hw_requirement] = {
+            method: (False, int) for method in known_methods
+            }
+
+        if not story.link:
+            pass
+
+        for link in story.link.get(relation='implemented-by'):
+            implemented_by_method = Path(link.target).stem
+
+            if implemented_by_method == 'mrack':
+                implemented_by_method = 'beaker'
+
+            elif implemented_by_method == 'testcloud':
+                implemented_by_method = 'virtual.testcloud'
+
+            if implemented_by_method not in matrix[hw_requirement]:
+                raise Exception(f'{implemented_by_method} unknown')
+
+            if link.note:
+                notes.append(f'``{hw_requirement}`` with ``{implemented_by_method}``: {link.note}')
+
+                matrix[hw_requirement][implemented_by_method] = (True, len(notes))
+
+            else:
+                matrix[hw_requirement][implemented_by_method] = (True, None)
+
+    output_filepath.write_text(render_template_file(
+        template_filepath,
+        LOGGER=logger,
+        MATRIX=matrix,
+        NOTES=notes))
+
+
+if __name__ == '__main__':
+    main()
