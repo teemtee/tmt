a36bf3b86fc1f576312f383fb65d7f4e815f2ab0
Ismail Ibrahim Quwarah <iquwarah@redhat.com>
2024-07-02 13:42:36 +0200

    Add `package` prepare plugin using `install` in tmt try

--- tmt/cli.py
+++ tmt/cli.py
@@ -1645,6 +1645,9 @@ def init(
     "--epel", is_flag=True, default=False,
     help="Enable epel repository.")
 @option(
+    "--package", default=[], metavar="PACKAGE", multiple=True,
+    help="Install local rpm package on the guest.")
+@option(
     "-a", "--ask", is_flag=True, default=False,
     help="Just provision the guest and ask what to do next.")
 @verbosity_options
