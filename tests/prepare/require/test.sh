65ae568f427543ea73be612bf886929d8034258b
Lukáš Zachar <1548503+lukaszachy@users.noreply.github.com>
2024-07-02 15:05:07 +0200

    Use vault.centos for centos-7 image repos (#3070)
    mirror/baseurl used in the image are gone now

--- tests/prepare/require/test.sh
+++ tests/prepare/require/test.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 . /usr/share/beakerlib/beakerlib.sh || exit 1
+. ../../images.sh || exit 1
+
+PROVISION_HOW=container
 
 rlJournalStart
     rlPhaseStartSetup
+
+        build_container_image "centos/7/upstream\:latest"
+        build_container_image "fedora/40:\latest"
+
         rlRun "pushd data"
         rlRun "set -o pipefail"
     rlPhaseEnd
 
-    for image in fedora centos:7 ; do
+    for image in $TEST_IMAGE_PREFIX/fedora/40:latest \
+                 $TEST_IMAGE_PREFIX/centos/7/upstream:latest; do
         # Prepare the tmt command and expected error message
-        tmt="tmt run -avr provision -h container -i $image"
-        if [[ $image == fedora ]]; then
-            error='Unable to find a match: forest'
+        tmt="tmt run -avr provision -h $PROVISION_HOW -i $image"
+        if [[ $image =~ fedora ]]; then
+            error='No match for argument: forest'
         else
             error='No package forest available'
         fi
