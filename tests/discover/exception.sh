868adc0eed095bf3b9dd9f95a6b5242d5c813ef1
Miloš Prchlík <mprchlik@redhat.com>
2024-07-09 10:22:54 +0200

    squash: fix test

--- tests/discover/exception.sh
+++ tests/discover/exception.sh
@@ -2,22 +2,23 @@
 
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
+PTTY_WRAPPER="../execute/tty/ptty-wrapper"
+
 rlJournalStart
     rlPhaseStartSetup
-        rlRun "pushd order"
         rlRun "tmp=\$(mktemp -d)" 0 "Creating tmp directory"
     rlPhaseEnd
 
     rlPhaseStartTest "Test exception without option '--no-color'"
-        rlRun -s "tmt run discover -h fmf -u https://127.0.0.1/nonexistent" "1-255"
+        rlRun -s "$PTTY_WRAPPER tmt            run --id $tmp --scratch discover -h fmf -u https://127.0.0.1/nonexistent" "1-255"
         cat -A $rlRun_LOG
-        #rlAssertGrep "^[[31m" $rlRun_LOG
+        rlRun "grep -aP '\e\[31m' $rlRun_LOG"
     rlPhaseEnd
 
     rlPhaseStartTest "Test exception with option '--no-color'"
-        rlRun -s "tmt --no-color run discover -h fmf -u https://127.0.0.1/nonexistent" "1-255"
+        rlRun -s "$PTTY_WRAPPER tmt --no-color run --id $tmp --scratch discover -h fmf -u https://127.0.0.1/nonexistent" "1-255"
         cat -A $rlRun_LOG
-        #rlAssertNotGrep "^[[31m" $rlRun_LOG
+        rlRun "grep -aP '\e\[31m' $rlRun_LOG" 1
     rlPhaseEnd
 
     rlPhaseStartCleanup
