teemtee/tmt

commit e4e9448807527a8b4e201240f1bffa6827e5c6ac

Fix normalization of playbook/playbooks keys of ansible plugin (#3053)

tmt/utils.py
@@ -6158,7 +6158,22 @@ def normalize_string_list(
     if value is None:
         return []
 
-    return [value] if isinstance(value, str) else value
+    if isinstance(value, str):
+        return [value]
+
+    if isinstance(value, (list, tuple)):
+        normalized_value: list[str] = []
+
+        for i, raw_item in enumerate(value):
+            if isinstance(raw_item, str):
+                normalized_value.append(raw_item)
+                continue
+
+            raise NormalizationError(f'{key_address}[{i}]', raw_item, 'a string')
+
+        return normalized_value
+
+    raise NormalizationError(key_address, value, 'a string or a list of strings')
 
 
 def normalize_pattern_list(

tmt/steps/prepare/ansible.py
@@ -11,7 +11,7 @@ import tmt.steps
 import tmt.steps.prepare
 import tmt.utils
 from tmt.steps.provision import Guest
-from tmt.utils import Path, PrepareError, field, retry_session
+from tmt.utils import Path, PrepareError, field, normalize_string_list, retry_session
 
 
 class _RawAnsibleStepData(tmt.steps._RawStepData, total=False):
@@ -52,11 +52,10 @@ class PrepareAnsibleData(tmt.steps.prepare.PrepareStepData):
         super().pre_normalization(raw_data, logger)
 
         # Perform `playbook` normalization here, so we could merge `playbooks` to it.
-        playbook = raw_data.pop('playbook', [])
-        raw_data['playbook'] = [playbook] if isinstance(playbook, str) else playbook
+        playbook = normalize_string_list('playbook', raw_data.pop('playbook', []), logger)
+        playbooks = normalize_string_list('playbook', raw_data.pop('playbooks', []), logger)
 
-        assert isinstance(raw_data['playbook'], list)
-        raw_data['playbook'] += raw_data.pop('playbooks', [])
+        raw_data['playbook'] = [*playbook, *playbooks]
 
 
 @tmt.steps.provides_method('ansible')

commit 69c113b4516fffbda0edda5c10839450976e57ce

Fix normalization of regular expression lists
It got broken by changes introduced in
https://github.com/teemtee/tmt/pull/3053

tmt/utils.py
@@ -6133,7 +6133,7 @@ def normalize_storage_size(
 
 def normalize_string_list(
         key_address: str,
-        value: Union[None, str, list[str]],
+        value: Any,
         logger: tmt.log.Logger) -> list[str]:
     """
     Normalize a string-or-list-of-strings input value.
@@ -6178,7 +6178,7 @@ def normalize_string_list(
 
 def normalize_pattern_list(
         key_address: str,
-        value: Union[None, str, list[str]],
+        value: Any,
         logger: tmt.log.Logger) -> list[Pattern[str]]:
     """
     Normalize a pattern-or-list-of-patterns input value.
@@ -6192,18 +6192,40 @@ def normalize_pattern_list(
          - '(?i)BaZ+'
     """
 
-    normalized_value = normalize_string_list(key_address, value, logger)
+    def _normalize(raw_patterns: list[Any]) -> list[Pattern[str]]:
+        patterns: list[Pattern[str]] = []
 
-    patterns: list[Pattern[str]] = []
+        for i, raw_pattern in enumerate(raw_patterns):
+            if isinstance(raw_pattern, str):
+                try:
+                    patterns.append(re.compile(raw_pattern))
 
-    for i, raw_pattern in enumerate(normalized_value):
-        try:
-            patterns.append(re.compile(raw_pattern))
+                except Exception:
+                    raise NormalizationError(
+                        f'{key_address}[{i}]', raw_pattern, 'a regular expression')
 
-        except Exception:
-            raise NormalizationError(f'{key_address}[{i}]', raw_pattern, 'a regular expression')
+            elif isinstance(raw_pattern, re.Pattern):
+                patterns.append(raw_pattern)
 
-    return patterns
+            else:
+                raise NormalizationError(
+                    f'{key_address}[{i}]', raw_pattern, 'a regular expression')
+
+        return patterns
+
+    if value is None:
+        return []
+
+    if isinstance(value, str):
+        return _normalize([value])
+
+    if isinstance(value, (list, tuple)):
+        return _normalize(list(value))
+
+    raise NormalizationError(
+        key_address,
+        value,
+        'a regular expression or a list of regular expressions')
 
 
 def normalize_integer_list(