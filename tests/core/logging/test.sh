47cc892670b430a1a50eab79550ea75ada7acfb7
Miloš Prchlík <mprchlik@redhat.com>
2024-07-19 12:21:41 +0200

    The `--quiet` option shall silence warnings (#2909)
    Related to https://github.com/teemtee/tmt/issues/2106
    
    Co-authored-by: Petr Šplíchal <psplicha@redhat.com>

--- tests/core/logging/test.sh
+++ tests/core/logging/test.sh
@@ -24,19 +24,17 @@ rlJournalStart
         rlRun "grep -E '^[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2} ' $rlRun_LOG" 0
     rlPhaseEnd
 
-    # TODO: enable the test once --quiet starts silencing all kinds of logging
-    # See https://github.com/teemtee/tmt/issues/2106
-    # rlPhaseStartTest "Verify --quiet suppresses all logging"
-    #     rlRun "pushd data"
-    #
-    #     rlRun -s "tmt test show"
-    #     rlAssertGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
-    #
-    #     rlRun -s "tmt --quiet test show"
-    #     rlAssertNotGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
-    #
-    #     rlRun "popd"
-    # rlPhaseEnd
+    rlPhaseStartTest "Verify --quiet suppresses all logging"
+        rlRun "pushd data"
+
+        rlRun -s "tmt test show"
+        rlAssertGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
+
+        rlRun -s "tmt --quiet test show"
+        rlAssertNotGrep "warn: /tests: - 'non-existent-key' does not match any of the regexes" "$rlRun_LOG"
+
+        rlRun "popd"
+    rlPhaseEnd
 
     rlPhaseStartCleanup
     rlPhaseEnd
