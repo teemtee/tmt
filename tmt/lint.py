9b75f55a7790c45bcf27d4255b394e2703c78000
Miloš Prchlík <mprchlik@redhat.com>
2024-08-02 19:40:56 +0200

    squash: add missing mention in the example

--- tmt/lint.py
+++ tmt/lint.py
@@ -19,9 +19,9 @@ following examples:
    def lint_path_exists(self) -> LinterReturn:
        # A linter must have a docstring which is then used to generate documentation,
        # e.g. when ``lint --list-checks`` is called. The docstring must begin with
-       # a linter *id*. The id should match ``[CTPS]\\d\\d\\d`` regular expression:
-       # ``C``ommon, ``T``est, ``P``lan, ``S``tory, plus a three-digit serial number
-       # of the check among its peers.
+       # a linter *id*. The id should match ``[CTPSG]\\d\\d\\d`` regular expression:
+       # ``C``ommon, ``T``est, ``P``lan, ``S``tory, ``G``roup, plus a three-digit
+       # serial number of the check among its peers.
        ''' T004: test directory path must exist '''
 
        # Linter implements a generator (see :py:member:`LinterReturn`) yielding
