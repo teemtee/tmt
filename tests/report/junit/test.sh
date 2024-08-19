45b1884dae7eccecdc7f97ff3ced4696ee837f14
Otto Sabart <osabart@redhat.com>
2024-08-19 09:00:00 +0200

    Add basic JUnit tests againts limited XSD schema

--- tests/report/junit/test.sh
+++ tests/report/junit/test.sh
@@ -10,9 +10,11 @@ rlJournalStart
     for method in tmt; do
         rlPhaseStartTest "$method"
             rlRun "tmt run -avr execute -h $method report -h junit --file junit.xml 2>&1 >/dev/null | tee output" 2
-            rlAssertGrep "2 tests passed, 2 tests failed and 2 errors" "output"
-            rlAssertGrep '<testsuite disabled="0" errors="2" failures="2" name="/plan" skipped="0" tests="6"' "junit.xml"
+            rlAssertGrep "2 tests passed, 2 tests failed, 2 tests skipped, 2 infos, 2 warns and 2 errors" "output"
+            rlAssertGrep '<testsuite disabled="0" errors="4" failures="2" name="/plan" skipped="2" tests="12"' "junit.xml"
             rlAssertGrep 'fail</failure>' "junit.xml"
+
+            rlRun "xmllint --noout --schema 'tmt-report-junit.xsd' 'junit.xml'" 0 "Checking generated JUnit against the XSD schema"
         rlPhaseEnd
     done
 
