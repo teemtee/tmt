5c4cb424a34fd8c80c30670855366bb420bdafac
Karel Srot <ksrot@redhat.com>
2024-09-03 07:37:38 +0200

    Use TMT_REPORT_ARTIFACTS_URL envvar again
    The former change accidentally switched the code to use
    TMT_PLUGIN_REPORT_REPORTPORTAL_ARTIFACTS_URL
    environment variable. That broke the functionality until a workaround
    had been implemented in TF.
    This change restores the original usage of TMT_REPORT_ARTIFACTS_URL
    due to the fact that this variable is not reportportal plugin
    specific.

--- tmt/steps/report/reportportal.py
+++ tmt/steps/report/reportportal.py
@@ -163,7 +163,7 @@ class ReportReportPortalData(tmt.steps.report.ReportStepData):
     artifacts_url: Optional[str] = field(
         metavar="ARTIFACTS_URL",
         option="--artifacts-url",
-        default=_str_env_to_default('artifacts_url', None),
+        default=os.getenv('TMT_REPORT_ARTIFACTS_URL'),
         help="Link to test artifacts provided for report plugins.")
 
     launch_url: Optional[str] = None
