0699e3a39f277be43b5eb27684cefec1dec91b45
Otto Sabart <osabart@redhat.com>
2024-11-08 14:35:27 +0100

    Fix the tests

--- tests/execute/result/special/beaker-phases-special-names.sh
+++ tests/execute/result/special/beaker-phases-special-names.sh
@@ -3,25 +3,24 @@
 
 
 rlJournalStart
+    # Ensure the regression from #teemtee/tmt/3333 is covered and also cover
+    # some other special characters in the phase name
     for i in \
-        # Ensure the regression from #teemtee/tmt/3333 is covered
         /sbin/ldconfig \
         /usr/sbin/ldconfig \
-
-        # Cover other special characters in the phase name
         '//01_some/-phase//na--me-' \
-        '\$02_so\$me<->phase?!*na--me/-'
-        '[03_some '
-        '- 04!some - '
-        '?05?some?'
-        '!06!some! '
-        '#07#some#'
-        '@08@some@'
-        '&09&some&'
-        '*10&some*'
-        '(11()some)'
-        '+12+some+'
-        ';13;some;'
+        '\$02_so\$me<->phase?!*na--me/-' \
+        '[03_some ' \
+        '- 04!some - ' \
+        '?05?some?' \
+        '!06!some! ' \
+        '#07#some#' \
+        '@08@some@' \
+        '&09&some&' \
+        '*10&some*' \
+        '(11()some)' \
+        '+12+some+' \
+        ';13;some;' \
         '=14=some='
     do
         rlPhaseStartTest "$i"
