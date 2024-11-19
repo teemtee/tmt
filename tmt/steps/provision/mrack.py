a8d2b60cf7c1fcac698f532ec46663aebd3f17a5
Lili Nie <lnie@redhat.com>
2024-11-19 05:55:57 -0500

    squash:update

--- tmt/steps/provision/mrack.py
+++ tmt/steps/provision/mrack.py
@@ -913,14 +913,14 @@ class BeakerGuestData(tmt.steps.provision.GuestSshData):
              Submitting user must be a submission delegate for the ``USERNAME``.
              """)
 
-    public_keys: Optional[list[str]] = field(
-        default=None,
-        option='--public-keys',
-        metavar='PUBLICKEYS',
+    public_key: list[str] = field(
+        default_factory=list,
+        option='--public-key',
+        metavar='PUBLICKEY',
         help="""
-             If set, Beaker jobs will be submitted on behalf of ``USERNAME``.
-             Submitting user must be a submission delegate for the ``USERNAME``.
+             If set, the public keys will be put under ssh authorized_keys.
              """,
+        multiple=True,
         normalize=tmt.utils.normalize_string_list)
 
 
@@ -958,7 +958,7 @@ class CreateJobParameters:
     kickstart: dict[str, str]
     whiteboard: Optional[str]
     beaker_job_owner: Optional[str]
-    public_keys: Optional[list[str]]
+    public_key: list[str]
     group: str = 'linux'
 
     def to_mrack(self) -> dict[str, Any]:
@@ -1090,6 +1090,7 @@ class GuestBeaker(tmt.steps.provision.GuestSsh):
     # Timeouts and deadlines
     provision_timeout: int
     provision_tick: int
+    public_key: list[str]
     api_session_refresh_tick: int
 
     _api: Optional[BeakerAPI] = None
@@ -1159,7 +1160,7 @@ class GuestBeaker(tmt.steps.provision.GuestSsh):
             name=f'{self.image}-{self.arch}',
             whiteboard=self.whiteboard or tmt_name,
             beaker_job_owner=self.beaker_job_owner,
-            public_keys=self.public_keys)
+            public_key=self.public_key)
 
         try:
             response = self.api.create(data)
