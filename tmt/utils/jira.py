fd9cdaa8bf002d44d38d279d23e1b7a4777448ff
Lili Nie <lnie@redhat.com>
2024-12-03 04:31:04 -0500

    Fix `tmt link` handling of links without relation
    By default, the link relation is `relates`, and the link relation
    is not mandatory when calling `tmt link`. But, `tmt link` does not
    accept any relation besides `verifies`, which makes links without
    relation ignored.
    
    Fixes #3385.

--- tmt/utils/jira.py
+++ tmt/utils/jira.py
@@ -196,7 +196,7 @@ def link(
     """
 
     # TODO: Shall we cover all relations instead?
-    for link in links.get("verifies"):
+    for link in (links.get("verifies") or links.get("relates")):
         # Save the link to test/plan/story metadata on disk
         for tmt_object in tmt_objects:
             save_link_to_metadata(tmt_object, link, logger)
