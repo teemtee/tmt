14b64eaa7809eb5f333d292b77a0de8030d6c7de
skycastlelily <lnie@redhat.com>
2024-12-09 15:01:18 +0800

    Translate the `or` constraint properly for `mrack` (#3327)

--- tests/unit/test_hardware.py
+++ tests/unit/test_hardware.py
@@ -241,6 +241,16 @@ FULL_HARDWARE_REQUIREMENTS = """
         mode: "CCA"
 """
 
+OR_HARDWARE_REQUIREMENTS = """
+    or:
+      - hostname: == dummy1.redhat.com
+      - or:
+        - hostname: == dummy2.redhat.com
+        - or:
+          - hostname: == dummy3.redhat.com
+          - hostname: == dummy4.redhat.com
+"""
+
 
 def test_parse_maximal_constraint() -> None:
     hw_spec_out = """
@@ -328,3 +338,19 @@ def test_parse_maximal_constraint() -> None:
     print(textwrap.dedent(hw_spec_out))
 
     assert tmt.utils.dict_to_yaml(hw.constraint.to_spec()) == textwrap.dedent(hw_spec_out).lstrip()
+
+
+def test_parse_or_constraint() -> None:
+    hw_spec_out = """
+     or:
+       - hostname: == dummy1.redhat.com
+       - or:
+           - hostname: == dummy2.redhat.com
+           - or:
+               - hostname: == dummy3.redhat.com
+               - hostname: == dummy4.redhat.com
+    """
+
+    hw = parse_hw(OR_HARDWARE_REQUIREMENTS)
+    assert tmt.utils.dict_to_yaml(
+        hw.constraint.to_spec()) == textwrap.dedent(hw_spec_out).lstrip()
