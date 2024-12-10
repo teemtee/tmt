733fddfe21018722d5b3104b596362cd91507a23
mcasquer <mcasquer@redhat.com>
2024-12-10 13:46:24 +0100

    Changes the setfacl command and adds it to the local workdir_root creation too.
    Signed-off-by: mcasquer <mcasquer@redhat.com>

--- tmt/steps/provision/__init__.py
+++ tmt/steps/provision/__init__.py
@@ -1161,6 +1161,7 @@ class Guest(tmt.utils.Common):
             f"""
             if [ ! -d {pid_directory} ]; then \
                    {sudo} mkdir -p {pid_directory} \
+                && {sudo} setfacl -d -m u::rwX,g::rwX,o::rwX \
                 && {sudo} chmod ugo+rwx {pid_directory}; \
             fi
             """
@@ -2086,7 +2087,7 @@ class GuestSsh(Guest):
                 ShellScript(
                     f"""
                     mkdir -p {workdir_root};
-                    sudo setfacl -d -m o:rX {workdir_root}
+                    sudo setfacl -d -m u::rwX,g::rwX,o::rwX {workdir_root}
                     """))
 
     def execute(
