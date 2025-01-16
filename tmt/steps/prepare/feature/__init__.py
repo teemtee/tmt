5e7eba6d44f5c3f451f3cee9521df4e70f352e7c
Miloš Prchlík <mprchlik@redhat.com>
2025-01-16 01:10:33 +0100

    Add `tmt about`, command showing things about tmt itself

--- tmt/steps/prepare/feature/__init__.py
+++ tmt/steps/prepare/feature/__init__.py
@@ -18,7 +18,7 @@ from tmt.utils import Path
 FEATURE_PLAYEBOOK_DIRECTORY = tmt.utils.resource_files('steps/prepare/feature')
 
 FeatureClass = type['Feature']
-_FEATURE_PLUGIN_REGISTRY: PluginRegistry[FeatureClass] = PluginRegistry()
+_FEATURE_PLUGIN_REGISTRY: PluginRegistry[FeatureClass] = PluginRegistry('prepare.feature')
 
 
 def provides_feature(
