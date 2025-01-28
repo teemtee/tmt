8af4e4dac13b5cc611cfd00a779a8ebcf78f873f
Miloš Prchlík <mprchlik@redhat.com>
2025-01-28 11:35:10 +0100

    squash: add test

--- tests/steps/when/test.sh
+++ tests/steps/when/test.sh
@@ -12,27 +12,33 @@ rlJournalStart
 
     rlPhaseStartTest
         # No tests discovered
-        rlRun -s "tmt run --id $tmp/no-context" "3"
+        rlRun -s "tmt run --id $tmp/no-context plan -n '/sanity'" "3"
     rlPhaseEnd
 
     rlPhaseStartTest
-        rlRun -s "tmt -c distro=fedora -c trigger=commit run --id $tmp/distro-trigger-context"
-        rlAssertExists "$tmp/distro-trigger-context/plan/data/when-in-discover"
-        rlAssertExists "$tmp/distro-trigger-context/plan/data/when-prepare-always"
-        rlAssertExists "$tmp/distro-trigger-context/plan/data/when-prepare-fedora"
-        rlAssertExists "$tmp/distro-trigger-context/plan/data/when-report-fedora"
-        rlAssertNotExists "$tmp/distro-trigger-context/plan/data/when-finish-no-distro"
+        rlRun -s "tmt -c distro=fedora -c trigger=commit run --id $tmp/distro-trigger-context plan -n '/sanity'"
+        rlAssertExists "$tmp/distro-trigger-context/plan/sanity/data/when-in-discover"
+        rlAssertExists "$tmp/distro-trigger-context/plan/sanity/data/when-prepare-always"
+        rlAssertExists "$tmp/distro-trigger-context/plan/sanity/data/when-prepare-fedora"
+        rlAssertExists "$tmp/distro-trigger-context/plan/sanity/data/when-report-fedora"
+        rlAssertNotExists "$tmp/distro-trigger-context/plan/sanity/data/when-finish-no-distro"
     rlPhaseEnd
 
     rlPhaseStartTest
-        rlRun -s "tmt -c trigger=commit run --id $tmp/trigger-context"
-        rlAssertExists "$tmp/trigger-context/plan/data/when-in-discover"
-        rlAssertExists "$tmp/trigger-context/plan/data/when-prepare-always"
-        rlAssertNotExists "$tmp/trigger-context/plan/data/when-prepare-fedora"
-        rlAssertNotExists "$tmp/trigger-context/plan/data/when-report-fedora"
-        rlAssertExists "$tmp/trigger-context/plan/data/when-finish-no-distro"
+        rlRun -s "tmt -c trigger=commit run --id $tmp/trigger-context plan -n '/sanity'"
+        rlAssertExists "$tmp/trigger-context/plan/sanity/data/when-in-discover"
+        rlAssertExists "$tmp/trigger-context/plan/sanity/data/when-prepare-always"
+        rlAssertNotExists "$tmp/trigger-context/plan/sanity/data/when-prepare-fedora"
+        rlAssertNotExists "$tmp/trigger-context/plan/sanity/data/when-report-fedora"
+        rlAssertExists "$tmp/trigger-context/plan/sanity/data/when-finish-no-distro"
     rlPhaseEnd
 
+    rlPhaseStartTest
+        rlRun -s "tmt run --id $tmp/execute-must-obey plan -n '/execute-must-obey'"
+        rlAssertExists "$tmp/execute-must-obey/plan/execute-must-obey/execute/data/guest/default-0/shell-run"
+        rlAssertExists "$tmp/execute-must-obey/plan/execute-must-obey/execute/data/guest/default-0/shell-run/yes-1/output.txt"
+        rlAssertNotExists "$tmp/execute-must-obey/plan/execute-must-obey/execute/data/guest/default-0/shell-not-run"
+    rlPhaseEnd
 
     rlPhaseStartCleanup
         rlRun "popd"
