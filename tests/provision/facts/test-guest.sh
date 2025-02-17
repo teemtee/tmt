7ccd5c2d1c818e302926c1bf93fd17e1090ae76c
Miloš Prchlík <mprchlik@redhat.com>
2025-02-17 09:31:29 +0100

    squash: build the desired image

--- tests/provision/facts/test-guest.sh
+++ tests/provision/facts/test-guest.sh
@@ -41,6 +41,8 @@ rlJournalStart
             fi
 
         elif [ "$PROVISION_HOW" = "container" ]; then
+            build_container_image "fedora/41/upstream\:latest"
+
             provision_options="--image localhost/tmt/container/test/fedora/41/upstream:latest"
             bfu_provision_options="$provision_options --user=nobody"
 
