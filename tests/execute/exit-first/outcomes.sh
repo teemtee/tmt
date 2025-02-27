836cc64efb8ac9ae317edb012a27fb3e27d36692
Filip Vágner <fvagner@redhat.com>
2025-02-27 12:53:55 +0100

    Ensure `exit-first` stops execution only on correct outcomes

---
+++ tests/execute/exit-first/outcomes.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "run=\$(mktemp -d)" 0 "Create run directory"
+        rlRun "pushd outcomes"
+        rlRun "set -o pipefail"
+    rlPhaseEnd
+
+    tmt_command="tmt run -vv --scratch --id ${run} plan --name"
+
+    planName="/plan/good"
+    rlPhaseStartTest "Execution finishes successfully"
+        rlRun -s "${tmt_command} ${planName} 2>&1 >/dev/null" 1 "Warning is expected"
+        rlAssertGrep "info /test/info" $rlRun_LOG
+        rlAssertGrep "warn /test/warn" $rlRun_LOG
+        rlAssertGrep "skip /test/skip" $rlRun_LOG
+        rlAssertGrep "pass /test/pass" $rlRun_LOG
+        rlAssertGrep "pass /test/another-pass" $rlRun_LOG
+        rlAssertNotGrep "warn: Test .* stopping execution." $rlRun_LOG
+        rlAssertGrep "summary: 4 tests executed, 1 test skipped" $rlRun_LOG
+        rlAssertGrep "total: 2 tests passed, 1 test skipped, 1 info and 1 warn" $rlRun_LOG
+    rlPhaseEnd
+
+    planName="/plan/fail"
+    rlPhaseStartTest "Execution stops after the first failure"
+        rlRun -s "${tmt_command} ${planName} 2>&1 >/dev/null" 1 "Failure is expected"
+        rlAssertGrep "pass /test/pass" $rlRun_LOG
+        rlAssertGrep "fail /test/fail" $rlRun_LOG
+        rlAssertNotGrep "pass /test/another-pass" $rlRun_LOG
+        rlAssertGrep "warn: Test /test/fail failed, stopping execution." $rlRun_LOG
+        rlAssertGrep "summary: 2 tests executed" $rlRun_LOG
+        rlAssertGrep "total: 1 test passed and 1 test failed" $rlRun_LOG
+    rlPhaseEnd
+
+    planName="/plan/error"
+    rlPhaseStartTest "Execution stops after the first error"
+        rlRun -s "${tmt_command} ${planName} 2>&1 >/dev/null" 2 "Error is expected"
+        rlAssertGrep "pass /test/pass" $rlRun_LOG
+        rlAssertGrep "errr /test/error" $rlRun_LOG
+        rlAssertNotGrep "pass /test/another-pass" $rlRun_LOG
+        rlAssertGrep "warn: Test /test/error failed, stopping execution." $rlRun_LOG
+        rlAssertGrep "summary: 2 tests executed" $rlRun_LOG
+        rlAssertGrep "total: 1 test passed and 1 error" $rlRun_LOG
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $run" 0 "Remove run directory"
+    rlPhaseEnd
+rlJournalEnd
