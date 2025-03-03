6748bd545e78d2042f327183c2b36bfe7a2ebfb1
Petr "Stone" Hracek <phracek@redhat.com>
2025-03-03 10:07:25 +0100

    Add Testing Farm As GitHub action to template ini so the user can easily integrate their project into GitHub Pipelines by GitHub Actions.
    The reference to Testing Farm as GitHub Action is here:
    https://github.com/sclorg/testing-farm-as-github-action
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tmt/base.py
+++ tmt/base.py
@@ -3709,6 +3709,14 @@ class Tree(tmt.utils.Common):
                 dry=dry,
                 logger=logger,
             )
+            tmt.Plan.create(
+                names=['/plans/example'],
+                template='tfaga',
+                path=path,
+                force=force,
+                dry=dry,
+                logger=logger,
+            )
         elif template == 'base':
             tmt.Test.create(
                 names=['/tests/example'],
