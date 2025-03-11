4e17e6a9857212169a00883283db435fa3b9a3b1
Miloš Prchlík <mprchlik@redhat.com>
2025-03-11 19:43:29 +0100

    Add a helper for creating temporary directories on guest
    It will come handy when building image mode image.

--- tests/unit/test_guest.py
+++ tests/unit/test_guest.py
@@ -1,12 +1,18 @@
 import os
+import re
 from typing import Any
 from unittest.mock import MagicMock
 
+import _pytest.logging
 import pytest
+from pytest_container.container import ContainerData
 
 from tmt.log import Logger
 from tmt.steps.provision import Guest, GuestData, GuestSsh, GuestSshData, Provision
-from tmt.utils import Command, CommandOutput
+from tmt.steps.provision.podman import GuestContainer
+from tmt.utils import Command, CommandOutput, Path, RunError, ShellScript
+
+from . import TEST_CONTAINERS
 
 
 def test_multihost_name(root_logger: Logger) -> None:
@@ -77,3 +83,31 @@ def test_execute_no_connection_closed(
 
     output = guest.execute(Command('some-command'))
     assert output.stdout == stdout
+
+
+@pytest.mark.containers
+@pytest.mark.parametrize(
+    'container',
+    ((TEST_CONTAINERS[container_name],) for container_name in sorted(TEST_CONTAINERS.keys())),
+    indirect=["container"],
+    ids=[TEST_CONTAINERS[container_name].url for container_name in sorted(TEST_CONTAINERS.keys())],
+)
+def test_mkdtemp(
+    container: ContainerData,
+    guest: GuestContainer,
+    root_logger: Logger,
+    caplog: _pytest.logging.LogCaptureFixture,
+) -> None:
+    guest.execute(ShellScript('mkdir -p /tmp/qux'))
+
+    with guest.mkdtemp(
+        prefix='bar',
+        template='XXXXXXbazXXXXXX',
+        parent=Path('/tmp/qux'),
+    ) as path:
+        guest.execute(ShellScript(f'ls -al {path}'))
+
+    with pytest.raises(RunError) as exc_context:
+        guest.execute(ShellScript(f'ls -al {path}'))
+
+    assert re.match(r'(?i).*?No such file or directory.*', exc_context.value.stderr or '')
