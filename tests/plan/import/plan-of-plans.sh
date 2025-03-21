8fdc378865a0db699f95dbde4c39b477938a679e
Miloš Prchlík <mprchlik@redhat.com>
2025-03-21 15:44:48 +0100

    squash: testing

--- tests/plan/import/plan-of-plans.sh
+++ tests/plan/import/plan-of-plans.sh
@@ -9,8 +9,14 @@ rlJournalStart
     rlPhaseStartTest "replace/first-plan-only"
         rlRun -s "tmt plan ls /plans/replace/first-plan-only"
 
-        rlAssertGrep "/plans/replace/first-plan-only" $rlRun_LOG
-        rlAssertEquals "Only one plan is listed" "$(wc -l $rlRun_LOG | awk '{print $1}')" "1"
+        rlAssertGrep "^/plans/replace/first-plan-only$" $rlRun_LOG -E
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/artemis.*' through '/plans/replace/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/beaker.*' through '/plans/replace/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/bootc' through '/plans/replace/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/connect' through '/plans/replace/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/container.*' through '/plans/replace/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/local' through '/plans/replace/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/virtual.*' through '/plans/replace/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
     rlPhaseEnd
 
     rlPhaseStartTest "replace/single-plan-only"
@@ -28,8 +34,14 @@ rlJournalStart
     rlPhaseStartTest "become-parent/first-plan-only"
         rlRun -s "tmt plan ls /plans/become-parent/first-plan-only"
 
-        rlAssertGrep "/plans/become-parent/first-plan-only/plans/provision/artemis/sanity/basic" $rlRun_LOG
-        rlAssertEquals "Only one plan is listed" "$(wc -l $rlRun_LOG | awk '{print $1}')" "1"
+        rlAssertGrep "^/plans/become-parent/first-plan-only/plans/provision/artemis/sanity/basic$" $rlRun_LOG -E
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/artemis.*' through '/plans/become-parent/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/beaker.*' through '/plans/become-parent/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/bootc' through '/plans/become-parent/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/connect' through '/plans/become-parent/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/container.*' through '/plans/become-parent/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/local' through '/plans/become-parent/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
+        rlAssertGrep "warn: Cannot import remote plan '/plans/provision/virtual.*' through '/plans/become-parent/first-plan-only', already imported '/plans/provision/artemis/.*' as the first plan." $rlRun_LOG
     rlPhaseEnd
 
     rlPhaseStartTest "become-parent/single-plan-only"
@@ -41,13 +53,13 @@ rlJournalStart
     rlPhaseStartTest "become-parent/all-plans"
         rlRun -s "tmt plan ls /plans/become-parent/all-plans"
 
-        rlAssertGrep "/plans/become-parent/all-plans/plans/provision/artemis" $rlRun_LOG
-        rlAssertGrep "/plans/become-parent/all-plans/plans/provision/beaker" $rlRun_LOG
-        rlAssertGrep "/plans/become-parent/all-plans/plans/provision/bootc" $rlRun_LOG
-        rlAssertGrep "/plans/become-parent/all-plans/plans/provision/connect" $rlRun_LOG
-        rlAssertGrep "/plans/become-parent/all-plans/plans/provision/container" $rlRun_LOG
-        rlAssertGrep "/plans/become-parent/all-plans/plans/provision/local" $rlRun_LOG
-        rlAssertGrep "/plans/become-parent/all-plans/plans/provision/virtual" $rlRun_LOG
+        rlAssertGrep "^/plans/become-parent/all-plans/plans/provision/artemis" $rlRun_LOG -E
+        rlAssertGrep "^/plans/become-parent/all-plans/plans/provision/beaker" $rlRun_LOG -E
+        rlAssertGrep "^/plans/become-parent/all-plans/plans/provision/bootc" $rlRun_LOG -E
+        rlAssertGrep "^/plans/become-parent/all-plans/plans/provision/connect" $rlRun_LOG -E
+        rlAssertGrep "^/plans/become-parent/all-plans/plans/provision/container" $rlRun_LOG -E
+        rlAssertGrep "^/plans/become-parent/all-plans/plans/provision/local" $rlRun_LOG -E
+        rlAssertGrep "^/plans/become-parent/all-plans/plans/provision/virtual" $rlRun_LOG -E
     rlPhaseEnd
 
     rlPhaseStartCleanup
