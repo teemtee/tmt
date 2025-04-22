6f32e5753de45e50d76fc533eb70eb076cfedd04
Miroslav Vadkerti <mvadkert@redhat.com>
2025-04-22 14:02:48 +0200

    Update tests/execute/unresponsive/test.sh
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- tests/execute/unresponsive/test.sh
+++ tests/execute/unresponsive/test.sh
@@ -8,7 +8,7 @@ rlJournalStart
         rlRun "pushd data"
     rlPhaseEnd
 
-    rlPhaseStartTest "Test must error out if machine becomes unresponsive during execution (#3630)."
+    rlPhaseStartTest "Test must error out if machine becomes unresponsive during execution (#3647)."
         rlRun -s "tmt run -vv -a provision -h $PROVISION_HOW" "2"
         rlAssertGrep 'fail: Failed to pull workdir from the guest.' $rlRun_LOG '-F'
         rlAssertGrep 'errr /unresponsive/test/error' $rlRun_LOG '-F'
