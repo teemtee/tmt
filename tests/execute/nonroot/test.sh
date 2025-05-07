12e0710b489da996071c2ab853cbb97bb341e187
Miloš Prchlík <mprchlik@redhat.com>
2025-05-07 17:14:18 +0200

    squash: test with more verbosity

--- tests/execute/nonroot/test.sh
+++ tests/execute/nonroot/test.sh
@@ -9,7 +9,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest
-        if ! rlRun "tmt run -vvv --id $run"; then
+        if ! rlRun "TMT_SHOW_TRACEBACK=full tmt run -vvv --id $run"; then
             rlFileSubmit $run/log.txt
         fi
     rlPhaseEnd
