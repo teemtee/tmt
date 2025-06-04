868a8fb57a171a2b70a9a5f64485cc33a07cc6f2
Miloš Prchlík <mprchlik@redhat.com>
2025-06-04 07:46:23 +0200

    Add `tmt about hints` command (#3709)
    Another step in the "make tmt CLI nicer", `tmt about hints` will list
    and print hints known to tmt. Next, we will make hints more verbose,
    rendered in HTML, and self-referencing for more details.

--- tmt/cli/about.py
+++ tmt/cli/about.py
@@ -7,6 +7,7 @@ from typing import Any
 from click import echo
 
 import tmt.utils
+import tmt.utils.hints
 import tmt.utils.rest
 from tmt.cli import Context, CustomGroup, pass_context
 from tmt.cli._root import main
@@ -59,6 +60,18 @@ def _render_plugins_list_rest() -> str:
     )
 
 
+def _ls(context: Context, how: str, content: Any) -> None:
+    print = context.obj.logger.print  # noqa: A001
+
+    if how in ('pretty', 'rest'):
+        print(
+            tmt.utils.rest.render_rst(content, context.obj.logger) if how == 'pretty' else content
+        )
+
+    elif how in ('json', 'yaml'):
+        print(json.dumps(content) if how == 'json' else tmt.utils.dict_to_yaml(content))
+
+
 @about.group(invoke_without_command=True, cls=CustomGroup)
 @pass_context
 def plugins(context: Context) -> None:
@@ -84,25 +97,59 @@ def plugins_ls(context: Context, how: str) -> None:
     List discovered tmt plugins.
     """
 
-    print = context.obj.logger.print  # noqa: A001
+    if how in ('pretty', 'rest'):
+        _ls(context, how, _render_plugins_list_rest())
+
+    elif how in ('json', 'yaml'):
+        _ls(
+            context,
+            how,
+            {
+                registry.name: list(registry.iter_plugin_ids())
+                for registry in iter_plugin_registries()
+            },
+        )
+
+
+@about.group(invoke_without_command=True, cls=CustomGroup)
+@option('--hint', 'hint_ids', metavar='ID', multiple=True, help='Hint to display.')
+@pass_context
+def hints(context: Context, hint_ids: Any) -> None:
+    """
+    Show hints on various topics.
+    """
+
+    if hint_ids:
+        for hint in tmt.utils.hints.get_hints(*hint_ids):
+            context.obj.logger.print(hint.render(context.obj.logger))
+
+    elif context.invoked_subcommand is None:
+        echo(context.get_help(), color=context.color)
+
+
+@hints.command(name='ls')
+@option(
+    '-h',
+    '--how',
+    choices=['json', 'yaml', 'rest', 'pretty'],
+    default='pretty',
+    help='Output format.',
+)
+@pass_context
+def hints_ls(context: Context, how: str) -> None:
+    """
+    List discovered tmt hints.
+    """
 
     if how in ('pretty', 'rest'):
-        text_output = _render_plugins_list_rest()
-
-        print(
-            tmt.utils.rest.render_rst(text_output, context.obj.logger)
-            if how == 'pretty'
-            else text_output
+        _ls(
+            context,
+            how,
+            render_template_file(
+                TEMPLATES_DIRECTORY / 'hints-ls.rst.j2',
+                HINTS=tmt.utils.hints.HINTS,
+            ),
         )
 
     elif how in ('json', 'yaml'):
-        structured_output = {
-            registry.name: list(registry.iter_plugin_ids())
-            for registry in iter_plugin_registries()
-        }
-
-        print(
-            json.dumps(structured_output)
-            if how == 'json'
-            else tmt.utils.dict_to_yaml(structured_output)
-        )
+        _ls(context, how, {hint_id: hint.text for hint_id, hint in tmt.utils.hints.HINTS.items()})
