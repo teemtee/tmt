31ff024514bd949b624545597fdb33cccce66935
Filip Vágner <fvagner@redhat.com>
2025-07-01 11:19:19 +0200

    squash: prepend file name to every log

--- tmt/steps/report/reportportal.py
+++ tmt/steps/report/reportportal.py
@@ -648,6 +648,8 @@ class ReportReportPortal(tmt.steps.report.ReportPlugin[ReportReportPortalData]):
             )
 
             for log in logs:
+                # Add file name to the log
+                log = f'# `{log_path.name}`\n{log}'
                 # Upload log
                 self.rp_api_post(
                     session=session,
