b16b393af061c923b862db9c62f1b13638535f60
Miloš Prchlík <mprchlik@redhat.com>
2025-07-03 22:16:52 +0200

    Extract abort-related properties from test invocation
    A standalone class grouping abort-related properties is born instead,
    and the test invocation class contains its instance.
    
    Eventually, new class should be more involved in handling of `tmt-abort`
    calls on tmt side, and should be composable with other tmt code that
    runs user-provided code - `shell` and `ansible` plugins of `prepare`
    and `finish` steps.
    
    Part of #902 - an experiment of sorts, because "reboot context" needs to
    be extracted from test invocation, so its code could be used in
    `prepare` and `finish` as well. The issue does not consider abort as
    interesting as reboots, but it's a similar concept, so starting with
    smaller class first, to see how it goes.

--- tmt/steps/execute/internal.py
+++ tmt/steps/execute/internal.py
@@ -18,9 +18,9 @@ from tmt.checks import CheckPlugin
 from tmt.container import container, field
 from tmt.result import Result, ResultOutcome
 from tmt.steps import safe_filename
+from tmt.steps.abort import AbortStep
 from tmt.steps.execute import (
     TEST_OUTPUT_FILENAME,
-    AbortExecute,
     TestInvocation,
 )
 from tmt.steps.provision import Guest
@@ -696,7 +696,7 @@ class ExecuteInternal(tmt.steps.execute.ExecutePlugin[ExecuteInternalData]):
         # via `self.results`, to signal abort we need a bigger gun. Once
         # we get back to refactoring the plugin, this would turn into a
         # better way of transporting "plugin outcome" back to the step.
-        abort_execute_exception: Optional[AbortExecute] = None
+        abort_execute_exception: Optional[AbortStep] = None
 
         with UpdatableMessage(self) as progress_bar:
             while index < len(test_invocations):
@@ -750,7 +750,7 @@ class ExecuteInternal(tmt.steps.execute.ExecutePlugin[ExecuteInternalData]):
                             result.result = ResultOutcome.ERROR
                             result.note.append('reboot timeout')
 
-                if invocation.abort_requested:
+                if invocation.abort.requested:
                     for result in invocation.results:
                         # In case of aborted all results in list will be aborted
                         result.note.append('aborted')
@@ -766,7 +766,7 @@ class ExecuteInternal(tmt.steps.execute.ExecutePlugin[ExecuteInternalData]):
                     variables={'PROGRESS': f'[{progress}]'},
                 ).print_results(invocation.results)
 
-                abort_execute = invocation.abort_requested or (
+                abort_execute = invocation.abort.requested or (
                     self.data.exit_first
                     and any(
                         result.result in (ResultOutcome.FAIL, ResultOutcome.ERROR)
@@ -775,13 +775,13 @@ class ExecuteInternal(tmt.steps.execute.ExecutePlugin[ExecuteInternalData]):
                 )
 
                 if abort_execute:
-                    if invocation.abort_requested:
+                    if invocation.abort.requested:
                         abort_message = f'Test {test.name} aborted, stopping execution.'
 
                     else:
                         abort_message = f'Test {test.name} failed, stopping execution.'
 
-                    abort_execute_exception = AbortExecute(abort_message)
+                    abort_execute_exception = AbortStep(abort_message)
 
                     progress_bar.clear()
 
