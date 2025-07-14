teemtee/tmt

commit 15ca013be2ae703f51b555cb59451af49fbb4cab

Allow dotenv files from `--environment` (#3799)
The ``--environment`` option now properly supports dotenv files as
documented. Previously, while the help text indicated that both YAML
and dotenv formats were supported, only YAML files worked correctly.
Now dotenv files can be used with the ``@filename.env`` syntax.

Co-authored-by: Miroslav Vadkerti <mvadkert@redhat.com>

tmt/utils/__init__.py
@@ -503,6 +503,7 @@ class Environment(dict[str, EnvVarValue]):
         cls,
         variables: Union[str, list[str]],
         logger: tmt.log.Logger,
+        root: Optional[Path] = None,
     ) -> 'Environment':
         """
         Construct environment from a sequence of variables.
@@ -513,7 +514,7 @@ class Environment(dict[str, EnvVarValue]):
         * ``@foo.yaml`` signaling variables to be read from a file.
 
         If a "variable" starts with ``@``, it is treated as a path to
-        a YAML file that contains key/value pairs which are then
+        a YAML or DOTENV file that contains key/value pairs which are then
         transparently loaded and added to the final environment.
 
         :param variables: string or a sequence of strings containing
@@ -526,6 +527,9 @@ class Environment(dict[str, EnvVarValue]):
             * ``'TXT="Some text with spaces in it"'``
             * ``@foo.yaml``
             * ``@../../bar.yaml``
+            * ``@foo.env``
+
+        :param root: root directory to load variable files from.
         """
 
         if not isinstance(variables, (list, tuple)):
@@ -541,12 +545,11 @@ class Environment(dict[str, EnvVarValue]):
                     if not var[1:]:
                         raise GeneralError(f"Invalid variable file specification '{var}'.")
 
-                    filepath = Path(var[1:])
-
-                    environment = cls.from_yaml_file(filepath, logger)
+                    filename = var[1:]
+                    environment = cls.from_file(filename=filename, root=root, logger=logger)
 
                     if not environment:
-                        logger.warning(f"Empty environment file '{filepath}'.")
+                        logger.warning(f"Empty environment file '{filename}'.")
 
                     result.update(environment)
 
@@ -770,7 +773,11 @@ class Environment(dict[str, EnvVarValue]):
         if raw_cli_environment is None:
             pass
         elif isinstance(raw_cli_environment, (list, tuple)):
-            from_cli = Environment.from_sequence(list(raw_cli_environment), logger)
+            from_cli = Environment.from_sequence(
+                variables=list(raw_cli_environment),
+                logger=logger,
+                root=file_root,
+            )
         else:
             raise NormalizationError(
                 'environment', raw_cli_environment, 'unset or a list of key/value pairs'

tests/core/env/test.sh
@@ -56,8 +56,27 @@ rlJournalStart
             done
         rlPhaseEnd
 
-        rlPhaseStartTest "Empty environment file ($execute)"
+        # Use the same setup as the test above, but instead of defining
+        # variables on the command line read them from a DOTENV file
+        rlPhaseStartTest "Variable in DOTENV file ($execute)"
+            for plan in yes no; do
+                for test in yes no; do
+                    rlRun -s "tmt run -avvvr -e @vars.env \
+                        execute --how $execute \
+                        plan --name $plan \
+                        test --name $test"
+                    rlAssertGrep '>>>O0<<<' $rlRun_LOG
+                done
+            done
+        rlPhaseEnd
+
+        rlPhaseStartTest "Empty YAML environment file ($execute)"
             rlRun -s "tmt run -r -e @empty.yaml"
+            rlAssertGrep "warn: Empty environment file" $rlRun_LOG
+        rlPhaseEnd
+
+        rlPhaseStartTest "Empty DOTENV environment file ($execute)"
+            rlRun -s "tmt run -r -e @empty.env"
             rlAssertGrep "warn: Empty environment file" $rlRun_LOG
         rlPhaseEnd
     done

tests/core/env/data/vars.env
@@ -1,0 +1,2 @@
+STR=O
+INT=0

commit 3435fd418bbeb4689b9ba0b0b11897ad6085f015

Environment files on command-line do not need to be under fmf root
Fixes a regression introduced in #3799.

Fixes #3877.

tmt/utils/__init__.py
@@ -503,7 +503,6 @@ class Environment(dict[str, EnvVarValue]):
         cls,
         variables: Union[str, list[str]],
         logger: tmt.log.Logger,
-        root: Optional[Path] = None,
     ) -> 'Environment':
         """
         Construct environment from a sequence of variables.
@@ -528,8 +527,6 @@ class Environment(dict[str, EnvVarValue]):
             * ``@foo.yaml``
             * ``@../../bar.yaml``
             * ``@foo.env``
-
-        :param root: root directory to load variable files from.
         """
 
         if not isinstance(variables, (list, tuple)):
@@ -546,7 +543,7 @@ class Environment(dict[str, EnvVarValue]):
                         raise GeneralError(f"Invalid variable file specification '{var}'.")
 
                     filename = var[1:]
-                    environment = cls.from_file(filename=filename, root=root, logger=logger)
+                    environment = cls.from_file(filename=filename, logger=logger)
 
                     if not environment:
                         logger.warning(f"Empty environment file '{filename}'.")
@@ -776,7 +773,6 @@ class Environment(dict[str, EnvVarValue]):
             from_cli = Environment.from_sequence(
                 variables=list(raw_cli_environment),
                 logger=logger,
-                root=file_root,
             )
         else:
             raise NormalizationError(

tests/core/env/test.sh
@@ -6,6 +6,8 @@ rlJournalStart
     rlPhaseStartSetup
         rlRun 'pushd data'
         rlRun 'set -o pipefail'
+
+        rlRun "tmp=\$(mktemp -d)" 0 "Create tmp directory"
     rlPhaseEnd
 
     rlPhaseStartTest "Check the TMT_DEBUG variable"
@@ -79,9 +81,29 @@ rlJournalStart
             rlRun -s "tmt run -r -e @empty.env"
             rlAssertGrep "warn: Empty environment file" $rlRun_LOG
         rlPhaseEnd
+
+        rlPhaseStartTest "Variable file outside of fmf root ($execute)"
+            rlRun "cp vars.yaml $tmp/out-of-tree-vars.yaml"
+            rlRun "fmf_root=$PWD"
+
+            rlRun "pushd $tmp"
+
+            for plan in yes no; do
+                for test in yes no; do
+                    rlRun -s "tmt --root $fmf_root run -avvvr -e @out-of-tree-vars.yaml \
+                        execute --how $execute \
+                        plan --name $plan \
+                        test --name $test"
+                    rlAssertGrep '>>>O0<<<' $rlRun_LOG
+                done
+            done
+
+            rlRun "popd"
+        rlPhaseEnd
     done
 
     rlPhaseStartCleanup
+        rlRun "rm -r $tmp" 0 "Remove tmp directory"
         rlRun 'rm -f output' 0 'Removing tmp file'
         rlRun 'popd'
     rlPhaseEnd