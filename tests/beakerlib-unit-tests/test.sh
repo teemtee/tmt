c62ca2de2ae150b71710497f01ed5df7d84ff6be
Qin Yuan <qiyuan@redhat.com>
2025-08-11 17:47:30 +0800

    Run beakerlib journal unit tests

---
+++ tests/beakerlib-unit-tests/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+. ../images.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "run=\$(mktemp -d)" 0 "Create a run directory"
+        rlRun "pushd data"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun -s "tmt run -vvv"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -rf $run" 0 "Remove run directory"
+    rlPhaseEnd
+rlJournalEnd
