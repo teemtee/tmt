cda05cc8b17f01b8b8ccbd6b007737e8f8bd34ea
Tomas Bajer <tbajer@redhat.com>
2025-08-11 18:43:47 +0200

    Added type annotation

--- tmt/utils/__init__.py
+++ tmt/utils/__init__.py
@@ -4827,8 +4827,9 @@ def _prenormalize_fmf_node(node: fmf.Tree, schema_name: str, logger: tmt.log.Log
             return
 
         # Handle None/empty step configuration (e.g., "provision:" with no value)
+        # comment
         if step_collection is None:
-            empty_step = {}
+            empty_step: dict[Any, Any] = {}
             _process_step(step_name, empty_step)
             node.data[step_name] = empty_step
 
