67b83448daf34af81078f61996663dcf29649b34
Tomas Bajer <tbajer@redhat.com>
2025-08-12 16:42:18 +0200

    Changed step_collection to list, adjusted tests

--- tests/lint/story/test.sh
+++ tests/lint/story/test.sh
@@ -20,12 +20,12 @@ rlJournalStart
         rlAssertGrep "warn C001 summary should not exceed 50 characters" $rlRun_LOG
 
         rlRun -s "tmt stories lint typo_in_key" 1
-        rlAssertGrep "warn C000 fmf node failed schema validation" $rlRun_LOG
+        rlAssertGrep "fail C000 fmf node failed schema validation" $rlRun_LOG
         rlAssertGrep "warn C001 summary key is missing" $rlRun_LOG
         rlAssertGrep "fail S001 unknown key \"exampleGG\" is used" $rlRun_LOG
 
         rlRun -s "tmt stories lint missing_story" 1
-        rlAssertGrep "warn C000 fmf node failed schema validation" $rlRun_LOG
+        rlAssertGrep "fail C000 fmf node failed schema validation" $rlRun_LOG
         rlAssertGrep "warn C001 summary key is missing" $rlRun_LOG
         rlAssertGrep "pass S001 correct keys are used" $rlRun_LOG
         rlAssertGrep "fail S002 story is required" $rlRun_LOG
