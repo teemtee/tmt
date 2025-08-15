da0f3ee920d05dbc81bc1a8e0e7dec427108e1cd
AthreyVinay <avinay@redhat.com>
2025-08-15 15:48:12 +0100

    chore: removed unintentional line commit

--- tmt/trying.py
+++ tmt/trying.py
@@ -673,7 +673,6 @@ class Try(tmt.utils.Common):
 
         # Set the default verbosity level, handle options
         for plan in self.plans:
-            self.action_host(plan)
             self.handle_options(plan)
             self.action_verbose(plan)
 
