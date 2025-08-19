57382b8cf167cfb29d5fea8da80f50cf91d2b54d
Cristian Le <git@lecris.dev>
2025-08-19 19:06:08 +0200

    Move sphinx-apidoc to sphinx builder
    Signed-off-by: Cristian Le <git@lecris.dev>

--- docs/ext/tmt_setup.py
+++ docs/ext/tmt_setup.py
@@ -12,6 +12,10 @@ if TYPE_CHECKING:
     from sphinx.application import Sphinx
 
 
+# TODO: Figure out how to use sphinx's source caching check
+# https://github.com/sphinx-doc/sphinx/issues/11556#issuecomment-3201453172
+
+
 def generate_tmt_docs(app: Sphinx) -> None:
     """
     Run `make generate` to populate the auto-generated sources
@@ -22,6 +26,10 @@ def generate_tmt_docs(app: Sphinx) -> None:
 
 
 def setup(app: Sphinx) -> None:
+    from sphinx_apidoc import sphinx_apidoc
+
+    # Do sphinx-apidoc
+    app.connect("builder-inited", sphinx_apidoc)
     # Generate sources after loading configuration. That should build
     # everything, including the logo, before Sphinx starts checking
     # whether all input files exist.
