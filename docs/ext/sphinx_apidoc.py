b54fcd421b603bd52849b8c057165bba1e46346e
Cristian Le <git@lecris.dev>
2025-08-19 19:06:08 +0200

    Move sphinx-apidoc to sphinx builder
    Signed-off-by: Cristian Le <git@lecris.dev>

---
+++ docs/ext/sphinx_apidoc.py
@@ -0,0 +1,42 @@
+"""
+Sphinx extension to generate code/autodocs files
+"""
+
+from typing import TYPE_CHECKING
+
+from sphinx.util import logging
+
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+
+logger = logging.getLogger(__name__)
+
+
+def sphinx_apidoc(app: "Sphinx") -> None:
+    """
+    Run `shpinx-apidoc`
+    """
+
+    from sphinx.ext import apidoc
+
+    # inspired from https://github.com/sphinx-contrib/apidoc
+    conf_dir = app.confdir
+    root = conf_dir.parent
+    # call sphinx-apidoc
+    # TODO: Clean the folder before running sphinx-apidoc
+    logger.info("Running sphinx-apidoc")
+    (conf_dir / "code/autodocs").mkdir(exist_ok=True)
+    apidoc.main(
+        [
+            "--force",
+            "--implicit-namespaces",
+            "--no-toc",
+            "-o",
+            str(conf_dir / "code/autodocs"),
+            str(root / "tmt"),
+        ]
+    )
+
+
+def setup(app: "Sphinx"):
+    app.connect("builder-inited", sphinx_apidoc)
