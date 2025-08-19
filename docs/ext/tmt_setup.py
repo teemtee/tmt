06b50d1de88ca8914a46a4e8e76a8439ea9a4c38
Cristian Le <git@lecris.dev>
2025-08-19 19:06:08 +0200

    Move sphinx-apidoc to sphinx builder
    Signed-off-by: Cristian Le <git@lecris.dev>

--- docs/ext/tmt_setup.py
+++ docs/ext/tmt_setup.py
@@ -8,8 +8,42 @@ import subprocess
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+from sphinx.util import logging
+
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
+
+
+logger = logging.getLogger(__name__)
+
+# TODO: Figure out how to use sphinx's source caching check
+# https://github.com/sphinx-doc/sphinx/issues/11556#issuecomment-3201453172
+
+
+def sphinx_apidoc(app: Sphinx) -> None:
+    """
+    Run `shpinx-apidoc`
+    """
+
+    from sphinx.ext import apidoc
+
+    # inspired from https://github.com/sphinx-contrib/apidoc
+    conf_dir = app.confdir
+    root = conf_dir.parent
+    # call sphinx-apidoc
+    # TODO: Clean the folder before running sphinx-apidoc
+    logger.info("Running sphinx-apidoc")
+    (conf_dir / "code/autodocs").mkdir(exist_ok=True)
+    apidoc.main(
+        [
+            "--force",
+            "--implicit-namespaces",
+            "--no-toc",
+            "-o",
+            str(conf_dir / "code/autodocs"),
+            str(root / "tmt"),
+        ]
+    )
 
 
 def generate_tmt_docs(app: Sphinx) -> None:
@@ -22,6 +56,8 @@ def generate_tmt_docs(app: Sphinx) -> None:
 
 
 def setup(app: Sphinx) -> None:
+    # Do sphinx-apidoc
+    app.connect("builder-inited", sphinx_apidoc)
     # Generate sources after loading configuration. That should build
     # everything, including the logo, before Sphinx starts checking
     # whether all input files exist.
