d99fddeafd1cca0d2000724ec8d8ecb177989cc4
Lukáš Zachar <lzachar@redhat.com>
2025-08-21 10:58:23 +0200

    squash: apply feedback

--- tmt/base.py
+++ tmt/base.py
@@ -3124,6 +3124,9 @@ class Plan(
         Discover plan-like nodes matching the given reference in fmf cache.
         """
 
+        # TODO: similar situation as in _resolve_import_from_git
+        assert reference.url is not None
+
         if str(reference.ref).startswith('@'):
             self.debug(
                 f"Not enough data to evaluate dynamic ref '{reference.ref}', "
@@ -3148,7 +3151,6 @@ class Plan(
 
                 reference.resolve_dynamic_ref(tmpdirname, self)
 
-        assert reference.url
         yield from self._resolve_import_to_nodes(
             reference,
             fmf.utils.fetch_tree(
