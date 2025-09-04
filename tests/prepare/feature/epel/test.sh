e09ace339f642b66e414075c80ff14cca5d33f63
Miroslav Vadkerti <mvadkert@redhat.com>
2025-09-05 01:12:30 +0200

    Workaround RHEL8 epel feature for Fedora 42+
    With Fedora42+ shipping Ansible-2.18+ the users get
    a failure when trying to connect to the remote host,
    because the default Python3.6 is not suported anymore.
    
    We can workaround this by installing Python3.9 via
    modules, but `dnf` will not work well as there is no
    `python3-dnf` module for this Python version. So in
    the case of RHEL8 use `rpm` instead to install the
    `epel-release` package.
    
    Note that this workaround will hopefully go away
    with Testing Farm profiles, which should sort out this
    problem for all RHELs.
    
    Enable `ubi8` tests to verify the feature.
    
    Resolves #3931
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- tests/prepare/feature/epel/test.sh
+++ tests/prepare/feature/epel/test.sh
@@ -16,7 +16,7 @@ rlJournalStart
 
     # EPEL
     for image in $images; do
-        if rlIsFedora ">=42" && (is_centos_7 "$image" || is_ubi_8 "$image"); then
+	if rlIsFedora ">=42" && is_centos_7 "$image"; then
             rlLogInfo "Skipping because Ansible shipped with Fedora does not support Python 3.6"
 
             continue
