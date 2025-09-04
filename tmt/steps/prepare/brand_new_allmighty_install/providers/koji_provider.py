c5206ed79c806a92b61136045d21b8880a1e6f32
Athrey Vinay <avinay@redhat.com>
2025-09-04 23:43:35 +0100

    Implement download artifact logic

--- tmt/steps/prepare/brand_new_allmighty_install/providers/koji_provider.py
+++ tmt/steps/prepare/brand_new_allmighty_install/providers/koji_provider.py
@@ -3,6 +3,7 @@ Koji Artifact Provider
 """
 
 from collections.abc import Iterator
+from shlex import quote
 from typing import Any
 
 from koji import ClientSession
@@ -11,14 +12,14 @@ import tmt.log
 from tmt.steps.prepare.brand_new_allmighty_install.providers import (
     ArtifactInfo,
     ArtifactProvider,
-    ArtifactType,
 )
 from tmt.steps.provision import Guest
-from tmt.utils import GeneralError, Path
+from tmt.utils import GeneralError, Path, ShellScript
 
 
 class KojiProvider(ArtifactProvider):
-    API_URL = "https://koji.fedoraproject.org/kojihub"
+    API_URL = "https://koji.fedoraproject.org/kojihub"  # For metadata
+    PKG_URL = "https://kojipkgs.fedoraproject.org/packages/"  # For actual package downloads
 
     def __init__(self, logger: tmt.log.Logger, artifact_id: str):
         super().__init__(logger, artifact_id)
@@ -56,6 +57,30 @@ class KojiProvider(ArtifactProvider):
             raise ValueError(f"Invalid artifact ID format: {artifact_id}")
         return artifact_id[len("koji.build:") :]
 
+    def _get_rpm_filename(self, rpm: dict[str, Any]) -> str:
+        """
+        Get the RPM filename for the given RPM metadata.
+
+        :param rpm: The RPM metadata to get the filename for
+        :return: The RPM filename
+        """
+        return f"{rpm['name']}-{rpm['version']}-{rpm['release']}.{rpm['arch']}.rpm"
+
+    def _get_download_url(self, rpm: dict[str, Any]) -> str:
+        """
+        Get the download URL for the given RPM metadata.
+
+        :param rpm: The RPM metadata to get the download URL for
+        :return: The download URL
+        """
+        return (
+            f"{self.PKG_URL}{rpm['name']}/"
+            f"{rpm['version']}/"
+            f"{rpm['release']}/"
+            f"{rpm['arch']}/"
+            f"{self._get_rpm_filename(rpm)}"
+        )
+
     def list_artifacts(self) -> Iterator[ArtifactInfo]:
         """
         TODO: Currently only lists rpms, extend to other types.
@@ -63,23 +88,37 @@ class KojiProvider(ArtifactProvider):
         """
         if rpm_list := self._call_api('listBuildRPMs', int(self.artifact_id)):
             for rpm in rpm_list:
-                self.logger.debug(f"Found RPM: {rpm['nvr']} ({rpm['arch']})")
+                rpm_filename = self._get_rpm_filename(rpm)
+                self.logger.debug(f"Found RPM: {rpm_filename}")
                 yield ArtifactInfo(
                     id=int(self.artifact_id),
-                    name=rpm.get('name'),
-                    type=ArtifactType.RPM,
-                    arch=rpm.get('arch'),
-                    version=rpm.get('version'),
-                    release=rpm.get('release'),
-                    epoch=rpm.get('epoch'),
-                    draft=rpm.get('draft'),
-                    payloadhash=rpm.get('payloadhash'),
-                    size=rpm.get('size'),
-                    buildtime=rpm.get('buildtime'),
-                    build_id=rpm.get('build_id'),
-                    buildroot_id=rpm.get('buildroot_id'),
+                    name=rpm_filename,
+                    location=self._get_download_url(rpm),
                 )
 
     def _download_artifact(self, artifact: ArtifactInfo, guest: Guest, destination: Path) -> Path:
-        # TODO: Implement actual download logic
-        return destination
+        """
+        Download the specified artifact to the given destination on the guest.
+
+        :param artifact: The artifact to download
+        :param guest: The guest where the artifact should be downloaded
+        :param destination: The destination path on the guest
+        :return: The path to the downloaded artifact on the guest
+        :raises GeneralError: If download fails
+        """
+
+        dest_dir: Path = destination.parent
+
+        # Create the destination directory if it doesn't exist and download the artifact
+        guest.execute(
+            ShellScript(
+                f"[ -d {quote(str(dest_dir))} ] || "
+                f'{"sudo " if not guest.facts.is_superuser else ""}'
+                f"mkdir -p {quote(str(dest_dir))}; "
+                f"cd {quote(str(dest_dir))} && "
+                f"curl -LOf {quote(artifact.location)}"
+            ).to_shell_command(),
+            silent=True,
+        )
+
+        return destination / str(artifact)
