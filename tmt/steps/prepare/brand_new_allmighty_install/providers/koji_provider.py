cda03b2ec62c0bb12c03d7080d8a0133b60daf3c
Athrey Vinay <avinay@redhat.com>
2025-09-05 16:38:28 +0100

    Refactor container classes to bespoke providers

--- tmt/steps/prepare/brand_new_allmighty_install/providers/koji_provider.py
+++ tmt/steps/prepare/brand_new_allmighty_install/providers/koji_provider.py
@@ -9,6 +9,7 @@ from typing import Any
 from koji import ClientSession
 
 import tmt.log
+from tmt.container import container
 from tmt.steps.prepare.brand_new_allmighty_install.providers import (
     ArtifactInfo,
     ArtifactProvider,
@@ -17,9 +18,34 @@ from tmt.steps.provision import Guest
 from tmt.utils import GeneralError, Path, ShellScript
 
 
+@container
+class KojiArtifactInfo(ArtifactInfo):
+    PKG_URL = "https://kojipkgs.fedoraproject.org/packages/"  # For actual package downloads
+
+    @property
+    def name(self) -> str:
+        """Get the RPM filename for the given RPM metadata."""
+        return (
+            f"{self._raw_artifact['name']}-"
+            f"{self._raw_artifact['version']}-"
+            f"{self._raw_artifact['release']}."
+            f"{self._raw_artifact['arch']}.rpm"
+        )
+
+    @property
+    def location(self) -> str:
+        """Get the download URL for the given RPM metadata."""
+        return (
+            f"{self.PKG_URL}{self._raw_artifact['name']}/"
+            f"{self._raw_artifact['version']}/"
+            f"{self._raw_artifact['release']}/"
+            f"{self._raw_artifact['arch']}/"
+            f"{self.name}"
+        )
+
+
 class KojiProvider(ArtifactProvider):
     API_URL = "https://koji.fedoraproject.org/kojihub"  # For metadata
-    PKG_URL = "https://kojipkgs.fedoraproject.org/packages/"  # For actual package downloads
 
     def __init__(self, logger: tmt.log.Logger, artifact_id: str):
         super().__init__(logger, artifact_id)
@@ -57,30 +83,6 @@ class KojiProvider(ArtifactProvider):
             raise ValueError(f"Invalid artifact ID format: {artifact_id}")
         return artifact_id[len("koji.build:") :]
 
-    def _get_rpm_filename(self, rpm: dict[str, Any]) -> str:
-        """
-        Get the RPM filename for the given RPM metadata.
-
-        :param rpm: The RPM metadata to get the filename for
-        :return: The RPM filename
-        """
-        return f"{rpm['name']}-{rpm['version']}-{rpm['release']}.{rpm['arch']}.rpm"
-
-    def _get_download_url(self, rpm: dict[str, Any]) -> str:
-        """
-        Get the download URL for the given RPM metadata.
-
-        :param rpm: The RPM metadata to get the download URL for
-        :return: The download URL
-        """
-        return (
-            f"{self.PKG_URL}{rpm['name']}/"
-            f"{rpm['version']}/"
-            f"{rpm['release']}/"
-            f"{rpm['arch']}/"
-            f"{self._get_rpm_filename(rpm)}"
-        )
-
     def list_artifacts(self) -> Iterator[ArtifactInfo]:
         """
         TODO: Currently only lists rpms, extend to other types.
@@ -88,12 +90,9 @@ class KojiProvider(ArtifactProvider):
         """
         if rpm_list := self._call_api('listBuildRPMs', int(self.artifact_id)):
             for rpm in rpm_list:
-                rpm_filename = self._get_rpm_filename(rpm)
-                self.logger.debug(f"Found RPM: {rpm_filename}")
-                yield ArtifactInfo(
+                yield KojiArtifactInfo(
+                    _raw_artifact=rpm,
                     id=int(self.artifact_id),
-                    name=rpm_filename,
-                    location=self._get_download_url(rpm),
                 )
 
     def _download_artifact(self, artifact: ArtifactInfo, guest: Guest, destination: Path) -> None:
@@ -104,6 +103,8 @@ class KojiProvider(ArtifactProvider):
         :param guest: The guest where the artifact should be downloaded
         :param destination: The destination path on the guest
         """
+
+        assert isinstance(artifact, KojiArtifactInfo)
 
         # Destination directory is guaranteed to exist, download the artifact
         guest.execute(
