365cc8c14a129ab70cd4da248e9ac583dfca7bf6
Therese Cornell <tcornell@redhat.com>
2025-09-11 12:07:38 -0400

    Add keys to beaker provision schema Include `key` and `order`. Fix `public-key`. Add `tmt lint` test.

---
+++ tests/provision/beaker/schema.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "pushd data"
+        rlRun "set -o pipefail"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Check that beaker schema contains correct keys"
+        rlRun -s "tmt lint"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
