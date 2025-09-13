b8cc3c6525266116933cb83abbe6f6ce379a0ace
Miloš Prchlík <happz@happz.cz>
2025-09-13 21:26:47 +0200

    squash: Update tmt/steps/provision/testcloud.py

--- tmt/steps/provision/testcloud.py
+++ tmt/steps/provision/testcloud.py
@@ -1473,6 +1473,10 @@ class ConsoleLog(tmt.steps.provision.GuestLog):
         logger.debug(f"Created console log directory '{self.exchange_directory}'.", level=3)
 
         self.exchange_directory.chmod(0o755)
+
+        assert isinstance(self.guest.parent, tmt.steps.provision.Provision)  # narrow type
+        assert self.guest.parent.plan.my_run is not None  # narrow type
+
         if self.guest.parent.plan.my_run.runner.facts.has_selinux:
             self.guest._run_guest_command(
                 Command("chcon", "--type", "virt_log_t", self.exchange_directory), silent=True
