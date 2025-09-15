9ba2f2fe415c27282e3ecded47870396b550b8b1
tcornell-bus <tcornell@redhat.com>
2025-09-15 09:08:25 -0400

    Include `key`, `order`, and `public-key` in beaker provision schema (#4048)
    - Add `key` and `order`.
    - Update `public-key` to allow a single string.
    - Add `tmt lint` test for beaker provision schema. 
    
    Fixes: #4033

---
+++ tests/provision/beaker/schema.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "pushd data"
+        rlRun "set -o pipefail"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Check that beaker schema contains correct keys"
+        rlRun -s "tmt lint /plan/schema"
+        rlAssertNotGrep "is not valid under any of the given schemas" $rlRun_LOG
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
