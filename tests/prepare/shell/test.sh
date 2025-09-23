c74bde25ae9d5e37e70e4e12079d472e75670177
Cristian Le <git@lecris.dev>
2025-09-23 16:15:40 +0200

    Ensure `PROVISION_HOW`
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/prepare/shell/test.sh
+++ tests/prepare/shell/test.sh
@@ -3,27 +3,26 @@
 
 rlJournalStart
     rlPhaseStartSetup
-        rlRun "provision_flags='${PROVISION_HOW:+provision --how=$PROVISION_HOW}'"
-
+        rlRun "PROVISION_HOW=${PROVISION_HOW:-local}"
         rlRun "pushd data"
     rlPhaseEnd
 
     rlPhaseStartTest "Custom Script"
-        rlRun "tmt run -arv $provision_flags plan -n custom" 0 "Prepare using a custom script"
+        rlRun "tmt run -arv provision --how=$PROVISION_HOW plan -n custom" 0 "Prepare using a custom script"
     rlPhaseEnd
 
     rlPhaseStartTest "Commandline Script"
-        rlRun "tmt run -arv $provision_flags plan -n custom \
+        rlRun "tmt run -arv provision --how=$PROVISION_HOW plan -n custom \
             prepare -h shell -s './prepare.sh'" 0 "Prepare using a custom script from cmdline"
     rlPhaseEnd
 
     rlPhaseStartTest "Multiple Commandline Scripts"
-        rlRun "tmt run -arv $provision_flags plans -n multiple \
+        rlRun "tmt run -arv provision --how=$PROVISION_HOW plans -n multiple \
             prepare -h shell -s 'touch /tmp/first' -s 'touch /tmp/second'"
     rlPhaseEnd
 
     rlPhaseStartTest "Remote Script"
-        rlRun -s "tmt -vvv run ${provision_flags:-provision} prepare finish cleanup plan -n url" 0 "Prepare using a remote script"
+        rlRun -s "tmt -vvv run provision --how=$PROVISION_HOW prepare finish cleanup plan -n url" 0 "Prepare using a remote script"
         rlAssertGrep "Hello world" "$rlRun_LOG" #check for the prepare script
         rlAssertGrep "third" "$rlRun_LOG" # check for the finish script
     rlPhaseEnd
