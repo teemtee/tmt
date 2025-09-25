6345c9359a8fa9af83efdd9b523fffc3a99c1536
Miloš Prchlík <mprchlik@redhat.com>
2025-09-25 12:54:32 +0200

    Polish artifact provider docs
    Mostly rewording and moving sentences around.

--- tmt/steps/prepare/artifact/providers/__init__.py
+++ tmt/steps/prepare/artifact/providers/__init__.py
@@ -6,10 +6,11 @@ from abc import ABC, abstractmethod
 from collections.abc import Iterator
 from re import Pattern
 from shlex import quote
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, Optional, TypeVar
 
 import tmt.log
 import tmt.utils
+from tmt._compat.typing import TypeAlias
 from tmt.container import container
 from tmt.steps.provision import Guest
 
@@ -23,12 +24,7 @@ class DownloadError(tmt.utils.GeneralError):
 @container
 class ArtifactInfo(ABC):
     """
-    Information about a single artifact.
-
-    Attributes:
-        id (str): A unique identifier for the artifact.
-                  * RPMs → 'tmt-1.58.0.dev21+gb229884df-main.fc41.noarch'
-                  * Containers → image digest or tag
+    Information about a single artifact, e.g. a package.
     """
 
     _raw_artifact: Any
@@ -36,6 +32,10 @@ class ArtifactInfo(ABC):
     @property
     @abstractmethod
     def id(self) -> str:
+        """
+        A unique identifier of the artifact.
+        """
+
         raise NotImplementedError
 
     @property
@@ -47,22 +47,28 @@ class ArtifactInfo(ABC):
         return self.id
 
 
+#: A type of an artifact identifier.
+ArtifactId: TypeAlias = str
+
+#: A type variable representing subclasses of :py:class:`ArtifactInfo`
+#: containers.
 ArtifactInfoT = TypeVar('ArtifactInfoT', bound=ArtifactInfo)
 
 
 class ArtifactProvider(ABC, Generic[ArtifactInfoT]):
     """
-    Abstract provider of artifacts
-    e.g. KojiArtifactProvider, BrewArtifactProvider, RepoFileArtifactProvider.
+    Base class for artifact providers.
 
     Each provider must implement:
-        - Parsing and validating the artifact ID
-        - Listing available artifacts
-        - Downloading a single artifact
-    The base class provides:
-        - Downloading all artifacts with filtering
 
+    * parsing and validating the artifact ID,
+    * listing available artifacts,
+    * downloading a single given artifact.
     """
+
+    #: Identifier of this artifact provider. It is valid and unique
+    #: in the domain of this provider.
+    artifact_id: ArtifactId
 
     def __init__(self, logger: tmt.log.Logger, artifact_id: str):
         self.logger = logger
@@ -71,21 +77,23 @@ class ArtifactProvider(ABC, Generic[ArtifactInfoT]):
         )  # Identifier for the source, e.g. 'koji.build:12345', URL for repository
 
     @abstractmethod
-    def _parse_artifact_id(self, artifact_id: str) -> str:
+    def _parse_artifact_id(self, artifact_id: str) -> ArtifactId:
         """
         Parse and validate the artifact identifier.
 
-        :param id: Identifier for the source
-        :return: The parsed identifier specific to this provider
-        :raises ValueError: If the artifact ID format is invalid
+        :param artifact_id: artifact identifier to parse and validate.
+        :returns: parsed identifier specific to this provider.
+        :raises ValueError: when the artifact identifier is invalid.
         """
+
         raise NotImplementedError
 
     @abstractmethod
     def list_artifacts(self) -> Iterator[ArtifactInfoT]:
         """
-        List all available artifacts for a given build
+        List all artifacts available from this provider.
         """
+
         raise NotImplementedError
 
     @abstractmethod
@@ -93,31 +101,38 @@ class ArtifactProvider(ABC, Generic[ArtifactInfoT]):
         self, artifact: ArtifactInfoT, guest: Guest, destination: tmt.utils.Path
     ) -> None:
         """
-        Action: Download a single artifact to the specified destination.
+        Download a single artifact to the specified destination on a given guest.
 
-        :param guest: The guest where the artifact should be downloaded
-        :param destination: Path where the artifact should be downloaded
+        :param guest: the guest on which the artifact should be downloaded.
+        :param destination: path into which the artifact should be downloaded.
         """
+
         raise NotImplementedError
 
     def download_artifacts(
         self,
         guest: Guest,
         download_path: tmt.utils.Path,
-        exclude_patterns: list[Pattern[str]],
+        exclude_patterns: Optional[list[Pattern[str]]] = None,
     ) -> list[tmt.utils.Path]:
         """
         Download all artifacts to the specified destination.
 
-        :param guest: The guest where the artifacts should be downloaded.
-        :param download_path: The local path to save the downloaded artifact.
-        :param exclude_patterns: Patterns to exclude certain files from being downloaded.
-        :returns: A list of paths to the downloaded artifacts.
+        :param guest: the guest on which the artifact should be
+            downloaded.
+        :param download_path: path into which the artifact should be
+            downloaded.
+        :param exclude_patterns: if set, artifacts whose names match any
+            of the given regular expressions would not be downloaded.
+        :returns: a list of paths to the downloaded artifacts.
         :raises GeneralError: Unexpected errors outside the download process.
         :note: Errors during individual artifact downloads are
             caught, logged as warnings, and ignored.
         """
+
         self.logger.info(f"Downloading artifacts to '{download_path!s}'.")
+
+        exclude_patterns = exclude_patterns or []
 
         # Ensure download directory exists on guest (create only if missing)
         guest.execute(
@@ -161,10 +176,13 @@ class ArtifactProvider(ABC, Generic[ArtifactInfoT]):
         """
         Filter artifacts based on exclude patterns.
 
-        :param exclude_patterns: The patterns to exclude.
-        :yields: Artifacts that do not match any of the exclude patterns.
+        :param exclude_patterns: artifact whose name matches any of
+            these patterns would be skipped.
+        :yields: artifacts that satisfy the filtering.
         """
 
         for artifact in self.list_artifacts():
-            if not any(pattern.search(artifact.id) for pattern in exclude_patterns):
+            if not exclude_patterns or not any(
+                pattern.search(artifact.id) for pattern in exclude_patterns
+            ):
                 yield artifact
