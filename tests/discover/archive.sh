050fa636296125f7d822aee1467cbc85c5ee6f3d
Cristian Le <git@lecris.dev>
2025-09-26 16:25:59 +0200

    Extend tests for url-type archive
    Signed-off-by: Cristian Le <git@lecris.dev>

---
+++ tests/discover/archive.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+
+PROVISION_HOW="${PROVISION_HOW:-local}"
+
+# Make it easier to see what went wrong
+export TMT_SHOW_TRACEBACK=1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "run=\$(mktemp -d)"
+        rlRun "pushd data"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Basic archive tests"
+        plan="/plans/fmf/archive-url"
+        rlRun -s "tmt run $run discover plans -n /plans/fmf/archive-url"
+        rlAssertGrep "2 test selected" $rlRun_LOG
+        plan_path="$run$plan"
+        step_workdir="$plan_path/discover/default-0"
+        rlAssertExists "$step_workdir/tests-main.tar.gz" 0 "Check that the archive is present"
+        rlAssertExists "$step_workdir/tests/test-main" 0 "Check that the extracted archive is present"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "rm -rf $run"
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
