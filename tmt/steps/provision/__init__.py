6d11588abe871a6fecd9c96e7bb2750086c3e2f4
Petr Šplíchal <psplicha@redhat.com>
2025-09-29 12:18:00 +0200

    Refresh package metadata before installing `rsync`
    Fix #3804.

--- tmt/steps/provision/__init__.py
+++ tmt/steps/provision/__init__.py
@@ -2843,6 +2843,7 @@ class GuestSsh(Guest):
         self.debug('rsync has not been confirmed on the guest, try installing it')
 
         try:
+            self.package_manager.refresh_metadata()
             self.package_manager.install(Package('rsync'))
 
         except Exception as exc:
