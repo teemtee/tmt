6e3712da7ce1b42c4e3479dc24f99334954333bb
Cristian Le <git@lecris.dev>
2025-10-03 14:35:10 +0200

    Add compat for TypeGuards
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tmt/_compat/typing.py
+++ tmt/_compat/typing.py
@@ -3,18 +3,25 @@ from __future__ import annotations
 import sys
 
 if sys.version_info >= (3, 10):
-    from typing import Concatenate, ParamSpec, TypeAlias
+    from typing import Concatenate, ParamSpec, TypeAlias, TypeGuard
 else:
-    from typing_extensions import Concatenate, ParamSpec, TypeAlias
+    from typing_extensions import Concatenate, ParamSpec, TypeAlias, TypeGuard
 
 if sys.version_info >= (3, 11):
     from typing import Self
 else:
     from typing_extensions import Self
 
+if sys.version_info >= (3, 13):
+    from typing import TypeIs
+else:
+    from typing_extensions import TypeIs
+
 __all__ = [
     "Concatenate",
     "ParamSpec",
     "Self",
     "TypeAlias",
+    "TypeGuard",
+    "TypeIs",
 ]
