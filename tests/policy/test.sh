3500fdb646e30c7e02bfb7f711bae19549c2a0d6
Miloš Prchlík <mprchlik@redhat.com>
2025-10-16 13:04:39 +0200

    Use `to_yaml` filter when changing complex objects in policies (#4144)
    Without the filter, some special Python values may produce unexpected
    values when modified by a policy. Namelt this has been seen with `None`
    which gets stringified and becomes `"None"`.
    
    Reported in https://issues.redhat.com/browse/TFT-3928

--- tests/policy/test.sh
+++ tests/policy/test.sh
@@ -34,8 +34,12 @@ rlJournalStart
         run_test environment.yaml /basic .environment "{\"FOO\":\"xyzzy\"}"
         run_test environment.yaml /full  .environment "{\"FOO\":\"baz\",\"QUX\":\"QUUX\"}"
 
-        run_test check.yaml /basic '.check | [.[] | with_entries(select(.key | test("^(how|result)$")))]' "[{\"how\":\"avc\",\"result\":\"respect\"}]"
-        run_test check.yaml /full  '.check | [.[] | with_entries(select(.key | test("^(how|result)$")))]' "[{\"how\":\"avc\",\"result\":\"info\"},{\"how\":\"dmesg\",\"result\":\"respect\"}]"
+        run_test check.yaml /basic '.check | [.[] | with_entries(select(.key | test("^(how|result)$")))]' "[{\"how\":\"avc\",\"result\":\"respect\"},{\"how\":\"journal\",\"result\":\"respect\"}]"
+        run_test check.yaml /full  '.check | [.[] | with_entries(select(.key | test("^(how|result)$")))]' "[{\"how\":\"avc\",\"result\":\"info\"},{\"how\":\"journal\",\"result\":\"respect\"},{\"how\":\"dmesg\",\"result\":\"respect\"}]"
+
+        # rhel-ci policy turns unset fields into "None" literal string in journal check plugin settings
+        # https://issues.redhat.com/browse/TFT-3928
+        run_test check.yaml /full '.check | [.[] | select(.how == "journal") | .unit]' '[null]'
     rlPhaseEnd
 
     rlPhaseStartTest "Test VALUE_SOURCE usage"
