af807665a063bf99ee4115cd9ef8c6bfaca38fb7
Cristian Le <git@lecris.dev>
2025-10-17 15:40:26 +0200

    Add a basic implementation
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tmt/steps/prepare/artifact/__init__.py
+++ tmt/steps/prepare/artifact/__init__.py
@@ -6,6 +6,7 @@ from tmt.container import container, field
 from tmt.log import Logger
 from tmt.steps import PluginOutcome
 from tmt.steps.prepare import PreparePlugin, PrepareStepData
+from tmt.steps.prepare.artifact.providers import _PROVIDER_REGISTRY
 from tmt.steps.provision import Guest
 from tmt.utils import Environment
 
@@ -42,6 +43,17 @@ class PrepareArtifact(PreparePlugin[PrepareArtifactData]):
         logger: Logger,
     ) -> PluginOutcome:
         outcome = super().go(guest=guest, environment=environment, logger=logger)
-        # TODO: Implementation
-        raise NotImplementedError
+        # TODO: Get and handle repositories
+        # TODO: Create the local repository
+        for raw_provider_id in self.data.provide:
+            provider_type = raw_provider_id.split(':')[0]
+            provider_class = _PROVIDER_REGISTRY.get_plugin(provider_type)
+            if not provider_class:
+                raise tmt.utils.PrepareError(f"Unknown provider type '{provider_type}'")
+            provider_id_sanitized = tmt.utils.sanitize_name(raw_provider_id, allow_slash=False)
+            logger = self._logger.descend(raw_provider_id)
+            provider = provider_class(raw_provider_id, logger=logger)
+            download_path = self.plan_workdir / "artifacts" / provider_id_sanitized
+            # TODO: Not using exclude_pattern yet.
+            provider.fetch_contents(guest, download_path)
         return outcome
