f730c173179c5c0d564f381b1e4108d1cc237468
Cristian Le <git@lecris.dev>
2025-10-24 13:11:16 +0200

    Drop the make generate and simplify comments
    Signed-off-by: Cristian Le <git@lecris.dev>

--- docs/ext/tmt_setup.py
+++ docs/ext/tmt_setup.py
@@ -14,25 +14,11 @@ if TYPE_CHECKING:
 # https://github.com/sphinx-doc/sphinx/issues/11556#issuecomment-3201453172
 
 
-def generate_tmt_docs(app: "Sphinx") -> None:
-    """
-    Run `make generate` to populate the auto-generated sources
-    """
-
-    conf_dir = Path(app.confdir)
-    subprocess.run(["make", "generate"], cwd=conf_dir, check=True)
-
-
 def setup(app: "Sphinx") -> None:
-    # Generate sources after loading configuration. That should build
-    # everything, including the logo, before Sphinx starts checking
-    # whether all input files exist.
-    app.connect("builder-inited", generate_tmt_docs)
-    # Check a cached version of the linkcheck results
+    # This is a meta extension that gatheres all of the individual extensions
+    # in the ext folder.
     app.setup_extension("linkcheck_cache")
-    # Do sphinx-apidoc
     app.setup_extension("sphinx_apidoc")
-    # Various other extensions
     app.setup_extension("generate_lint_checks")
     app.setup_extension("generate_hardware_matrix")
     app.setup_extension("generate_test_runner_guest_matrix")
