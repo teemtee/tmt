b1236aa959426b488b6cced8a55c87a81da0d28c
Therese Cornell <tcornell@redhat.com>
2025-10-27 09:40:02 -0400

    Use re.compile beforehand and change (?mi) to flags=...

--- tmt/base.py
+++ tmt/base.py
@@ -1133,10 +1133,11 @@ class Core(
             def detect_missing_required_properties(
                 error: jsonschema.ValidationError,
             ) -> LinterReturn:
-                match = re.search(
-                    r"(?mi)'([a-zA-Z0-9', \-]+)' is a required property",
-                    str(error),
+                pattern = re.compile(
+                    r"'([a-zA-Z0-9', \-]+)' is a required property",
+                    flags=re.MULTILINE | re.IGNORECASE,
                 )
+                match = pattern.search(str(error))
 
                 if not match:
                     return
