473d0aacaecc74e3fb09794d4a516abbc1df4012
vaibhavdaren <vaibhavdaren@gmail.com>
2025-10-27 03:36:03 +0000

    poc changes

--- tmt/steps/cleanup/internal.py
+++ tmt/steps/cleanup/internal.py
@@ -5,7 +5,9 @@ import tmt.log
 import tmt.steps
 import tmt.steps.cleanup
 import tmt.utils
+from tmt._compat.pathlib import Path
 from tmt.container import container
+
 from tmt.steps.provision import Guest
 
 
@@ -33,9 +35,46 @@ class CleanupInternal(tmt.steps.cleanup.CleanupPlugin[CleanupInternalData]):
         """
         Stop and remove guest
         """
+        from tmt.steps.prepare.artifact.providers import Repository
+        from tmt.steps.prepare.artifact.providers.repository import RepositoryFileProvider
+        logger.info(f'I AM ZEREF {guest} {logger}')
+        url = 'https://download.docker.com/linux/centos/docker-ce.repo'
+        # repo = Repository.from_url(url=url)
+        # logger.info("repo_ids", f"{repo.repo_ids}", "blue")
+        # logger.info("repo_ids", f"{repo.filename}", "blue")
+        # guest.package_manager.install_repository(repo)
+        # v = guest.execute(tmt.utils.ShellScript(f'cat /etc/yum.repos.d/{repo.filename}'))
+        # logger.info('check installed repo', f"{v.stdout}",'blue' )
+        # package_list = guest.package_manager.list_packages(repo)
+        # logger.info('check available packeages', f"{package_list}",'blue' )
 
+        # for i in package_list:
+        #     logger.info('ipackage', f"{i}",'blue' )
+        rp = RepositoryFileProvider(logger=logger, raw_provider_id=f'repository-url:{url}')
+        dp = Path()
+        rp.fetch_contents(guest=guest, download_path=dp)
+        afs = rp.artifacts
+        for i in afs:
+            logger.info('rpmi', i._raw_artifact)
+
+        # rp.artifacts
         outcome = super().go(guest=guest, environment=environment, logger=logger)
 
+        # # repo.install(guest=guest, logger=logger)
+        # # p = repo.rpms
+        # logger.info("", f"{len(p)}")
+        # #
+        # # rp = RepositoryFileProvider(raw_provider_id=url, logger=logger)
+        # rp.fetch_contents(guest=guest, download_path=dp)
+        # logger.info('BLAKZEREF ', rp.id)
+        # logger.info('BLAKZEREF ', rp.repo_filename)
+        # logger.info('BLAKZEREF ', rp._parsed_url)
+        # p1 = 0
+        # for i in rp.artifacts[:6]:
+        #     p1 = p1 + 1
+        #     if p1 == 6:
+        #         break
+        #     logger.info('WHITE_ZEREF ', str(i))
         # Nothing to do in dry mode
         if self.is_dry_run:
             return outcome
