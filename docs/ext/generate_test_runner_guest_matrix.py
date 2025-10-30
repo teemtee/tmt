0100519c7bbfaf07ac5ea6e54861e0146e96ead3
mcasquer <mcasquer@redhat.com>
2025-10-30 17:09:57 +0100

    Second round of 'error' naming alignment. Opportunistically removes some more formatted errors.
    Signed-off-by: mcasquer <mcasquer@redhat.com>

--- docs/ext/generate_test_runner_guest_matrix.py
+++ docs/ext/generate_test_runner_guest_matrix.py
@@ -47,24 +47,24 @@ def generate_test_runner_guest_matrix(app: "Sphinx") -> None:
             re.compile(pattern) for pattern in definitions['unsupported']
         ]
 
-    except re.error as exc:
-        raise GeneralError(f"Invalid 'unsupported' pattern '{exc.pattern}'.") from exc
+    except re.error as error:
+        raise GeneralError("Invalid 'unsupported' pattern.") from error
 
     try:
         unknown_environments: list[Pattern[str]] = [
             re.compile(pattern) for pattern in definitions['unknown']
         ]
 
-    except re.error as exc:
-        raise GeneralError(f"Invalid 'unknown' pattern '{exc.pattern}'.") from exc
+    except re.error as error:
+        raise GeneralError("Invalid 'unknown' pattern.") from error
 
     try:
         notes: dict[Pattern[str], Any] = {
             re.compile(note['pattern']): note for note in definitions['notes']
         }
 
-    except re.error as exc:
-        raise GeneralError(f"Invalid 'notes' pattern '{exc.pattern}'.") from exc
+    except re.error as error:
+        raise GeneralError("Invalid 'notes' pattern.") from error
 
     matrix: dict[str, list[tuple[str, str]]] = {}
 
