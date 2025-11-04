99bf53b5e2cc5e0f972dec3016e9aae42a93059a
Cristian Le <cristian.le@mpsd.mpg.de>
2025-11-04 20:15:35 +0100

    squash: fix typo
    Co-authored-by: tcornell-bus <tcornell@redhat.com>

--- docs/ext/sphinx_apidoc.py
+++ docs/ext/sphinx_apidoc.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 def sphinx_apidoc(app: "Sphinx") -> None:
     """
-    Run `shpinx-apidoc`
+    Run `sphinx-apidoc`
     """
 
     from sphinx.ext import apidoc
