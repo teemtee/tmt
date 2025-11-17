d3f40c9066db94af1e610109a817d2d5a04c2c4d
Miloš Prchlík <mprchlik@redhat.com>
2025-11-17 17:46:35 +0100

    Restraint context to govern restraint-related addition to execution (#4282)
    Similar to reboot, restarts, abort and pidfile contexts: bundles info
    connected to Restraint, and adds it to `execute` environment. Later, it
    will be used by `prepare` and `finish` too, and gain more functionality.
    Restraint-related info will not be spread across classes.

---
+++ tmt/steps/context/restraint.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+import tmt.log
+from tmt.container import container
+from tmt.utils import Environment, EnvVarValue
+
+
+@container
+class RestraintContext:
+    """
+    Provides restraint-related context for execution.
+    """
+
+    #: Phase owning this context.
+    # phase: tmt.steps.Phase
+
+    enabled: bool
+
+    #: Used for logging.
+    logger: tmt.log.Logger
+
+    taskname: Optional[str] = None
+
+    @property
+    def environment(self) -> Environment:
+        environment = Environment()
+
+        environment["TMT_RESTRAINT_COMPATIBLE"] = EnvVarValue(str(int(self.enabled)))
+
+        if self.enabled and self.taskname:
+            environment["RSTRNT_TASKNAME"] = EnvVarValue(self.taskname)
+
+        return environment
