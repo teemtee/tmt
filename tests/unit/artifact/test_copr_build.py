dcb1ef59042a7fb23dde6dbf60743a11ef51c6b0
Athrey Vinay <avinay@redhat.com>
2026-01-06 23:30:32 +0000

    support Pulp builds alongside regular Copr builds

--- tests/unit/artifact/test_copr_build.py
+++ tests/unit/artifact/test_copr_build.py
@@ -3,15 +3,34 @@ import pytest
 from tmt.steps.prepare.artifact.providers.copr_build import CoprBuildArtifactProvider
 from tmt.utils import GeneralError
 
-from . import MOCK_BUILD_ID_COPR, mock_copr_build_api_responses
+from . import MOCK_BUILD_ID_COPR, MOCK_BUILD_ID_PULP, MOCK_RPMS_PULP, mock_copr_build_api_responses
 
 
-def test_copr_valid_build(artifact_provider):
+def test_copr_valid_build_non_pulp(artifact_provider):
     provider = artifact_provider(f"copr.build:{MOCK_BUILD_ID_COPR}:fedora-41-x86_64")
     provider._session = mock_copr_build_api_responses(provider._session)
     assert isinstance(provider, CoprBuildArtifactProvider)
     assert provider.build_id == MOCK_BUILD_ID_COPR
     assert len(provider.artifacts) == 14
+    assert not provider.is_pulp
+
+    for artifact in provider.artifacts:
+        assert artifact._raw_artifact["url"].startswith(provider.result_url)
+
+
+def test_copr_pulp_build(artifact_provider, monkeypatch):
+    provider = artifact_provider(f"copr.build:{MOCK_BUILD_ID_PULP}:fedora-41-x86_64")
+    provider._session = mock_copr_build_api_responses(provider._session, storage="pulp")
+
+    monkeypatch.setattr(provider, "_fetch_results_json", lambda: MOCK_RPMS_PULP)
+
+    artifacts = provider.artifacts
+    assert provider.is_pulp
+    assert len(artifacts) == len(MOCK_RPMS_PULP)
+
+    for artifact in artifacts:
+        first_letter = artifact._raw_artifact["name"][0]
+        assert f"/Packages/{first_letter}/" in artifact._raw_artifact["url"]
 
 
 def test_copr_invalid_build_id(artifact_provider):
