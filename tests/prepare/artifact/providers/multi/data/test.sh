3a9909ba1c29635b7e6dfb5522fe606fc28be2b6
vaibhavdaren <vaibhavdaren@gmail.com>
2026-01-19 22:25:37 +0530

    address comments from code review

--- tests/prepare/artifact/providers/multi/data/test.sh
+++ tests/prepare/artifact/providers/multi/data/test.sh
@@ -5,10 +5,10 @@ set -ex
 rpm -q make
 
 # Verify make came from the tmt-artifact-shared repository
-dnf info --installed make | grep -Eq "From repository\s*:\s*tmt-artifact-shared"
+dnf info --installed make | grep -Eq "From repo(sitory)?\s*:\s*tmt-artifact-shared"
 
 # Verify docker-ce-cli is installed
 rpm -q docker-ce-cli
 
 # Verify docker-ce-cli came from the docker-ce repository
-dnf info --installed docker-ce-cli | grep -Eq "From repository :\s*docker-ce"
+dnf info --installed docker-ce-cli | grep -Eq "From repo(sitory)?\s*:\s*docker-ce"
