ecf36a0f2291c946a700551bd44d6aa06a99a7d9
Vaibhav D. Aren <vaibhavdaren@gmail.com>
2026-01-21 02:11:59 +0530

    Add example tests for artifact providers (#4449)
    Add simple, example-based integration tests for artifact providers:
    
    - koji.build: Fetches packages from Koji using build IDs
    - repository-url: Configures external package repositories
    - multi: Demonstrates using multiple providers together
    
    Once the design is approved, additional providers (koji.task, koji.nvr,
    file, copr.build, brew.*) can be added following the same pattern.
    
    Related to #4420

---
+++ tests/prepare/artifact/providers/multi/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Example test for using multiple artifact providers together
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+. ../../../../images.sh || exit 1
+. ../../lib/common.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "PROVISION_HOW=${PROVISION_HOW:-container}"
+        rlRun "pushd data"
+        rlRun "run=\$(mktemp -d)" 0 "Create run directory"
+
+        setup_distro_environment
+
+        # Get koji build ID for make
+        get_koji_build_id "make" "f${fedora_release}"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Test multiple providers with command-line override"
+        rlRun "tmt run -i $run --scratch -vv --all \
+            provision -h $PROVISION_HOW --image $TEST_IMAGE_PREFIX/$image_name \
+            prepare --how artifact \
+                --provide koji.build:$KOJI_BUILD_ID \
+                --provide repository-file:https://download.docker.com/linux/fedora/docker-ce.repo" 0 "Run with multiple providers"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "rm -rf $run"
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
---
+++ tests/prepare/artifact/providers/repository-file/data/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+# Verify docker-ce-cli is installed
+rpm -q docker-ce-cli
+
+# Verify it came from the docker-ce repository
+dnf info --installed docker-ce-cli | grep -Eq "From repo(sitory)?\s*:\s*docker-ce"
