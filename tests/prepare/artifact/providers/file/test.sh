c5bfd6264a3aabd0526fee772311e212051cab34
vaibhavdaren <vaibhavdaren@gmail.com>
2026-01-30 01:16:00 +0530

    dyanamic url for the packages

--- tests/prepare/artifact/providers/file/test.sh
+++ tests/prepare/artifact/providers/file/test.sh
@@ -7,11 +7,6 @@
 # Detect architecture
 ARCH=$(uname -m)
 
-# Remote URL uses cowsay (noarch package)
-REMOTE_RPM_URL="https://kojipkgs.fedoraproject.org/packages/cowsay/3.8.4/3.fc43/noarch/cowsay-3.8.4-3.fc43.noarch.rpm"
-# Local file uses figlet (arch-specific package)
-LOCAL_RPM_URL="https://kojipkgs.fedoraproject.org/packages/figlet/2.2.5/32.20151018gita565ae1.fc43/${ARCH}/figlet-2.2.5-32.20151018gita565ae1.fc43.${ARCH}.rpm"
-
 rlJournalStart
     rlPhaseStartSetup
         rlRun "PROVISION_HOW=${PROVISION_HOW:-container}"
@@ -21,11 +16,35 @@ rlJournalStart
 
         setup_distro_environment
 
-        # Download an RPM file for local file test
-        rlRun "curl -L -o $rpm_dir/figlet.rpm $LOCAL_RPM_URL" 0 "Download RPM for local test"
+        # Choose a tag – change to 'rawhide' or e.g. 'f44' for newer packages if desired
+        TAG="f43"
 
-        # Download multiple RPMs into a directory for directory test using dnf download
-        # Using different packages (jq and nano) to test directory functionality
+        # 1. REMOTE URL (noarch) ---
+        COWSAY_NVR=$(koji latest-pkg "$TAG" cowsay --quiet | awk '{print $1}')
+        if [ -z "$COWSAY_NVR" ]; then
+            rlFail "Failed to find latest cowsay in tag $TAG"
+            exit 1
+        fi
+
+        # Construct URL: Parses NVR (Name-Version-Release) and assembles Koji URL
+        # Regex explanation: ^(.*)-([^-]+)-([^-]+)$ captures Name, Version, Release
+        REMOTE_RPM_URL=$(echo "$COWSAY_NVR" | sed -E "s|^(.*)-([^-]+)-([^-]+)$|https://kojipkgs.fedoraproject.org/packages/\1/\2/\3/noarch/&.noarch.rpm|")
+
+        rlLog "Using cowsay URL: $REMOTE_RPM_URL"
+
+        # 2. LOCAL FILE (arch-specific) ---
+        FIGLET_NVR=$(koji latest-pkg "$TAG" figlet --quiet | awk '{print $1}')
+        if [ -z "$FIGLET_NVR" ]; then
+            rlFail "Failed to find latest figlet in tag $TAG"
+            exit 1
+        fi
+
+        # Construct URL using current $ARCH
+        LOCAL_RPM_URL=$(echo "$FIGLET_NVR" | sed -E "s|^(.*)-([^-]+)-([^-]+)$|https://kojipkgs.fedoraproject.org/packages/\1/\2/\3/${ARCH}/&.${ARCH}.rpm|")
+
+        rlLog "Using figlet URL: $LOCAL_RPM_URL"
+        rlRun "curl -L -o $rpm_dir/figlet.rpm $LOCAL_RPM_URL" 0 "Download figlet RPM for local test"
+
         rlRun "multi_rpm_dir=$(mktemp -d)" 0 "Create directory for multiple RPMs"
         rlRun "dnf download --destdir=$multi_rpm_dir jq nano" 0 "Download jq and nano RPMs using dnf"
     rlPhaseEnd
