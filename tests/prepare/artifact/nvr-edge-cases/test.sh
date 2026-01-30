c59cb96e1ec02448904bb900c37de509864f5076
vaibhavdaren <vaibhavdaren@gmail.com>
2026-01-30 20:13:09 +0530

    Add integration test for NVR edge cases with file provider
    Add test to verify file artifact provider correctly handles
    NVR edge cases.
    
    Related: #4546

---
+++ tests/prepare/artifact/nvr-edge-cases/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Example test for file artifact provider
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+. ../../../images.sh || exit 1
+. ../lib/common.sh || exit 1
+
+# Force x86_64 architecture
+ARCH=x86_64
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "PROVISION_HOW=${PROVISION_HOW:-container}"
+        rlRun "pushd data"
+        rlRun "run=$(mktemp -d)" 0 "Create run directory"
+
+        setup_distro_environment
+
+        rlRun "rpm_dir=$(mktemp -d)" 0 "Create directory for RPMs"
+        # Use $fedora_release (set by setup_distro_environment) to ensure packages match test distro
+        rlRun "dnf download --forcearch=$ARCH --releasever=$fedora_release --destdir=$rpm_dir jq nano" 0 "Download jq and nano RPMs using dnf"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Test file provider with NVR edge cases"
+        rlRun "tmt run -i $run --scratch -vvv --all \
+            provision -h $PROVISION_HOW --image $TEST_IMAGE_PREFIX/$image_name \
+            prepare --how artifact --provide file:$rpm_dir" 0 "Run with file provider (multiple RPMs)"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "rm -rf $run $rpm_dir"
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
