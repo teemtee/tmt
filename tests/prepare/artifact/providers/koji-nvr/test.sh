1e67cc3be9b1fba4d6a8e87f8a1db480662f15f0
Vaibhav D. Aren <vaibhavdaren@gmail.com>
2026-02-03 20:34:26 +0530

    Add integration test for `koji.nvr` artifact provider (#4513)
    Add test for koji.nvr provider following the same format as the
    existing koji.build test. Also add get_koji_nvr() helper function
    to lib/common.sh for fetching the latest NVR for a package.
    
    Related to https://github.com/teemtee/tmt/issues/4420

---
+++ tests/prepare/artifact/providers/koji-nvr/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Example test for koji.nvr artifact provider
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+. ../../../../images.sh || exit 1
+. ../../lib/common.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "PROVISION_HOW=${PROVISION_HOW:-container}"
+        rlRun "pushd data"
+        rlRun "run=\$(mktemp -d)" 0 "Create run directory"
+
+        setup_distro_environment
+
+        # Get koji NVR for make
+        get_koji_nvr "make" "f${fedora_release}"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Test koji.nvr provider"
+        rlRun "tmt run -i $run --scratch -vvv --all \
+            provision -h $PROVISION_HOW --image $TEST_IMAGE_PREFIX/$image_name \
+            prepare --how artifact --provide koji.nvr:$KOJI_NVR" 0 "Run with koji.nvr provider"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "rm -rf $run"
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
