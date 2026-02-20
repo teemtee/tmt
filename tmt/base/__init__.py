f404114f3a8207d6feda6677ea96a2005ae467b6
Cristian Le <git@lecris.dev>
2026-02-20 11:45:57 +0100

    [DEBUG] Try without re-exports

--- tmt/base/__init__.py
+++ tmt/base/__init__.py
@@ -2,34 +2,4 @@
 Base Metadata Classes
 """
 
-from tmt.base.core import (
-    Clean,
-    Core,
-    DependencyFile,
-    DependencyFmfId,
-    DependencySimple,
-    Link,
-    Links,
-    Plan,
-    Run,
-    Status,
-    Story,
-    Test,
-    Tree,
-)
-
-__all__ = [
-    "Clean",
-    "Core",
-    "DependencyFile",
-    "DependencyFmfId",
-    "DependencySimple",
-    "Link",
-    "Links",
-    "Plan",
-    "Run",
-    "Status",
-    "Story",
-    "Test",
-    "Tree",
-]
+# TODO: Export the main definitions at this level
