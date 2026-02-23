a93db277e2164cfc059f2a48bef169f11005841d
Miloš Prchlík <mprchlik@redhat.com>
2026-02-23 18:15:12 +0100

    squash: address comments

--- tests/__init__.py
+++ tests/__init__.py
@@ -43,6 +43,7 @@ class RunTmt(Protocol):
         command: Optional[click.BaseCommand] = None,
         input: Optional[Union[str, bytes, IO[Any]]] = None,
         env: Optional[Mapping[str, Optional[str]]] = None,
+        catch_exceptions: bool = True,
         color: bool = False,
         **kwargs: Any,
     ) -> click.testing.Result:
