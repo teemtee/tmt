fb19a24f277b66921ca8f2ed0db2815373c10019
Cristian Le <git@lecris.dev>
2026-03-05 16:59:21 +0100

    Simplify the log._Formatter
    Comparing with the current implementation, the only difference we make is injecting `_decolorize` which could equally be added to `formatMessage`
    
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tmt/log.py
+++ tmt/log.py
@@ -333,41 +333,8 @@ class _Formatter(logging.Formatter):
 
         self._decolorize = create_decolorizer(apply_colors)
 
-    def format(self, record: logging.LogRecord) -> str:
-        if self.usesTime():
-            record.asctime = self.formatTime(record, self.datefmt)
-
-        # When message already exists, do nothing - it either some other logging subsystem,
-        # or tmt's own, already rendered message.
-        if hasattr(record, 'message'):
-            pass
-
-        # Otherwise render the message.
-        elif record.msg and record.args:
-            record.message = record.msg % record.args
-
-        else:
-            record.message = record.msg
-
-        # Original code from Formatter.format() - hard to inherit when overriding
-        # Formatter.format()...
-        s = self._decolorize(self.formatMessage(record))
-        # SIM102: Use a single `if` statement instead of nested `if` statements. Keeping for
-        # readability.
-        if record.exc_info:  # noqa: SIM102
-            # Cache the traceback text to avoid converting it multiple times
-            # (it's constant anyway)
-            if not record.exc_text:
-                record.exc_text = self.formatException(record.exc_info)
-        if record.exc_text:
-            if s[-1:] != "\n":
-                s = s + "\n"
-            s = s + record.exc_text
-        if record.stack_info:
-            if s[-1:] != "\n":
-                s = s + "\n"
-            s = s + self.formatStack(record.stack_info)
-        return s
+    def formatMessage(self, record: logging.LogRecord) -> str:
+        return self._decolorize(super().formatMessage(record))
 
 
 class LogfileFormatter(_Formatter):
