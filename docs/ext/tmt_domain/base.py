teemtee/tmt

commit db507cb44da8b01a708c3ac2210c76a01406c3bf

Migrate the `generate_stories` script to tmt sphinx domain documenter (#4288)
This is the first example case of how the sphinx tmt domain is
utilized.

Depends on: https://github.com/teemtee/tmt/pull/4287

---------

Signed-off-by: Cristian Le <git@lecris.dev>
Co-authored-by: Petr Šplíchal <psplicha@redhat.com>

docs/ext/tmt_domain/domain.py
@@ -7,12 +7,15 @@ from collections.abc import Iterable
 from functools import cached_property
 
 from docutils import nodes
-from sphinx.domains import Domain
+from sphinx.domains import Domain, ObjType
+from sphinx.roles import XRefRole
 from sphinx.util import logging
 from sphinx.util.nodes import make_refnode
 
 import tmt
 import tmt.log
+
+from .story import AutoStoryDirective, StoryDirective, StoryIndex
 
 if typing.TYPE_CHECKING:
     from sphinx.addnodes import pending_xref
@@ -38,10 +41,19 @@ class TmtDomain(Domain):
     # Required attributes to setup the domain.
     name = "tmt"
     label = "Internal tmt sphinx domain"
-    roles = {}
-    directives = {}
-    indices = []
-    object_types = {}
+    roles = {
+        "story": XRefRole(),
+    }
+    directives = {
+        "autostory": AutoStoryDirective,
+        "story": StoryDirective,
+    }
+    indices = [
+        StoryIndex,
+    ]
+    object_types = {
+        "story": ObjType("story", "story"),
+    }
     initial_data = {
         "objects": {},
         "tmt_trees": {},

docs/ext/tmt_domain/story.py
@@ -1,0 +1,262 @@
+import re
+import typing
+from collections.abc import Callable, Iterable
+
+import fmf.utils
+from docutils import nodes
+from docutils.parsers.rst import directives
+from sphinx import addnodes
+from sphinx.domains import Index, IndexEntry
+from sphinx.util import logging
+
+import tmt.base
+from tmt._compat.pathlib import Path
+from tmt.base import Story
+from tmt.utils.git import web_git_url
+
+from .autodoc import Content
+from .base import TmtAutodocDirective, TmtDirective
+
+if typing.TYPE_CHECKING:
+    from .domain import TmtDomain
+
+
+logger = logging.getLogger(__name__)
+
+EXAMPLE_SYNTAX = re.compile(r"^# syntax: (?P<syntax>[a-z]+)")
+DOCS_LINK = re.compile(r"^/docs(?P<doc>/.+)\.rst(?:#.*)?$")
+
+
+# Note: Cannot make this a TmtObjectDirective because this injects a signature node that we
+# otherwise want to remove and it cannot inject titles as we want instead.
+# TODO: Move to `ObjectDescription` once upstream generalizes it
+#  https://github.com/sphinx-doc/sphinx/issues/14042
+class StoryDirective(TmtDirective):
+    has_content = True
+    required_arguments = 1
+    option_spec = {
+        "title": directives.unchanged_required,
+    }
+
+    def run(self):
+        name = self.arguments[0]
+        title = self.options.get("title", name.split("/")[-1])
+
+        self.tmt_domain.note_object(
+            typ="story",
+            name=name,
+            entry=IndexEntry(
+                name=title,
+                subtype=0,
+                docname=self.env.docname,
+                # TODO: Would this anchor actually work?
+                anchor=name,
+                extra="",
+                qualifier="",
+                # TODO: What to actually add for the descr?
+                descr=title,
+            ),
+        )
+
+        # Replicating much of the logic of `ObjectDescription`
+        target_node = nodes.target(ids=[name])
+        node = nodes.paragraph()
+        index_node = addnodes.index(entries=[])
+        node += self.parse_content_to_nodes(allow_section_headings=True)
+        return [
+            index_node,
+            target_node,
+            *node.children,
+        ]
+
+
+class AutoStoryDirective(TmtAutodocDirective[Story]):
+    option_spec = {
+        "title": directives.unchanged_required,
+    }
+
+    @property
+    def story(self) -> Story:
+        return self.tmt_object
+
+    def _get_tmt_object(self) -> None:
+        name = self.arguments[0]
+        name_re = f"^{re.escape(name)}$"
+        stories = self.tmt_tree.stories(names=[name_re], whole=True)
+        if not stories:
+            raise ValueError(f"Story {name} not found in tree {self.tmt_tree.root}")
+        if len(stories) > 1:
+            raise ValueError(f"Multiple stories matched '{name_re}'")
+        self.tmt_object = stories[0]
+        for source in self.story.node.sources:
+            self.env.note_dependency(source)
+
+    def _has_story_attr(self, attr: str) -> bool:
+        value = getattr(self.story, attr)
+        if not value:
+            # Attribute was not provided
+            return False
+        if not self.story.node.parent:
+            # There is no parent story, so we know this attribute belongs to this
+            return True
+        # Otherwise we check if we have inherited the value (return False if it was inherited)
+        return value != self.story.node.parent.get(attr)
+
+    def _add_title_content(self, title: str) -> None:
+        # TODO: Find a better way to insert the title
+        self.append(title)
+        self.append("=" * len(title))
+        self.new_line()
+
+    def _add_story_content(
+        self,
+        section: str,
+        *,
+        source_suffix: str = "",
+        content: typing.Optional[str] = None,
+        transform_line: Callable[[str], str] = lambda x: x,
+        new_line: bool = True,
+    ) -> None:
+        """
+        Add content lines from a story section in the current content object.
+
+        :param section: story section used as the source reference
+        :param source_suffix: additional info used for the source annotation
+        :param content: content to use instead of the ``section`` story attribute
+        :param transform_line: transformation function applied for each line in the content
+        :param new_line: make sure the content is terminated by a blank new line
+        """
+        if not self._has_story_attr(section):
+            # Do nothing if the story does not have the current section
+            return
+
+        # TODO: better handling of `source` reference directly from the fmf files.
+        source = f"story[{self.story.name}].{section}{source_suffix}"
+        content = content or getattr(self.story, section)
+        assert isinstance(content, str)
+        if not content.strip():
+            return
+        content_lines = content.splitlines()
+        content_lines = [transform_line(line) for line in content_lines]
+        if new_line and content_lines[-1].strip():
+            content_lines.append("")
+        section_content = Content(
+            content_lines,
+            source=source,
+            parent=self.content,
+            parent_offset=next(self.content_offset_count),
+        )
+        self.content.extend(section_content)
+
+    def _generate_leaf_story_content(self) -> None:
+        title = self.story.name.split("/")[-1]
+        if self._has_story_attr("title"):
+            # We only use the title if it is specific to the current story (not inherited)
+            title = self.story.title
+        if "title" in self.options:
+            title = self.options["title"]
+        with self.directive(
+            "tmt:story",
+            self.story.name,
+            title=title,
+        ):
+            self._add_title_content(title)
+            self._add_story_content("summary")
+            self._add_story_content("story", transform_line=lambda line: f"*{line}*")
+            if not self.story.implemented:
+                # We are assuming we only document leaf stories
+                with self.directive("note"):
+                    self.append("This is a draft, the story is not implemented yet.")
+            self._add_story_content("description")
+            if self._has_story_attr("example"):
+                self.append("**Examples:**")
+                self.new_line()
+                for ind, example in enumerate(self.story.example):
+                    syntax = "yaml"
+                    if match := EXAMPLE_SYNTAX.search(example):
+                        syntax = match.group("syntax")
+                        example = example.replace(match.group(0), "")
+                    with self.directive("code-block", syntax):
+                        self._add_story_content(
+                            "example",
+                            source_suffix=f"[{ind}]",
+                            content=example,
+                        )
+            self.append(f"**Status:** {fmf.utils.listed(self.story.status) or 'idea'}")
+            self.new_line()
+            self._generate_links_content()
+
+    def _generate_branch_story_content(self) -> None:
+        if "title" in self.options:
+            title = self.options["title"]
+        elif self._has_story_attr("title"):
+            title = self.story.title
+        else:
+            title = self.story.name.split("/")[-1]
+
+        with self.directive("tmt:story", self.story.name, title=title):
+            self._add_title_content(title)
+            self._add_story_content("story", transform_line=lambda line: f"*{line}*")
+            self._generate_links_content()
+
+            for child in self.story.node.children:
+                with self.directive("tmt:autostory", f"{self.story.name}/{child}"):
+                    pass
+
+    def _generate_links_content(self) -> None:
+        if not self.story.link:
+            return
+        with self.new_list():
+            for ind, link in enumerate(self.story.link.get()):
+                self.new_item()
+                self._add_story_content(
+                    "link",
+                    source_suffix=f"[{ind}]",
+                    content=self._handle_link(link),
+                    new_line=False,
+                )
+
+    def _generate_autodoc_content(self) -> None:
+        if self.story.node.children:
+            self._generate_branch_story_content()
+        else:
+            self._generate_leaf_story_content()
+
+    def _handle_link(self, link: tmt.base.Link) -> str:
+        relation = link.relation.replace("relates", "relates-to").replace("-", " ").capitalize()
+        # TODO: Generalize this handling to a custom xref role
+        # TODO: Handle tmt objects, plugins etc.
+        if match := DOCS_LINK.search(link.target):
+            # Special handling for doc links
+            target = f":doc:`{match.group('doc')}`"
+        elif link.target.startswith(("https://", "http://")):
+            # External links
+            target = f"`{link.target} <{link.target}>`_"
+        elif (self.tmt_tree.root / Path(link.target).unrooted()).exists():
+            # If these are files relative to the tmt tree use git repo links
+            target_url = web_git_url(
+                self.story.fmf_id.url, self.story.fmf_id.ref, Path(link.target)
+            )
+            label = link.target
+            target = f"`{label} <{target_url}>`_"
+        else:
+            target = f"``{link.target}``"
+        return f"{relation} {target} {link.note or ''}"
+
+
+class StoryIndex(Index):
+    name = "storyindex"
+    localname = "Tmt Story Index"
+    shortname = "story"
+    domain: "TmtDomain"
+
+    def generate(
+        self, docnames: Iterable[str] | None = None
+    ) -> tuple[list[tuple[str, list[IndexEntry]]], bool]:
+        content = {}
+        # TODO: What to actually use for key-value in the index?
+        for story, index_item in self.domain.objects.get("story", {}).items():
+            index_key = story.split("/")[0]
+            entries = content.setdefault(index_key, [])
+            entries.append(index_item)
+        return sorted(content.items()), True

commit a42b13c19bd2bdeb8ba00de060851fdba8de6d37

Fix the xref formatting of `:tmt:story:` role to normal text (#4599)
This fixes a regression after #4288 where the format was changed to a
code-format.

Fixes #4579

---------

Signed-off-by: Cristian Le <git@lecris.dev>

docs/ext/tmt_domain/base.py
@@ -13,10 +13,12 @@ This split is similar to the python sphinx domain workflow.
 import abc
 import typing
 from functools import cached_property
-from typing import Generic
+from typing import Generic, Optional
 
 from docutils.parsers.rst import directives
+from sphinx import addnodes
 from sphinx.directives import ObjDescT, ObjectDescription
+from sphinx.roles import XRefRole
 from sphinx.util import logging
 from sphinx.util.docutils import SphinxDirective
 
@@ -26,6 +28,8 @@ from tmt._compat.pathlib import Path
 from .autodoc import AutodocDirectiveBase
 
 if typing.TYPE_CHECKING:
+    from docutils.nodes import Element, Node, TextElement, system_message
+
     from .domain import TmtDomain
 
 
@@ -129,3 +133,45 @@ class TmtAutodocDirective(
         Get the :py:attr:`tmt_object`
         """
         raise NotImplementedError
+
+
+class TmtXRefRole(XRefRole):
+    """
+    Base tmt role for cross-referencing to a tmt object.
+
+    This saves some additional options indicating how to expand the role during the
+    call to :py:meth:`sphinx.domains.Domain.resolve_xref`.
+    """
+
+    #: Whether to use the tmt object's name/title key instead of the address
+    use_obj_name: bool = False
+
+    def __init__(
+        self,
+        fix_parens: bool = False,
+        lowercase: bool = False,
+        nodeclass: Optional[type["Element"]] = None,
+        innernodeclass: Optional[type["TextElement"]] = None,
+        warn_dangling: bool = False,
+        use_obj_name: Optional[bool] = None,
+    ) -> None:
+        if use_obj_name is not None:
+            self.use_obj_name = use_obj_name
+        super().__init__(
+            fix_parens=fix_parens,
+            lowercase=lowercase,
+            nodeclass=nodeclass,
+            innernodeclass=innernodeclass,
+            warn_dangling=warn_dangling,
+        )
+
+    def create_xref_node(self) -> tuple[list["Node"], list["system_message"]]:
+        # We do not have access to the actual tmt object until `Domain.resolve_xref` is
+        # being executed. We can only save the intent in the pending_xref node's data
+        # and handle it accordingly in the `resolve_xref`. See similar logic used with
+        # `refexplicit` (`sphinx.domains.std`)
+        nodes, messages = super().create_xref_node()
+        ref_node = nodes[0]
+        assert isinstance(ref_node, addnodes.pending_xref)
+        ref_node["tmtrefuseobjname"] = self.use_obj_name
+        return nodes, messages

docs/ext/tmt_domain/domain.py
@@ -8,14 +8,13 @@ from functools import cached_property
 
 from docutils import nodes
 from sphinx.domains import Domain, ObjType
-from sphinx.roles import XRefRole
 from sphinx.util import logging
 from sphinx.util.nodes import make_refnode
 
 import tmt
 import tmt.log
 
-from .story import AutoStoryDirective, StoryDirective, StoryIndex
+from .story import AutoStoryDirective, StoryDirective, StoryIndex, StoryRole
 
 if typing.TYPE_CHECKING:
     from sphinx.addnodes import pending_xref
@@ -42,7 +41,7 @@ class TmtDomain(Domain):
     name = "tmt"
     label = "Internal tmt sphinx domain"
     roles = {
-        "story": XRefRole(),
+        "story": StoryRole(),
     }
     directives = {
         "autostory": AutoStoryDirective,
@@ -137,9 +136,12 @@ class TmtDomain(Domain):
         typ_objects = self.objects.get(obj_types[0], {})
         if not (obj := typ_objects.get(target)):
             return None
-        # Fix title from the actual object's title.
-        # Check `refexplicit` aka `has_explicit_title` if we actually need to do it.
-        if not node.get("refexplicit", False):
+        # Fix title from the actual object's title if needed.
+        # - `tmtrefuseobjname` aka `use_obj_name` is defined for each `TmtXRefRole` and
+        #   specifies if we want to use the tmt object's name/title (saved as `obj.name`)
+        # - `refexplicit` aka `has_explicit_title` is `True` if the role was defined like
+        #   :tmt:story:`Other title</spec/plans>` (we do not need to change the title then)
+        if node.get("tmtrefuseobjname", False) and not node.get("refexplicit", False):
             contnode.pop(0)
             contnode += nodes.Text(obj.name)
         return make_refnode(

docs/ext/tmt_domain/story.py
@@ -15,7 +15,7 @@ from tmt.base.core import Link, Story
 from tmt.utils.git import web_git_url
 
 from .autodoc import Content
-from .base import TmtAutodocDirective, TmtDirective
+from .base import TmtAutodocDirective, TmtDirective, TmtXRefRole
 
 if typing.TYPE_CHECKING:
     from .domain import TmtDomain
@@ -258,3 +258,12 @@ class StoryIndex(Index):
             entries = content.setdefault(index_key, [])
             entries.append(index_item)
         return sorted(content.items()), True
+
+
+class StoryRole(TmtXRefRole):
+    # The default literal node formats the string as a code.
+    # Using inline instead, same as sphinx's `ref` role (`sphinx.domains.std`)
+    innernodeclass = nodes.inline
+    # We want to use the story titles here. See `tmt_domain.note_object` call in
+    # `StoryDirective.run` for the index entry that we created.
+    use_obj_name = True