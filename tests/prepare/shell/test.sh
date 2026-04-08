79049983d1425c5c6ce9522db5183953779da8f2
Miroslav Vadkerti <mvadkert@redhat.com>
2026-04-08 03:45:18 +0200

    Address review comments, improve prepare/shell test
    Script from remote url is broken, see #4785
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- tests/prepare/shell/test.sh
+++ tests/prepare/shell/test.sh
@@ -12,53 +12,44 @@ rlJournalStart
         rlRun "pushd data"
     rlPhaseEnd
 
-    if [ "$IMAGE_MODE" = "yes" ]; then
-        while IFS= read -r image; do
-            provision_opts="--how=virtual --image=$image"
+    while IFS= read -r image; do
+        if [ -n "$image" ]; then
+            image_opt="--image=$image"
+        fi
 
-            rlPhaseStartTest "[$image] Custom Script"
-                rlRun -s "tmt run -arv provision $provision_opts plan -n custom" 0 "Prepare using a custom script"
+        assert_image_mode() {
+            rlAssertGrep "building container image from collected commands" $rlRun_LOG
+            rlAssertGrep "switching to new image" $rlRun_LOG
+            rlAssertGrep "rebooting to apply new image" $rlRun_LOG
+        }
 
-                # Verify image mode specific behavior (debug-level messages
-                # like "Collected command for Containerfile" are not visible
-                # at -arv verbosity)
-                rlAssertGrep "building container image from collected commands" $rlRun_LOG
-                rlAssertGrep "switching to new image" $rlRun_LOG
-                rlAssertGrep "rebooting to apply new image" $rlRun_LOG
-            rlPhaseEnd
-
-            rlPhaseStartTest "[$image] Commandline Script"
-                rlRun "tmt run -arv provision $provision_opts plan -n custom \
-                    prepare -h shell -s './prepare.sh'" 0 "Prepare using a custom script from cmdline"
-            rlPhaseEnd
-
-            # TODO: On image mode, /tmp is a tmpfs cleared on reboot. The inner
-            # test plan verifies files in /tmp which don't persist. Similarly,
-            # the remote script URL plan's $TMT_PREPARE_SHELL_URL_REPOSITORY
-            # env var is empty during the Containerfile RUN. Both need image
-            # mode support in tmt core before these tests can pass.
-        done <<< "$IMAGES"
-    else
         rlPhaseStartTest "Custom Script"
-            rlRun -s "tmt run -arv provision --how=$PROVISION_HOW plan -n custom" 0 "Prepare using a custom script"
+            rlRun -s "tmt run -arv provision --how=$PROVISION_HOW $image_opt plan -n custom" 0 "Prepare using a custom script"
+            assert_image_mode
         rlPhaseEnd
 
         rlPhaseStartTest "Commandline Script"
-            rlRun "tmt run -arv provision --how=$PROVISION_HOW plan -n custom \
+            rlRun "tmt run -arv provision --how=$PROVISION_HOW $image_opt plan -n custom \
                 prepare -h shell -s './prepare.sh'" 0 "Prepare using a custom script from cmdline"
+            assert_image_mode
         rlPhaseEnd
 
         rlPhaseStartTest "Multiple Commandline Scripts"
-            rlRun "tmt run -arv provision --how=$PROVISION_HOW plans -n multiple \
-                prepare -h shell -s 'touch /tmp/first' -s 'touch /tmp/second'"
+            rlRun "tmt run -arv provision --how=$PROVISION_HOW $image_opt plans -n multiple \
+                prepare -h shell -s 'touch /var/tmp/first' -s 'touch /var/tmp/second'"
+            assert_image_mode
         rlPhaseEnd
 
-        rlPhaseStartTest "Remote Script"
-            rlRun -s "tmt -vvv run provision --how=$PROVISION_HOW prepare finish cleanup plan -n url" 0 "Prepare using a remote script"
-            rlAssertGrep "Hello world" "$rlRun_LOG" #check for the prepare script
-            rlAssertGrep "third" "$rlRun_LOG" # check for the finish script
-        rlPhaseEnd
-    fi
+        # TODO: #4785 Preparing from a remote script is broken in Image Mode
+        if [ "$IMAGE_MODE" != "yes" ]; then
+            rlPhaseStartTest "Remote Script"
+                rlRun -s "tmt -vvv run provision --how=$PROVISION_HOW $image_opt prepare finish cleanup plan -n url" 0 "Prepare using a remote script"
+                rlAssertGrep "Hello world" "$rlRun_LOG" #check for the prepare script
+                rlAssertGrep "third" "$rlRun_LOG" # check for the finish script
+                assert_image_mode
+            rlPhaseEnd
+        fi
+    done <<< "$IMAGES"
 
     rlPhaseStartCleanup
         rlRun "popd"
