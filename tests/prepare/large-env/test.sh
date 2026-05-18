4e668a2500d6a0356aa4d5ce7cd289ffcc37efb5
James Molet <jmolet@redhat.com>
2026-05-18 09:48:53 -0400

    Address review feedback: fix CI, add release note
    - Generate large-env.yaml inside the fmf data/ tree so tmt accepts
      it as an --environment-file source (fixes CI failure)
    - Add TODO comment for enabling container/virtual provision methods
    - Change link from verifies to relates (fix is partial for #4518)
    - Add release note for 1.74

--- tests/prepare/large-env/test.sh
+++ tests/prepare/large-env/test.sh
@@ -7,6 +7,8 @@ rlJournalStart
         rlRun "PROVISION_HOW=${PROVISION_HOW:-virtual}"
         rlRun "IMAGE_MODE=${IMAGE_MODE:-yes}"
 
+        # TODO: Enable IMAGES for container and virtual provision methods
+        # once the large-env fix is verified for those backends.
         if [ "$IMAGE_MODE" = "yes" ]; then
             rlRun "IMAGES='$TEST_IMAGE_MODE_IMAGES'"
 
@@ -21,15 +23,18 @@ rlJournalStart
         fi
 
         rlRun "run=\$(mktemp -d -p /var/tmp)" 0 "Create run directory"
-
-        # Generate a ~67KB base64 string simulating an encoded secret.
-        # This exceeds ARG_MAX when multiplied across Containerfile RUN
-        # directives (the bug), but is small enough to pass through
-        # individual SSH execute() calls.
-        rlRun "python3 -c 'import base64; print(\"LARGE_SECRET: \" + base64.b64encode(b\"x\" * 50000).decode())' > \$run/large-env.yaml"
-        rlRun "rlLogInfo \"LARGE_SECRET size: \$(wc -c < \$run/large-env.yaml) bytes\""
-
         rlRun "pushd data"
+
+        # Size the env dynamically so 2 shell prepare steps produce a
+        # Containerfile that exceeds MAX_ARG_STRLEN (PAGE_SIZE * 32).
+        # Each RUN inlines the full export; raw_bytes * 4/3 (base64) ≈
+        # half the limit, so two RUNs combined just exceed it.
+        rlRun "max_arg_strlen=\$(( \$(getconf PAGE_SIZE) * 32 ))"
+        rlRun "raw_bytes=\$(( max_arg_strlen * 5 / 12 ))"
+        rlRun "rlLogInfo \"MAX_ARG_STRLEN: \$max_arg_strlen  raw_bytes: \$raw_bytes\""
+        rlRun "python3 -c 'import base64; print(\"LARGE_SECRET: \" + base64.b64encode(b\"x\" * '\$raw_bytes').decode())' > large-env.yaml"
+        rlRun "rlLogInfo \"LARGE_SECRET size: \$(wc -c < large-env.yaml) bytes\""
+
         rlRun "export TMT_BOOT_TIMEOUT=300"
         rlRun "export TMT_CONNECT_TIMEOUT=300"
     rlPhaseEnd
@@ -38,7 +43,7 @@ rlJournalStart
         phase_prefix="$(test_phase_prefix "$image")"
 
         rlPhaseStartTest "$phase_prefix Prepare/install with large environment"
-            rlRun -s "tmt -vvv run --scratch -i \$run --environment-file \$run/large-env.yaml provision --how $PROVISION_HOW --image $image"
+            rlRun -s "tmt -vvv run --all --scratch -i \$run --environment-file large-env.yaml provision --how $PROVISION_HOW --image $image"
 
             rlAssertGrep "building container image" $rlRun_LOG
             rlAssertGrep "switching to new image" $rlRun_LOG
@@ -47,6 +52,7 @@ rlJournalStart
     done <<< "$IMAGES"
 
     rlPhaseStartCleanup
+        rlRun "rm -f large-env.yaml" 0 "Remove generated env file"
         rlRun "popd"
         rlRun "rm -rf \$run" 0 "Remove run directory"
     rlPhaseEnd
