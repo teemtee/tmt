d2eab20255816bc1e26b7eded32772e018745534
Cristian Le <git@lecris.dev>
2026-05-28 13:19:09 +0200

    Make providers/file generic
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/prepare/artifact/providers/file/data/test.sh
+++ tests/prepare/artifact/providers/file/data/test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -ex
 
-for package in cowsay figlet boxes fortune-mod; do
-    # verify the package is installed
-    rpm -q "$package"
-    # verify package came from the tmt-artifact-shared repository
-    dnf info --installed "$package" | grep -Eq "From repo(sitory)?\s*:\s*tmt-artifact-shared"
-done
+# verify the package is installed
+rpm -q bar
+# verify package came from the tmt-artifact-shared repository
+dnf info --installed bar | grep -Eq "From repo(sitory)?\s*:\s*tmt-artifact-shared"
