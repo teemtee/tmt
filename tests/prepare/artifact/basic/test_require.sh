2ea0b6050df9d96ba451eb390d42f7e57b2b4c00
Cristian Le <git@lecris.dev>
2026-05-28 12:40:17 +0200

    Make basic test generic
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/prepare/artifact/basic/test_require.sh
+++ tests/prepare/artifact/basic/test_require.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-. /usr/share/beakerlib/beakerlib.sh || exit 1
 
-rlJournalStart
-    rlPhaseStartTest "Test artifact installation on Fedora"
-        # Check that make comes from tmt-artifact-shared
-        rlRun "rpm -q make" 0 "Check that make is installed"
-        rlRun -s "dnf info --installed make"
-        rlAssertGrep "From repo(sitory)?\s*:\s*tmt-artifact-shared" $rlRun_LOG -E
-
-        # Verify docker-ce repository is enabled
-        rlRun -s "dnf repo info docker-ce-stable"
-        rlAssertGrep "Status.*: enabled" $rlRun_LOG
-
-    rlPhaseEnd
-rlJournalEnd
+if ! rpm -q bar; then
+  echo "Error: bar was not installed"
+  exit 1
+fi
