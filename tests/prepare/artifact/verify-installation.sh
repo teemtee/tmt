8867accab8b914a87d8551bfd6a3cc3d8e01f20f
Cristian Le <git@lecris.dev>
2026-05-28 10:54:12 +0200

    Flatten artifact test organization
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/prepare/artifact/verify-installation.sh
+++ tests/prepare/artifact/verify-installation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 . /usr/share/beakerlib/beakerlib.sh || exit 1
-. ../../../images.sh || exit 1
-. ../lib/common.sh || exit 1
+. ../../images.sh || exit 1
+. lib/common.sh || exit 1
 
 rlJournalStart
     rlPhaseStartSetup
         rlRun "PROVISION_HOW=${PROVISION_HOW:-container}"
-        rlRun "pushd data"
+        rlRun "pushd verify-installation"
         rlRun "run=\$(mktemp -d)" 0 "Create run directory"
 
         setup_distro_environment
