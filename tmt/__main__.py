fc2a55b19462260db5f532246cb0e66b7b6ef7b8
Miloš Prchlík <mprchlik@redhat.com>
2026-06-15 20:36:31 +0200

    squash: simplify, no need for extra special handling of import errors

--- tmt/__main__.py
+++ tmt/__main__.py
@@ -1,6 +1,4 @@
-import sys
 import traceback
-from typing import NoReturn, Optional
 
 
 def import_cli_commands() -> None:
@@ -14,40 +12,6 @@ def import_cli_commands() -> None:
     import tmt.cli.lint  # noqa: F401,I001,RUF100 # type: ignore[reportUnusedImport]
     import tmt.cli.status  # noqa: F401,I001,RUF100 # type: ignore[reportUnusedImport]
     import tmt.cli.trying  # noqa: F401,I001,RUF100 # type: ignore[reportUnusedImport]
-
-
-def report_exception(
-    exception: Exception, exit_code: int = 2, message: Optional[str] = None
-) -> NoReturn:
-    # `tmt` may be unbound. In theory, `import tmt.utils` might have
-    # raised an exception, and we might end up touching `tmt.utils`
-    # that's not fully imported.
-    #
-    # Yet the reporting tools we have available are very nice, it would
-    # be a shame to not use them if we can. Let's try using our tools,
-    # and fall back to the very basic tools if anything goes wrong.
-
-    if message:
-        print(message, file=sys.stderr)
-
-    try:
-        # If we already succeeded importing `tmt.utils`, this will proceed
-        # safely, pretty much a no-op. If we failed to import `tmt.utils`,
-        # this will fail, but that's fine, we are ready for double fault.
-        from tmt.utils import show_exception
-
-        show_exception(exception)
-
-        raise SystemExit(exit_code) from exception
-
-    except Exception:
-        # No need to capture the exception in a variable: we are still
-        # inside an `except` clause, Python will chain exceptions for
-        # us. Reporting "the current" exception will include the nested
-        # one as well.
-        traceback.print_exc()
-
-        raise SystemExit(exit_code) from exception
 
 
 def run_cli() -> None:
@@ -74,13 +38,33 @@ def run_cli() -> None:
 
         tmt.cli._root.main()
 
-    except ImportError as error:
-        report_exception(
-            error, exit_code=1, message="Error: tmt package does not seem to be installed"
-        )
-
     except Exception as error:
-        report_exception(error)
+        # `tmt` may be unbound. In theory, `import tmt.utils` might have
+        # raised an exception, and we might end up touching `tmt.utils`
+        # that's not fully imported.
+        #
+        # Yet the reporting tools we have available are very nice, it would
+        # be a shame to not use them if we can. Let's try using our tools,
+        # and fall back to the very basic tools if anything goes wrong.
+
+        try:
+            # If we already succeeded importing `tmt.utils`, this will proceed
+            # safely, pretty much a no-op. If we failed to import `tmt.utils`,
+            # this will fail, but that's fine, we are ready for double fault.
+            from tmt.utils import show_exception
+
+            show_exception(error)
+
+            raise SystemExit(2) from error
+
+        except Exception:
+            # No need to capture the exception in a variable: we are still
+            # inside an `except` clause, Python will chain exceptions for
+            # us. Reporting "the original" exception will include "the
+            # current" one as well.
+            traceback.print_exc()
+
+            raise SystemExit(2) from error
 
 
 if __name__ == "__main__":
