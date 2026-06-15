ca4fc4ff2a8faad19207a1d5b79841c5f6f63e60
Miloš Prchlík <mprchlik@redhat.com>
2026-06-15 10:47:50 +0200

    Add a background thread fetching guest logs periodically

--- tmt/steps/provision/mock.py
+++ tmt/steps/provision/mock.py
@@ -647,7 +647,7 @@ class GuestMock(tmt.Guest):
         (self.mock_command_prefix + Command('--scrub=all')).run(cwd=None, logger=self._logger)
 
     def suspend(self) -> None:
-        self.stop()
+        self.stop(logger=self._logger)
 
     def start(self) -> None:
         if self.is_dry_run:
@@ -655,7 +655,7 @@ class GuestMock(tmt.Guest):
 
         self.mock_shell.enter_shell()
 
-    def stop(self) -> None:
+    def stop(self, logger: tmt.log.Logger) -> None:
         self.mock_shell.exit_shell()
         self.run(Command('rm', '-rf', str(self.root_path / MOCK_PIPE)))
 
