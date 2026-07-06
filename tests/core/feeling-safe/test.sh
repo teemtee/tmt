e934a9f36340b7947668502db6fa63625e9db533
Miloš Prchlík <mprchlik@redhat.com>
2026-07-06 23:14:33 +0200

    Granular "feeling safe" control
    Instead of one big all-or-nothing switch, finer control over various
    dangerous action is needed. Users can still allow everything, with
    `--feeling-safe[=all]`, or allow individual dangerous functionalities.

--- tests/core/feeling-safe/test.sh
+++ tests/core/feeling-safe/test.sh
@@ -16,18 +16,30 @@ rlJournalStart
         rlAssertNotGrep "User is feeling safe" $rlRun_LOG
 
         # Environment variable
-        rlRun -s "TMT_FEELING_SAFE=0 tmt $options" 2
+        rlRun -s "TMT_FEELING_SAFE= tmt $options" 2
         rlAssertNotGrep "User is feeling safe" $rlRun_LOG
     rlPhaseEnd
 
     rlPhaseStartTest "Feeling Safe"
         # Command line option
-        rlRun -s "tmt --feeling-safe $options"
-        rlAssertGrep "User is feeling safe" $rlRun_LOG
+        rlRun -s "tmt --feeling-safe=all $options"
+        rlAssertGrep "User is feeling safe: all unsafe behavior allowed" $rlRun_LOG
+
+        rlRun -s "tmt --feeling-safe=provision/local $options"
+        rlAssertGrep "User is feeling safe: 'local' provisioning plugin allowed" $rlRun_LOG
+
+        rlRun -s "tmt --feeling-safe=provision/local --feeling-safe=all $options"
+        rlAssertGrep "User is feeling safe: all unsafe behavior allowed" $rlRun_LOG
 
         # Environment variable
-        rlRun -s "TMT_FEELING_SAFE=1 tmt $options"
-        rlAssertGrep "User is feeling safe" $rlRun_LOG
+        rlRun -s "TMT_FEELING_SAFE=all tmt $options"
+        rlAssertGrep "User is feeling safe: all unsafe behavior allowed" $rlRun_LOG
+
+        rlRun -s "TMT_FEELING_SAFE=provision/local tmt $options"
+        rlAssertGrep "User is feeling safe: 'local' provisioning plugin allowed" $rlRun_LOG
+
+        rlRun -s "TMT_FEELING_SAFE='provision/local all' tmt $options"
+        rlAssertGrep "User is feeling safe: all unsafe behavior allowed" $rlRun_LOG
     rlPhaseEnd
 
     rlPhaseStartCleanup
