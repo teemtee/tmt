14d2f90339790c1cc599c5e28b84bcf3ec16374c
Cristian Le <git@lecris.dev>
2026-07-07 09:48:40 +0200

    Drop test for repo priority
    Also make grep verbose
    
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/prepare/artifact/providers/repository-url/data/test.sh
+++ tests/prepare/artifact/providers/repository-url/data/test.sh
@@ -5,7 +5,4 @@ set -ex
 rpm -q bar
 
 # Verify bar came from the tmt-repo-default-0 repository
-dnf info --installed bar | grep -Eq "From repo(sitory)?\s*:\s*tmt-repo-default-0"
-
-# Verify the repository priority is set to default 50
-dnf repoinfo tmt-repo-default-0 | grep -Eq "Priority\s*:\s*50"
+dnf info --installed bar | grep -E "From repo(sitory)?\s*:\s*tmt-repo-default-0"
