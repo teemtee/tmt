8afe00d33ed15d0d7846a9a2731f2984a59b9e68
Cristian Le <git@lecris.dev>
2026-07-08 15:24:17 +0200

    Skip dnf info call in one more test
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/prepare/artifact/providers/file/data/test.sh
+++ tests/prepare/artifact/providers/file/data/test.sh
@@ -3,5 +3,3 @@ set -ex
 
 # verify the package is installed
 rpm -q bar
-# verify package came from the tmt-artifact-shared repository
-dnf info --installed bar | grep -Eq "From repo(sitory)?\s*:\s*tmt-artifact-shared"
