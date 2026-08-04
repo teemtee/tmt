8cfad655826b6b98c73f5fb6e8adabf3954e8720
Miloš Prchlík <mprchlik@redhat.com>
2026-08-04 13:01:33 +0200

    squash: drop todo, but for real

--- docs/conf.py
+++ docs/conf.py
@@ -265,6 +265,3 @@ redirects = {
     "releases": "releases/index.html",
     "code/autodocs": "z_autodocs/",
 }
-
-# -- Options for sphinx.ext.todo ------------------------------------------
-todo_include_todos = True
