cfd482012792805096e9832a660b824cf2f60488
Miloš Prchlík <mprchlik@redhat.com>
2026-08-06 22:23:56 +0200

    Add support for excluding files based on .gitignore to copy_tree
    When copying a tree from a git repository, it is useful to not copy
    files ignored by git, i.e. "honoring" the `.gitignore` files. This has
    been implemented at one place - plan populating its worktree - but there
    are other places that would benefit from this feature.
    
    Therefore `copy_tree` gains new parameters, existing strategies are
    extended where possible.

--- tmt/utils/filesystem.py
+++ tmt/utils/filesystem.py
@@ -4,11 +4,13 @@ Utility functions for filesystem operations.
 
 import contextlib
 import enum
+import functools
 import shutil
 from collections.abc import Generator
-from typing import Optional, Protocol
+from typing import Literal, Optional, Protocol, overload
 
 import tmt.log
+import tmt.utils.git
 from tmt._compat.pathlib import Path
 from tmt.utils import Command, GeneralError, RunError, show_exception_as_warning
 from tmt.utils.environment import Environment
@@ -45,6 +47,8 @@ class CopyStrategy(Protocol):
         src: Path,
         dst: Path,
         tmpdir_creator: Optional[TmpDirCreator] = None,
+        exclude_gitignore: bool = False,
+        git_root: Optional[Path] = None,
         logger: tmt.log.Logger,
     ) -> StrategyOutcome:  # type: ignore[reportReturnType,unused-ignore]
         pass
@@ -55,6 +59,8 @@ def _copy_tree_cp(
     src: Path,
     dst: Path,
     tmpdir_creator: Optional[TmpDirCreator] = None,
+    exclude_gitignore: bool = False,
+    git_root: Optional[Path] = None,
     logger: tmt.log.Logger,
 ) -> StrategyOutcome:
     """
@@ -75,6 +81,14 @@ def _copy_tree_cp(
     :returns: ``True`` if successful, ``False`` if ``cp`` command fails
         with :py:class:`RunError`.
     """
+
+    if exclude_gitignore:
+        logger.debug(
+            f"Copy tree '{src}' => '{dst}' using 'cp --reflink=auto' strategy"
+            " does not support gitignore filtering."
+        )
+
+        return StrategyOutcome.YIELD
 
     logger.debug(f"Copy tree '{src}' => '{dst}' using 'cp --reflink=auto' strategy.")
 
@@ -102,6 +116,8 @@ def _copy_tree_rsync(
     src: Path,
     dst: Path,
     tmpdir_creator: Optional[TmpDirCreator] = None,
+    exclude_gitignore: bool = False,
+    git_root: Optional[Path] = None,
     logger: tmt.log.Logger,
 ) -> StrategyOutcome:
     """
@@ -121,6 +137,21 @@ def _copy_tree_rsync(
 
     logger.debug(f"Copy tree '{src}' => '{dst}' using 'rsync -a' strategy.")
 
+    # Honor filtering
+    filters: list[str] = []
+
+    if exclude_gitignore and git_root is not None:
+        current = src.resolve()
+        root = git_root.resolve()
+
+        while current not in (root, current.parent):
+            current = current.parent
+
+            gitignore = current / '.gitignore'
+
+            if gitignore.is_file():
+                filters += ['--filter', f'dir-merge,- {gitignore}']
+
     with tmpdir_creator(prefix='rsync-') as rsync_tempdir:
         try:
             Command(
@@ -128,6 +159,10 @@ def _copy_tree_rsync(
                 '-ar',
                 '--temp-dir',
                 rsync_tempdir,
+                *filters,
+                '--include=**.gitignore',
+                '--exclude=/.git',
+                '--filter=:- .gitignore',
                 f"{src}/",
                 dst,
             ).run(cwd=None, environment=Environment.from_environ(), silent=True, logger=logger)
@@ -146,6 +181,8 @@ def _copy_tree_shutil(
     src: Path,
     dst: Path,
     tmpdir_creator: Optional[TmpDirCreator] = None,
+    exclude_gitignore: bool = False,
+    git_root: Optional[Path] = None,
     logger: tmt.log.Logger,
 ) -> StrategyOutcome:
     """
@@ -161,12 +198,34 @@ def _copy_tree_shutil(
 
     logger.debug(f"Copy tree '{src}' => '{dst}' using 'shutil.copytree' strategy.")
 
-    shutil.copytree(
+    _copytree = functools.partial(
+        shutil.copytree,
         src,
         dst,
         symlinks=True,
         dirs_exist_ok=True,
     )
+
+    if exclude_gitignore and git_root is not None:
+        exclude_paths = {
+            str(path).rstrip('/')
+            for path in tmt.utils.git.git_ignore(root=git_root, logger=logger)
+        }
+
+        def _ignore(path: str, entries: list[str]) -> set[str]:
+            current_dirpath_relative = Path(path).relative_to(src)
+
+            return {
+                entry
+                for entry in entries
+                if str(current_dirpath_relative / entry).rstrip('/') in exclude_paths
+                or entry.rstrip('/') in exclude_paths
+            }
+
+        _copytree(ignore=_ignore)
+
+    else:
+        _copytree()
 
     return StrategyOutcome.SUCCESS
 
@@ -178,11 +237,39 @@ _COPY_TREE_STRATEGIES: tuple[CopyStrategy, ...] = (
 )
 
 
+@overload
 def copy_tree(
     *,
     src: Path,
     dst: Path,
     tmpdir_creator: Optional[TmpDirCreator] = None,
+    exclude_gitignore: Literal[False] = False,
+    git_root: None = None,
+    logger: tmt.log.Logger,
+) -> None:
+    pass
+
+
+@overload
+def copy_tree(
+    *,
+    src: Path,
+    dst: Path,
+    tmpdir_creator: Optional[TmpDirCreator] = None,
+    exclude_gitignore: Literal[True] = True,
+    git_root: Path,
+    logger: tmt.log.Logger,
+) -> None:
+    pass
+
+
+def copy_tree(
+    *,
+    src: Path,
+    dst: Path,
+    tmpdir_creator: Optional[TmpDirCreator] = None,
+    exclude_gitignore: bool = False,
+    git_root: Optional[Path] = None,
     logger: tmt.log.Logger,
 ) -> None:
     """
@@ -211,6 +298,12 @@ def copy_tree(
     :param tmpdir_creator: a context manager that, when invoked, would
         create and hold a temporary directory. Some strategies may
         require such a directory for their work.
+    :param exclude_gitignore: if set, exclude files ``git`` would ignore
+        because of them being listed in one of the ``.gitignore`` files
+        in the repository. ``git_root`` must be provided as well,
+        otherwise this feature would remain disabled.
+    :param git_root: path to the root of the git repository to search
+        for ``.gitignore`` files when ``exclude_gitignore`` is set.
     :param logger: Logger to use for debug messages.
     :raises GeneralError: when copying fails using all strategies, or if
         ``src`` does not exist or is not a directory.
@@ -229,7 +322,12 @@ def copy_tree(
     for strategy in _COPY_TREE_STRATEGIES:
         try:
             outcome = strategy(
-                src=src, dst=dst, tmpdir_creator=tmpdir_creator, logger=logger.descend()
+                src=src,
+                dst=dst,
+                tmpdir_creator=tmpdir_creator,
+                exclude_gitignore=exclude_gitignore,
+                git_root=git_root,
+                logger=logger.descend(),
             )
 
             if outcome is StrategyOutcome.SUCCESS:
