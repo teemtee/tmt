0a46981e1dfcf2676d8416dac530ae8db82e4386
Filip Vágner <fvagner@redhat.com>
2026-08-18 15:29:13 +0200

    squash: fix test

--- tests/recipe/generate.sh
+++ tests/recipe/generate.sh
@@ -8,43 +8,40 @@ rlJournalStart
         recipe="$run/recipe.yaml"
     rlPhaseEnd
 
-    function replace_values () {
+    function compare_recipe () {
+        local expected="$1"
+        # Replace the run root to match the expected recipe.
         temp_recipe=$(mktemp)
         yq '.run.root = "/path/to/fmf_root"' "$recipe" > "$temp_recipe"
         mv "$temp_recipe" "$recipe"
+        # Compare recipe content.
+        rlRun "yq -o json 'sort_keys(..)' \"$recipe\" > $run/actual_normalized_recipe.json"
+        rlRun "yq -o json 'sort_keys(..)' \"$expected\" > $run/expected_normalized_recipe.json"
+        rlRun "diff $run/actual_normalized_recipe.json $run/expected_normalized_recipe.json"
     }
 
     for plan_name in 'local' 'remote' 'minimal'; do
         rlPhaseStartTest "Test recipe generation of a $plan_name plan"
             rlRun -s "tmt -vv run --scratch --id $run -e RUN_ENV=run_value plan -n /plans/$plan_name"
             rlAssertExists "$recipe" "Recipe file exists"
-            replace_values
-            rlRun "yq 'sort_keys(..)' \"$recipe\" > $run/actual_normalized_recipe.yaml"
-            rlRun "yq 'sort_keys(..)' \"$plan_name.yaml\" > $run/expected_normalized_recipe.yaml"
-            rlRun "diff $run/actual_normalized_recipe.yaml $run/expected_normalized_recipe.yaml"
+            compare_recipe "$plan_name.yaml"
         rlPhaseEnd
     done
 
     rlPhaseStartTest "Test recipe generation of a plan with inserted discover step"
         rlRun -s "tmt -vv run --scratch --id $run -e RUN_ENV=run_value --all discover --insert -h fmf plan -n /plans/insert"
         rlAssertExists "$recipe" "Recipe file exists"
-        replace_values
-        rlRun "yq 'sort_keys(..)' \"$recipe\" > $run/actual_normalized_recipe.yaml"
-        rlRun "yq 'sort_keys(..)' insert.yaml > $run/expected_normalized_recipe.yaml"
-        rlRun "diff $run/actual_normalized_recipe.yaml $run/expected_normalized_recipe.yaml"
+        compare_recipe insert.yaml
     rlPhaseEnd
 
     rlPhaseStartTest "Test recipe generation of an imported plan"
         rlRun -s "tmt -vv run --scratch --id $run -e RUN_ENV=run_value discover plan -n /plans/import"
         rlAssertExists "$recipe" "Recipe file exists"
-        replace_values
-        rlRun "yq 'sort_keys(..)' \"$recipe\" > $run/actual_normalized_recipe.yaml"
-        rlRun "yq 'sort_keys(..)' import.yaml > $run/expected_normalized_recipe.yaml"
-        rlRun "diff $run/actual_normalized_recipe.yaml $run/expected_normalized_recipe.yaml"
+        compare_recipe import.yaml
     rlPhaseEnd
 
     rlPhaseStartCleanup
         rlRun "popd"
-        rlRun "rm -r $run" 0 "Remove run directory"
+        rlRun "rm -rf $run" 0 "Remove run directory"
     rlPhaseEnd
 rlJournalEnd
