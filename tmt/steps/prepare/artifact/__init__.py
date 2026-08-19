bffaf44c83760c5a4b997df549f5a46af45bdf82
Cristian Le <git@lecris.dev>
2026-08-19 08:03:09 +0200

    Install yum-plugin-priorities
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tmt/steps/prepare/artifact/__init__.py
+++ tmt/steps/prepare/artifact/__init__.py
@@ -438,9 +438,13 @@ class PrepareArtifact(PreparePlugin[PrepareArtifactData]):
 
     def essential_requires(self, guest: Guest) -> list[tmt.base.core.Dependency]:
         # createrepo is needed to create repository metadata from downloaded artifacts
-        return [
-            tmt.base.core.DependencySimple('/usr/bin/createrepo'),
+        requires: list[tmt.base.core.Dependency] = [
+            tmt.base.core.DependencySimple('/usr/bin/createrepo')
         ]
+        if guest.facts.package_manager == "yum":
+            # On yum we need `yum-plugin-priorities` in order to handle repo priorities
+            requires.append(tmt.base.core.DependencySimple("yum-plugin-priorities"))
+        return requires
 
     def _detect_duplicate_nvras(
         self, provider: ArtifactProvider, seen_nvras: dict[str, str]
