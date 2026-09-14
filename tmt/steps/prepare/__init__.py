98cc9a10c65288c162e16d161fd59713d3389865
Cristian Le <git@lecris.dev>
2026-09-14 10:29:38 +0200

    Dirty hack to make sure package manager's logger is from prepare
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tmt/steps/prepare/__init__.py
+++ tmt/steps/prepare/__init__.py
@@ -260,6 +260,9 @@ class Prepare(tmt.steps.StepWithQueue[PrepareStepData, PluginOutcome]):
                     self._logger,
                 )
 
+            # TODO(5154): package_manager's logger here is of provision. We swap it here
+            #  for a prepare one until all PackageManager methods require logger parameter.
+            guest.package_manager._logger = self._logger
             # Package managers can also have essential_requires
             collected_essential_requires[
                 guest
